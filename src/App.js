49ee311120417b83f8dce74fa65b99ffcf6304b9
d064310 <andreas.pfau@sap.com>
2021-02-10 17:01:38 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react"
+import logo from "./logo.svg"
+import "./App.css"
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p className="App-name">Juno</p>
+        <p className="App-slogan">The Global Dashboard</p>
+        <span className="App-info">will start soon...</span>{" "}
+        <img src="rocket.gif" width="200" />
+      </header>
+    </div>
+  )
+}
+
+export default App
