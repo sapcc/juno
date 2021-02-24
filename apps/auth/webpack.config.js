6a0e00fb01bbc68737c6483b165213188f6d1abd
d064310 <andreas.pfau@sap.com>
2021-02-24 08:56:20 +0100

    remove node_modules from apps/auth. Configure apps/auth to use jest

--- apps/auth/webpack.config.js
+++ apps/auth/webpack.config.js
@@ -1,75 +1,94 @@
-require("dotenv").config()
 const path = require("path")
-
 const HtmlWebpackPlugin = require("html-webpack-plugin")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const { HotModuleReplacementPlugin } = require("webpack")
 const { CleanWebpackPlugin } = require("clean-webpack-plugin")
 const { ModuleFederationPlugin } = require("webpack").container
 
-const package = require(path.join(__dirname, "./package.json"))
-console.log(package.name)
-module.exports = (env) => {
-  const isEnvDevelopment = env.NODE_ENV === "development"
-  const isEnvProduction = env.NODE_ENV === "production"
-
-  return {
-    entry: "./src/index",
-    mode: "development",
-
-    devServer: {
-      contentBase: path.join(__dirname, "./dist"),
-      port: env.PORT,
-      host: "0.0.0.0",
-
-      disableHostCheck: true,
-      // Enable hot reloading server. It will provide WDS_SOCKET_PATH endpoint
-      // for the WebpackDevServer client so it can learn when the files were
-      // updated. The WebpackDevServer client is included as an entry point
-      // in the webpack development configuration. Note that only changes
-      // to CSS are currently hot reloaded. JS changes will refresh the browser.
-      hot: true,
-      // Use 'ws' instead of 'sockjs-node' on server since we're using native
-      // websockets in `webpackHotDevClient`.
-      transportMode: "ws",
-      publicPath: process.env.PUBLIC_URL,
-    },
-    output: {
-      path: path.join(__dirname, "./dist"),
-      pathinfo: isEnvDevelopment,
-      publicPath: process.env.PUBLIC_URL,
-      chunkFilename: isEnvProduction
-        ? "js/[name].[contenthash:8].chunk.js"
-        : "js/[name].chunk.js",
-    },
-
-    module: {
-      rules: [
-        { parser: { requireEnsure: false } },
-        {
-          test: /\.jsx?$/,
-          loader: "babel-loader",
-          exclude: /node_modules/,
-          options: {
-            presets: ["@babel/preset-react"],
+module.exports = {
+  //our index file
+  entry: path.resolve(__dirname, "src/index.js"),
+  //Where we put the production code
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.[contenthash].js",
+    publicPath: "/",
+  },
+  // This says to webpack that we are in development mode and write the code in webpack file in different way
+  mode: "development",
+  module: {
+    rules: [
+      //Allows use javascript
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/, //don't test node_modules folder
+        loader: "babel-loader",
+        options: {
+          presets: [["@babel/preset-react", { runtime: "automatic" }]],
+        },
+      },
+      //Allows use of CSS
+      {
+        test: /\.css$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
           },
-        },
-      ],
-    },
-    plugins: [
-      new CleanWebpackPlugin(),
-      new ModuleFederationPlugin({
-        name: "auth",
-        library: { type: "var", name: "auth" },
-        exposes: {
-          // expose each component
-          "./App": "./src/App",
-        },
-        shared: ["react", "react-dom"],
-      }),
-      new HtmlWebpackPlugin({
-        template: "./public/index.html",
-        inject: true,
-        minify: isEnvProduction,
-      }),
+          "css-loader",
+        ],
+      },
+      //Allows use of images
+      {
+        test: /\.(png|jpg|svg)$/i,
+        loader: "file-loader",
+      },
     ],
-  }
+  },
+  optimization: {
+    splitChunks: { chunks: "all" },
+  },
+  plugins: [
+    //Allows remove/clean the build folder
+    new CleanWebpackPlugin(),
+    //Allows update react components in real time
+    new HotModuleReplacementPlugin(),
+    //Allows to create an index.html in our build folder
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public/index.html"), //we put the file that we created in public folder
+    }),
+    //This get all our css and put in a unique file
+    new MiniCssExtractPlugin({
+      filename: "styles.[contentHash].css",
+    }),
+    new ModuleFederationPlugin({
+      name: "auth",
+      library: { type: "var", name: "auth" },
+      exposes: {
+        // expose each component
+        "./App": "./src/App",
+      },
+      shared: ["react", "react-dom"],
+    }),
+  ],
+  //Config for webpack-dev-server module
+  devServer: {
+    contentBase: path.resolve(__dirname, "dist"),
+    port: process.env.PORT,
+    host: "0.0.0.0",
+
+    historyApiFallback: true,
+    disableHostCheck: true,
+    injectClient: false,
+    // Enable hot reloading server. It will provide WDS_SOCKET_PATH endpoint
+    // for the WebpackDevServer client so it can learn when the files were
+    // updated. The WebpackDevServer client is included as an entry point
+    // in the webpack development configuration. Note that only changes
+    // to CSS are currently hot reloaded. JS changes will refresh the browser.
+    hot: true,
+    // quiet: true,
+    // Use 'ws' instead of 'sockjs-node' on server since we're using native
+    // websockets in `webpackHotDevClient`.
+    transportMode: "ws",
+    publicPath: "/",
+  },
 }
