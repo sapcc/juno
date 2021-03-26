6fe6368c663d8e97a73f9df54687346a805ef90c
d064310 <andreas.pfau@sap.com>
2021-03-26 23:12:00 +0100

    dashboard: use custom emotion style cache to avoid conflicts with other mfes

--- apps/dashboard/src/App.js
+++ apps/dashboard/src/App.js
@@ -3,16 +3,19 @@ import Layout from "./components/layout/Layout"
 import GlobalStyles from "./lib/styling/GlobalStyles"
 import useMicroFrontendWidget from "./lib/hooks/useMicroFrontendWidget"
 
-function App() {
-  // useMicroFrontendWidget({
-  //   url: "https://juno.qa-de-1.cloud.sap/cdn/auth/0_1_1/widget.js",
-  // })
+import createCache from "@emotion/cache"
+import { CacheProvider } from "@emotion/react"
+// use custom style cache to avoid conflicts with other apps
 
+function App() {
+  useMicroFrontendWidget({
+    url: "https://juno.qa-de-1.cloud.sap/cdn/auth/0_1_1/widget.js",
+  })
   return (
-    <>
+    <CacheProvider value={createCache({ key: "juno-dashboard" })}>
       <GlobalStyles />
       <Layout />
-    </>
+    </CacheProvider>
   )
 }
 
