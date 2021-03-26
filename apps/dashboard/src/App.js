76e4602c1935a6ac345c957ef4c7454b86c2724c
d064310 <andreas.pfau@sap.com>
2021-03-26 23:29:26 +0100

    dashboard: do not re-render the whole app while loading mfes

--- apps/dashboard/src/App.js
+++ apps/dashboard/src/App.js
@@ -7,16 +7,19 @@ import createCache from "@emotion/cache"
 import { CacheProvider } from "@emotion/react"
 // use custom style cache to avoid conflicts with other apps
 
-function App() {
+const MFELoader = () => {
   useMicroFrontendWidget({
     url: "https://juno.qa-de-1.cloud.sap/cdn/auth/0_1_1/widget.js",
   })
-  return (
-    <CacheProvider value={createCache({ key: "juno-dashboard" })}>
-      <GlobalStyles />
-      <Layout />
-    </CacheProvider>
-  )
+  return null
 }
+
+const App = () => (
+  <CacheProvider value={createCache({ key: "juno-dashboard" })}>
+    <GlobalStyles />
+    <MFELoader />
+    <Layout />
+  </CacheProvider>
+)
 
 export default App
