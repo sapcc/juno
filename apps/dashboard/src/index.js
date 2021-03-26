c5eb3f80da8ba55105f1f27377b4a7cb2ac3fed7
d064310 <andreas.pfau@sap.com>
2021-03-26 08:04:40 +0100

    switch dashboard from styled-components to emotion

--- apps/dashboard/src/index.js
+++ apps/dashboard/src/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import App from "./App"
+import GlobalStyles from "./lib/styling/GlobalStyles"
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration"
 import reportWebVitals from "./reportWebVitals"
 
