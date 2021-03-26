11b3a1018ef8eccd0cf75096f68b3e7ed3fa7a8e
d064310 <andreas.pfau@sap.com>
2021-03-26 18:09:59 +0100

    deactivate auth ij dashboard

--- apps/dashboard/src/App.js
+++ apps/dashboard/src/App.js
@@ -4,9 +4,9 @@ import GlobalStyles from "./lib/styling/GlobalStyles"
 import useMicroFrontendWidget from "./lib/hooks/useMicroFrontendWidget"
 
 function App() {
-  useMicroFrontendWidget({
-    url: "https://juno.qa-de-1.cloud.sap/cdn/auth/0_1_1/widget.js",
-  })
+  // useMicroFrontendWidget({
+  //   url: "https://juno.qa-de-1.cloud.sap/cdn/auth/0_1_1/widget.js",
+  // })
 
   return (
     <>
