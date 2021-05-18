b5bf603ed4dd5622637c2b5950c5a1d7e782f768
d038721 <esther.schmitz@sap.com>
2021-05-18 17:54:15 +0200

    prepare for new cost comparison tool

--- apps/dashboard/src/components/landingpage/Pricing.js
+++ apps/dashboard/src/components/landingpage/Pricing.js
@@ -10,10 +10,9 @@ import { H4, FlexContainer } from "../shared/StyledComponents"
 import { getBgColorStyle, getTextColorStyle } from "../../lib/styling/StyleMap"
 
 
-const Container = styled.div`
-  ${tw`
-    // backdrop-filter
-    // backdrop-blur-xl
+const Container = tw.div`
+    backdrop-filter
+    backdrop-blur-xl
     rounded-lg
     bg-white
     bg-opacity-5
@@ -26,23 +25,17 @@ const Container = styled.div`
     divide-x-4
     divide-sap-dark-blue-900
     divide-opacity-50
-  `}
-
-  backdrop-filter: saturate(1.1) blur(20px);
 `
 
 const Box = tw.div`
   px-16
   py-4
-  border-b-4
-  border-sap-dark-blue
-  border-opacity-50
 `
 
 const BoxHighlight = tw(Box)`
   bg-gray-200
-  border-none
   rounded-tr-lg
+  border-none
   text-black
   py-16
   pr-8
@@ -62,6 +55,20 @@ const HeroNumber = tw.div`
   pb-4
 `
 
+const HeroSm = tw.span`
+  text-lg
+  xl:text-xl
+  font-light
+  px-2
+`
+
+const HeroMd = tw.span`
+  text-2xl
+  xl:text-3xl
+  font-light
+  px-2
+`
+
 const P = tw.p`
   font-thin
   text-base
@@ -72,138 +79,123 @@ const IconContainer = tw.div`
   pl-4
 `
 
-const ProgressContainer = tw.div`
-  relative
-  pt-20
-  pb-8
-`
+// const ProgressContainer = tw.div`
+//   relative
+//   pt-20
+//   pb-8
+// `
 
-const Progress = tw.div`
-  h-8
-  rounded-full
-  border-sap-dark-blue
-  border-opacity-70
-  bg-clip-padding
-  border-4
-  bg-white
-  relative
-`
+// const Progress = tw.div`
+//   h-8
+//   rounded-full
+//   border-sap-dark-blue
+//   border-opacity-70
+//   bg-clip-padding
+//   border-4
+//   bg-white
+//   relative
+// `
 
-const ProgressBar = tw.div`
-  relative
-  overflow-hidden
-  h-full
-  rounded-full
-  bg-sap-blue
-`
+// const ProgressBar = tw.div`
+//   relative
+//   overflow-hidden
+//   h-full
+//   rounded-full
+//   bg-sap-blue
+// `
 
-const ProgressIndicator = styled.div(() => [
-  tw`
-    absolute
-    top-0
-    -mt-1
-    -ml-4
-    h-8
-    w-8
-    rounded-full
-    border-white
-    border-opacity-70
-    border-4
-    bg-clip-padding
-    bg-white
-  `,
-  getBgColorStyle
-])
+// const ProgressIndicator = styled.div(() => [
+//   tw`
+//     absolute
+//     top-0
+//     -mt-1
+//     -ml-4
+//     h-8
+//     w-8
+//     rounded-full
+//     border-white
+//     border-opacity-70
+//     border-4
+//     bg-clip-padding
+//     bg-white
+//   `,
+//   getBgColorStyle
+// ])
 
-const ProgressFootnote = tw.div`
-  flex
-  justify-between
-  p-2
-  font-thin
-`
+// const ProgressFootnote = tw.div`
+//   flex
+//   justify-between
+//   p-2
+//   font-thin
+// `
 
-const IndicatorLogo = styled.div`
-  ${tw`
-    absolute
-    top-0
-  `}
+// const IndicatorLogo = styled.div`
+//   ${tw`
+//     absolute
+//     top-0
+//   `}
 
-  margin-left: -4.75rem;
-`
+//   margin-left: -4.75rem;
+// `
 
-const IndicatorText = styled.div(() => [
-  tw`
-    absolute
-    top-0
-    mr-8
-    text-sm
-    font-medium
-    leading-6
-  `,
-  getTextColorStyle
-])
+// const IndicatorText = styled.div(() => [
+//   tw`
+//     absolute
+//     top-0
+//     mr-8
+//     text-sm
+//     font-medium
+//     leading-6
+//   `,
+//   getTextColorStyle
+// ])
 
 
 
 const Pricing = () => {
 
   return (
-    <Container>
-      <Box>
-        <HeroNumber>150k</HeroNumber>
-        <H4>Average Price Benefit</H4>
-        <P>
-          Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
-        </P>
-      </Box>
-      <Box>
-        <HeroNumber>30%</HeroNumber>
-        <H4>More Performance</H4>
-        <P>
-          Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
-        </P>
-      </Box>
-      <BoxHighlight>
-        <H4 color="gold">Cost Safeguarding</H4>
-        <FlexContainer>
+    <>
+      <Container>
+        <Box>
+          <HeroNumber>
+            <HeroSm>up to</HeroSm>
+            -65,5
+            <HeroMd>%</HeroMd>
+          </HeroNumber>
+          <H4>Average Price Benefit</H4>
           <P>
-            Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
+            Depending on your configuration, you have a significant cost benefit compared to the most expensive provider.
           </P>
-          <IconContainer>
-            <UmbrellaIcon />
-          </IconContainer>
-        </FlexContainer>
-      </BoxHighlight>
-  
+        </Box>
+        <Box>
+          <HeroNumber>
+            100
+            <HeroMd>%</HeroMd>
+          </HeroNumber>
+          <H4>Free Traffic</H4>
+          <P>
+                  In CONVERGED CLOUD internet traffic is free. If you require large amounts of internet traffic this will be a significant cost saving compared to hyperscalers.
+          </P>
+        </Box>
+        <BoxHighlight>
+          <H4 color="gold">Cost Safeguarding</H4>
+          <FlexContainer>
+            <P>
+              Services tailored to your needs. By dynamically adapting your quota package to your actual needs, we protect you from unnecessary costs.
+            </P>
+            <IconContainer>
+              <UmbrellaIcon />
+            </IconContainer>
+          </FlexContainer>
+        </BoxHighlight>
+      </Container>
+
+
       <CostComparison>
-        <ProgressContainer>
-          <IndicatorLogo style={{"left": "30%"}}>
-            <CCShortLogo />
-          </IndicatorLogo>
-          <IndicatorLogo style={{"left": "88%"}}>
-            <AzureLogo />
-          </IndicatorLogo>
-          <IndicatorLogo style={{"left": "100%"}}>
-            <AWSLogo />
-          </IndicatorLogo>
-
-          <Progress>
-            <ProgressBar style={{"width": "30%"}} />
-            <IndicatorText style={{"right": "70%"}}>1,632 &euro;*</IndicatorText>
-            <IndicatorText style={{"right": "12%"}} color="black">4,497 &euro;*</IndicatorText>
-            <IndicatorText style={{"right": "0"}} color="black">4,960 &euro;*</IndicatorText>
-            <ProgressIndicator color="gold" style={{"left": "30%" }}/>
-            <ProgressIndicator color="darkBlueLight" style={{"left": "88%" }}/>
-            <ProgressIndicator color="darkBlueLight" style={{"left": "100%" }}/>
-          </Progress>
-
-          <ProgressFootnote>
-            <div>Configuration: Big Data example, 2500GB internet data traffic</div>
-            <div>* approximate cost / month</div>
-          </ProgressFootnote>
-        </ProgressContainer>  
+ 
       </CostComparison>
-    </Container>
+    </>
   )
 }
 
