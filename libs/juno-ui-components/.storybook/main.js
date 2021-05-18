ac1f02f38d33c5858d58b4afc688e6dea84f7088
d038721 <esther.schmitz@sap.com>
2021-05-18 18:14:18 +0200

    add storybook-dark-mode addon to main

--- libs/juno-ui-components/.storybook/main.js
+++ libs/juno-ui-components/.storybook/main.js
@@ -2,7 +2,7 @@ const path = require("path")
 
 module.exports = {
   stories: ["../src/**/stories.mdx", "../src/**/stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "storybook-dark-mode"],
   webpackFinal: async (config) => {
     config.module.rules.push({
       test: /\.scss$/,
