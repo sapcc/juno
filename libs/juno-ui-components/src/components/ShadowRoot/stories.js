865bd0e011724e3002b5029b2948c93db9a23838
Andreas Pfau <andreas.pfau@sap.com>
2021-05-31 16:41:47 +0200

    Juno UI components (#21)
    * juno-ui-components: fix show code action in shadow root component

--- libs/juno-ui-components/src/components/ShadowRoot/stories.js
+++ libs/juno-ui-components/src/components/ShadowRoot/stories.js
@@ -1,39 +1,14 @@
 import React from "react"
 import { ShadowRoot } from "."
 
-export default {
-  title: "Design System/ShadowRoot",
-  component: ShadowRoot,
-  argTypes: {
-    mode: {
-      control: {
-        type: "radio",
-        options: ["open", "closed"],
-      },
-    },
-    styles: {
-      control: {
-        type: "text",
-      },
-      defaultValue: "h1 {color: yellow;}",
-    },
-    delegateFocus: {
-      control: {
-        type: "radio",
-        options: [true, false],
-      },
-    },
-  },
-}
-
 const Template = (args) => (
   <ShadowRoot {...args}>
     <h1>Wellcome</h1>
   </ShadowRoot>
 )
 
-export const WithStyles = Template.bind({})
-WithStyles.args = {
+export const EncapsulateStyles = Template.bind({})
+EncapsulateStyles.args = {
   mode: "closed",
-  styles: "h1 {color: red;}",
+  styles: "h1 {color: orange;}",
 }
