e473efa2ba66426620e27c5faa745c77835e04ad
d038721 <esther.schmitz@sap.com>
2021-05-31 17:24:56 +0200

    button tests

--- libs/juno-ui-components/src/components/Button/Button.test.js
+++ libs/juno-ui-components/src/components/Button/Button.test.js
@@ -1,15 +1,43 @@
 import * as React from "react"
 import { render, screen } from "@testing-library/react"
-import { Primary } from "./stories.js"
-import "@testing-library/jest-dom/extend-expect"
+import { Button } from "./index"
 
 describe("Button", () => {
+  
+  test("renders a button with text passed as label", async () => {
+    render(<Button data-testid="button" label="Click me"></Button>)
+    expect(screen.getByTestId("button")).toBeInTheDocument()
+  })
+  
+  test("renders a button with text passed as children", async () => {
+    render(<Button data-testid="button">Click me</Button>)
+    expect(screen.getByTestId("button")).toBeInTheDocument()
+  })
+  
   test("renders a default button with text", async () => {
-    render(<Primary>Click me</Primary>)
-    expect(screen.getByText("Click me")).toBeInTheDocument()
-    expect(screen.getByText("Click me")).toHaveClass(
+    render(<Button data-testid="button">Click me</Button>)
+    expect(screen.getByTestId("button")).toBeInTheDocument()
+    expect(screen.getByTestId("button")).toHaveClass(
       "text-theme-on-default",
       "bg-theme-default"
+    )
+  })
+  
+  test("renders a primary button with text", async () => {
+    render(<Button data-testid="button" variant="primary">Click me</Button>)
+    expect(screen.getByTestId("button")).toBeInTheDocument()
+    expect(screen.getByTestId("button")).toHaveClass(
+      "text-theme-on-primary",
+      "bg-theme-primary"
+    )
+  })
+
+  test("renders a danger button with text", async () => {
+    render(<Button data-testid="button" variant="danger">Click me</Button>)
+    expect(screen.getByTestId("button")).toBeInTheDocument()
+    expect(screen.getByTestId("button")).toHaveClass(
+      "text-theme-on-danger",
+      "bg-theme-danger"
     )
   })
 })
