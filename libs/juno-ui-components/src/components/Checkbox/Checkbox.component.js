437173ee25d9245209a9a7f0b46d6d934b054ffd
Franz Heidl <franz@franzheidl.de>
2021-06-03 13:27:27 +0000

    add simple checkbox with tests

---
+++ libs/juno-ui-components/src/components/Checkbox/Checkbox.component.js
@@ -0,0 +1,40 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+
+export const Checkbox = ({
+	name,
+	value,
+	checked,
+	onChange,
+	...props
+}) => {
+	return (
+		<input 
+			type="checkbox"
+			name={name || "unnamed checkbox"}
+			value={value}
+			defaultChecked={checked}
+			onChange={onChange}
+			{...props}
+		/>
+	)
+}
+
+
+Checkbox.propTypes = { 
+	name: PropTypes.string,
+	value: PropTypes.string,
+	checked: PropTypes.oneOfType([
+		PropTypes.bool,
+		PropTypes.oneOf([ 'multiple' ])
+	]),
+	onChange: PropTypes.func,
+}
+
+
+Checkbox.defaultProps = {
+	checked: null,
+	value: "",
+	onChange: undefined,
+}
---
+++ libs/juno-ui-components/src/components/Checkbox/Checkbox.test.js
@@ -0,0 +1,61 @@
+import * as React from "react"
+import { render, screen, fireEvent } from "@testing-library/react"
+import { Checkbox } from "./index"
+
+
+describe("Checkbox", () => {
+	
+	test("renders a valid html input type checkbox", async () => {
+		render(<Checkbox />)
+		expect(screen.getByRole("checkbox")).toBeInTheDocument()
+		expect(screen.getByRole("checkbox")).toHaveAttribute('type', "checkbox")
+	})
+	
+	test("renders a checkbox with a name as passed", async () => {
+		render(<Checkbox name="My Checkbox" />)
+		expect(screen.getByRole("checkbox")).toBeInTheDocument()
+		expect(screen.getByRole("checkbox")).toHaveAttribute('name', "My Checkbox")
+	})
+	
+	test("renders a checkbox with a value as passed", async () => {
+		render(<Checkbox value="ValueAsPassed" />)
+		expect(screen.getByRole("checkbox")).toBeInTheDocument()
+		expect(screen.getByRole("checkbox")).toHaveAttribute('value', "ValueAsPassed")
+	})
+	
+	test("renders a checked checkbox as passed", async () => {
+		render(<Checkbox checked={true} />)
+		expect(screen.getByRole("checkbox")).toBeInTheDocument()
+		expect(screen.getByRole("checkbox")).toHaveAttribute('checked')
+	})
+	
+	test("renders all props as passed", async () => {
+		render(<Checkbox id="check-1" data-test="23" />)
+		expect(screen.getByRole("checkbox")).toBeInTheDocument()
+		expect(screen.getByRole("checkbox")).toHaveAttribute('id', "check-1")
+		expect(screen.getByRole("checkbox")).toHaveAttribute('data-test', "23")
+	})
+	
+	
+	
+	test("fire handler on change as passed", async () => {
+		const handleChange = jest.fn()
+		const { container } = render(
+			<Checkbox onChange={handleChange} />
+		)
+		const checkbox = container.firstChild
+		fireEvent.click(checkbox)
+		expect(handleChange).toHaveBeenCalledTimes(1)
+		expect(checkbox.checked).toBe(true)
+	})
+	
+	// test("renders a multiple checked checkbox as passed", async () => {
+		// 	render(<Checkbox checked="multiple" />)
+		// 	expect(screen.getByRole("checkbox")).toBeInTheDocument()
+		// 	expect(screen.getByRole("checkbox")).toHaveAttribute('checked', "multiple")
+		// })
+	
+	
+
+
+})
