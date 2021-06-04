5bbcb4e1c81f2b9d2e474b41d8ad2f1f61a882b7
Franz Heidl <franz@franzheidl.de>
2021-06-04 16:00:50 +0000

    Add simple radio component

---
+++ libs/juno-ui-components/src/components/Radio/stories.js
@@ -0,0 +1,21 @@
+import React from "react"
+import { Radio } from "./index.js"
+
+export default {
+  title: "Design System/Radio",
+  component: Radio,
+  argTypes: {},
+}
+
+const Template = (args) => <Radio {...args} />
+
+export const Default = Template.bind({})
+Default.args = {
+	checked: true
+}
+
+export const Disabled = Template.bind({})
+Disabled.args = {
+	checked: true,
+	disabled: true,
+}
