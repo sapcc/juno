79353b1c6b832b5705760d93119c76961398ce29
Franz Heidl <franz@franzheidl.de>
2021-06-09 10:26:07 +0000

    Add all files

---
+++ libs/juno-ui-components/src/components/RadioGroup/RadioGroup.test.js
@@ -0,0 +1,39 @@
+import * as React from "react"
+import { render, screen, fireEvent } from "@testing-library/react"
+import { RadioGroup } from "./index"
+import {Radio} from "../Radio/index"
+
+
+describe("RadioGroup", () => {
+	
+	test("does not render any radios if no children passed", async () => {
+		render(
+			<RadioGroup name="my-radiogroup"> 
+			</RadioGroup>
+		)
+		expect(() => {
+			screen.getByRole("radio");
+		  }).toThrow()
+	})
+	
+	test("renders radios as passed", async () => {
+		render(
+			<RadioGroup name="my-radiogroup"> 
+				<Radio />
+				<Radio />
+				<Radio />
+			</RadioGroup>
+		)
+		expect(screen.getAllByRole("radio")).toHaveLength(3)
+	})
+	
+	test("renders named checkboxes as passed", async () => {
+		render(
+			<RadioGroup name="my-radiogroup"> 
+				<Radio />
+			</RadioGroup>
+		)
+		expect(screen.getByRole("radio")).toHaveAttribute('name', "my-radiogroup")
+	})
+	
+})
