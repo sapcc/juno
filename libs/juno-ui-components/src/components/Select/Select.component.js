b9934a9177af7248a7b36a6bd33d87deee4af14d
Franz Heidl <franz@franzheidl.de>
2021-06-29 08:41:40 +0000

    Add onChange handler to Select

--- libs/juno-ui-components/src/components/Select/Select.component.js
+++ libs/juno-ui-components/src/components/Select/Select.component.js
@@ -5,11 +5,13 @@ import PropTypes from "prop-types"
 export const Select = ({
 	name,
 	children,
+	onChange,
 	...props
 }) => {
 	return (
 		<select 
 			name={name || "unnamed select"}
+			onChange={onChange}
 			{...props}
 		>
 		{children}
@@ -22,8 +24,11 @@ Select.propTypes = {
 	name: PropTypes.string,
 	/** Pass SelectOption and SelectOptionGroup as children. */
 	children: PropTypes.node,
+	/** Pass a handler */
+	onChange: PropTypes.func,
 }
 
 Select.defaultProps = {
 	name: null,
+	onChange: undefined,
 }
