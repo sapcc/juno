f3c02f327ee72b4c237b9b9bda7063e08d31c59d
Franz Heidl <franz@franzheidl.de>
2021-06-30 17:55:18 +0200

    add RadioRow

---
+++ libs/juno-ui-components/src/components/RadioRow/RadioRow.test.js
@@ -0,0 +1,25 @@
+import * as React from "react"
+import { render, screen, fireEvent } from "@testing-library/react"
+import { RadioRow } from "./index"
+
+describe("RadioRow", () => {
+	
+	
+	test("renders a radio row", async () => {
+		render(<RadioRow data-testid="radio-row" />)
+		expect(screen.getByTestId("radio-row")).toBeInTheDocument()
+	})
+	
+	test("renders a radio group with a cradio and an associated label with an id as passed", async () => {
+		render(<RadioRow data-testid="my-radio-row" label="My Radio Row" id="radio-row" />)
+		expect(screen.getByRole("radio")).toBeInTheDocument()
+		expect(screen.getByLabelText("My Radio Row")).toBeInTheDocument()
+		expect(screen.getByRole("radio")).toHaveAttribute("id", 'radio-row')
+	})
+	
+	test("renders a help text as passed", async () => {
+		render(<RadioRow helptext="Helptext goes here" />)
+		expect(screen.getByText("Helptext goes here")).toBeInTheDocument()
+	})
+	
+})
