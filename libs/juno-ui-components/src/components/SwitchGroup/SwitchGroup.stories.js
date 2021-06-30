c0a0e98c91409be4268985f9e36c56f7588082c0
Franz Heidl <franz@franzheidl.de>
2021-06-30 08:11:18 +0000

    add SwitchGroup stories (TODO: need fixing)

--- libs/juno-ui-components/src/components/SwitchGroup/SwitchGroup.stories.js
+++ libs/juno-ui-components/src/components/SwitchGroup/SwitchGroup.stories.js
@@ -13,3 +13,16 @@ export const Default = Template.bind({})
 Default.args = {
 	label: "Default Switch Group",
 }
+
+export const Checked = Template.bind({})
+Checked.args = {
+  label: "Checked Switch Group",
+  checked: true,
+}
+
+export const Disabled = Template.bind({})
+Disabled.args = {
+  label: "Disabled Switch Group",
+  checked: true,
+}
+
