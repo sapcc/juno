e832aebdc13edcac1703963e89e62732d0854bb5
Franz Heidl <franz@franzheidl.de>
2021-06-30 07:23:36 +0000

    Remove layout directions from CheckboxGroup

--- libs/juno-ui-components/src/components/CheckboxGroup/CheckboxGroup.component.js
+++ libs/juno-ui-components/src/components/CheckboxGroup/CheckboxGroup.component.js
@@ -5,32 +5,12 @@ import { useFormLayoutContext, FormLayoutProvider } from "../FormLayoutProvider"
 
 const checkboxgroup = `
 	flex
-`
-
-const checkboxgroupHorizontal = `
 	flex-row
 `
-
-const checkboxgroupVertical = `
-	flex-col
-`
-
-const layoutClass = (layoutDirection) => {
-	switch (layoutDirection) {
-		case "vertical":
-			return checkboxgroupVertical
-		case "horizontal":
-			return checkboxgroupHorizontal
-		default:
-			return checkboxgroupHorizontal
-	}
-}
-
 
 /** A checkbox input group containing a checkbox, associated label, and structural markup */
 export const CheckboxGroup =({
 	value,
-	layout,
 	name,
 	label,
 	id,
@@ -38,40 +18,14 @@ export const CheckboxGroup =({
 	onChange,
 	...props
 }) => {
-	/* 
-	Determine layout direction from prop or context:
-	1. Use as passed if passed as a prop to the component directly
-	2. if not, try to get from context and use if context exists
-	3. if no context exists, default.
-	*/
-	
-	/* Use from prop if passed: */
-	let layoutDirection = layout
-	/* Define default direction: */
-	const defaultLayoutDirection = "horizontal"
-	/* if no direction as been passed… */
-	if (!layoutDirection) {
-		/* … try to get direction from context */
-		try {
-			 layoutDirection = useFormLayoutContext()
-		/* If trying to get direction from context errors out, use default: */
-		} catch (e) {
-			 layoutDirection = defaultLayoutDirection
-			console.log(e)
-		/* If there is still no layout direction set, use default: */
-		} finally {
-			if (!layoutDirection) {
-				layoutDirection = defaultLayoutDirection
-			}
-		}
-	}
+
 	return (
 		<div
-			className={`${checkboxgroup} checkboxgroup-${layoutDirection} ${layoutClass(layoutDirection)}`}
+			className={`${checkboxgroup}`}
 			{...props}
 		>
 			<div>
-				<Checkbox name={name} onChange={onChange} id={id} value={value} />
+				<Checkbox name={name} onChange={onChange} id={id} value={value || ""} />
 				{helptext ? <p>{helptext}</p> : ""}
 			</div>
 			<div>
@@ -84,8 +38,6 @@ export const CheckboxGroup =({
 CheckboxGroup.propTypes = {
 	/** Optional initial value */
 	value: PropTypes.string,
-	/** Layout direction */
-	layout: PropTypes.oneOf(["horizontal", "vertical"]),
 	/** Name attribute of the checkbox element */
 	name: PropTypes.string,
 	/** Label text */
@@ -100,7 +52,6 @@ CheckboxGroup.propTypes = {
 
 CheckboxGroup.defaultProps = {
 	value: null,
-	layout: null,
 	name: null,
 	label: null,
 	id: null,
--- libs/juno-ui-components/src/components/CheckboxGroup/CheckboxGroup.test.js
+++ libs/juno-ui-components/src/components/CheckboxGroup/CheckboxGroup.test.js
@@ -17,21 +17,6 @@ describe("CheckboxGroup", () => {
 		expect(screen.getByRole("checkbox")).toHaveAttribute("id", 'checkbox-group')
 	})
 	
-	test("renders a horizontal checkbox group as passed", async () => {
-		render(<CheckboxGroup data-testid="my-checkbox-group" layout="horizontal" />)
-		expect(screen.getByTestId("my-checkbox-group")).toHaveClass("checkboxgroup-horizontal")
-	})
-	
-	test("renders a vertical checkbox group as passed", async () => {
-		render(<CheckboxGroup data-testid="my-checkbox-group" layout="vertical" />)
-		expect(screen.getByTestId("my-checkbox-group")).toHaveClass("checkboxgroup-vertical")
-	})
-	
-	test("renders a horizontal checkbox group per default", async () => {
-		render(<CheckboxGroup data-testid="my-checkbox-group" />)
-		expect(screen.getByTestId("my-checkbox-group")).toHaveClass("checkboxgroup-horizontal")
-	})
-	
 	test("renders a help text as passed", async () => {
 		render(<CheckboxGroup helptext="Helptext goes here" />)
 		expect(screen.getByText("Helptext goes here")).toBeInTheDocument()
