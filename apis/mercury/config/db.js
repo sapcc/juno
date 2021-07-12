5ad11e47bf938812eac243559f2537440f4d51f0
d064310 <andreas.pfau@sap.com>
2021-07-12 14:57:10 +0200

    mercury: improve db config

--- apis/mercury/config/db.js
+++ apis/mercury/config/db.js
@@ -1,3 +1,5 @@
+require("dotenv").config()
+
 const DB_HOST = process.env.JUNO_MERCURY_POSTGRESQL_SERVICE_HOST || "localhost"
 const DB_PORT = process.env.JUNO_MERCURY_POSTGRESQL_SERVICE_PORT || 5432
 
@@ -12,7 +14,12 @@ module.exports = {
     logging: false,
   },
   production: {
-    url: `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${process.env.DB_NAME}?schema=public`, //process.env.DATABASE_URL,
+    //url: `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${process.env.DB_NAME}?schema=public`, //process.env.DATABASE_URL,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: DB_HOST,
+    port: DB_PORT,
     dialect: "postgres",
   },
 }
