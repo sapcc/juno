b5acaad7a1c8b291529b152659d7dc55c24e72da
d064310 <andreas.pfau@sap.com>
2021-07-14 15:10:21 +0200

    designate: fix tests

--- apps/designate/babel.config.js
+++ apps/designate/babel.config.js
@@ -1,21 +1,3 @@
 module.exports = {
   presets: ["@babel/preset-env", "@babel/preset-react"],
-  plugins: [
-    "macros",
-    [
-      "@emotion/babel-plugin-jsx-pragmatic",
-      {
-        export: "jsx",
-        import: "__cssprop",
-        module: "@emotion/react",
-      },
-    ],
-    [
-      "@babel/plugin-transform-react-jsx",
-      {
-        pragma: "__cssprop",
-        pragmaFrag: "React.Fragment",
-      },
-    ],
-  ],
 }
