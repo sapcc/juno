9353460b1131b6c1d82cfdc2f6dc6d02684804ed
Franz Heidl <franz@franzheidl.de>
2021-07-20 16:42:03 +0200

    tmp: defautlt icon color to white for now

--- libs/juno-ui-components/src/components/Icon/Icon.component.js
+++ libs/juno-ui-components/src/components/Icon/Icon.component.js
@@ -2,21 +2,16 @@ import React from "react"
 import PropTypes from "prop-types"
 import Help from "./icons/help.svg"
 
-
 /**
-* A Message holds generally important information to help understand the contents, purpose, or state of a whole page or view. Use sparingly, there should never be any two or more subsequent instances of Message as direct siblings/neighbors on an individual view.
 The icon svgs in the icons folder correspond to the respective "xyz_24px.svg" from material-ui icons.
 */
 
 const getColorizedIcon = (icon, color) => {
-	
-	// TODO: determine default color based on theme
-	
 	  switch (icon) {
 		case "help":
 		  return <Help fill={color} />
 		default:
-		  return <Help  fill={color} />
+		  return <Help fill={color} />
 	  }
 	}
 
@@ -25,7 +20,8 @@ export const Icon = ({
 	color,
 	...props
 }) => {
-	return ( getColorizedIcon(icon, color) )
+	const clr = color || "white"
+	return ( getColorizedIcon(icon, clr) )
 }
 
 Icon.propTypes = { 
