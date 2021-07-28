4046e962759b5bc6f77b8a05298b1e8fc709a353
Franz Heidl <franz@franzheidl.de>
2021-07-28 13:46:54 +0200

    add basic floting label behaviour

--- libs/juno-ui-components/src/components/TextInputRow/TextInputRow.component.js
+++ libs/juno-ui-components/src/components/TextInputRow/TextInputRow.component.js
@@ -1,11 +1,44 @@
-import React from "react"
+import React, { useState } from "react"
 import PropTypes from "prop-types"
 import { TextInput } from "../TextInput/index.js"
 import { Label } from "../Label/index.js"
 
-const textinputrow = `
+const stackedcontainerstyles = `
 	flex
 	flex-col
+`
+
+const floatingcontainerstyles = `
+relative
+`
+
+const stackedlabelcontainerstyles = `
+`
+
+const floatinglabelcontainerstyles = `
+absolute
+top-0
+left-0
+px-3
+py-5
+pointer-events-none
+transform 
+origin-top-left 
+transition-all 
+duration-100 
+ease-in-out
+`
+
+const focusedlabelcontainerstyles = `
+scale-75
+-translate-x-3
+translate-x-1
+`
+
+const floatinginputstyles = `
+p-3 
+h-16
+placeholder-transparent
 `
 
 const helptextstyles = `
@@ -13,9 +46,32 @@ const helptextstyles = `
 	text-theme-disabled
 `
 
+const variantStyle = (variant, element) => {
+	switch (variant) {
+		case "floating":
+			switch (element) {
+				case "container":
+					return floatingcontainerstyles
+				case "labelcontainer":
+				return floatinglabelcontainerstyles
+				case "input":
+				return floatinginputstyles
+			}
+		case "stacked":
+			switch (element) {
+				case "container":
+					return stackedcontainerstyles
+				case "labelcontainer":
+				return stackedlabelcontainerstyles
+			}
+	}
+	
+}
+
 /** A text input group containing an input of type text, password, email, tel, or url, an associated label, and necessary structural markup. */
 export const TextInputRow = ({
 	type,
+	variant,
 	value,
 	name,
 	label,
@@ -26,16 +82,28 @@ export const TextInputRow = ({
 	onChange,
 	...props
 }) => {
+	
+	const [focus, setFocus] = useState(0)
+	
 	return (
 		<div 
-			className={`textinput-row ${textinputrow}`}
+			className={`textinput-row ${variantStyle(variant, "container")} `}
 			{...props}
 		>
-			<div>
+			<div className={`label-container ${variantStyle(variant, "labelcontainer")} ${variant == "floating" && focus ? focusedlabelcontainerstyles : ""}`}>
 				<Label text={label} htmlFor={id} />
 			</div>
 			<div>
-				<TextInput type={type} name={name} id={id} placeholder={placeholder} onChange={onChange} className={className} />
+				<TextInput 
+					type={type} 
+					name={name} 
+					id={id} 
+					placeholder={placeholder} 
+					onChange={onChange} 
+					onFocus={() => setFocus(1)}
+					onBlur={() => setFocus(0)}
+					className={`${variantStyle(variant, "input")} ${className}`} 
+				/>
 				{helptext ? <p className={`${helptextstyles}`}>{helptext}</p> : ""}
 			</div>
 		</div>	
@@ -45,6 +113,8 @@ export const TextInputRow = ({
 TextInputRow.propTypes = { 
 	/** The type of the input element to render */
 	type: PropTypes.oneOf(["text", "password", "email", "tel", "url"]),
+	/** Floating (default) or stacked layout variant */
+	variant: PropTypes.oneOf(["floating", "stacked"]),
 	/** Optional initial value */
 	value: PropTypes.string,
 	/** Name attribute of the input */
@@ -53,7 +123,7 @@ TextInputRow.propTypes = {
 	label: PropTypes.string,
 	/** Id */
 	id: PropTypes.string,
-	/** Placeholder for input */
+	/** Placeholder for the text input. Will not be visible on floating label inputs. */
 	placeholder: PropTypes.string,
 	/** Help text */
 	helptext: PropTypes.string,
@@ -65,6 +135,7 @@ TextInputRow.propTypes = {
 
 TextInputRow.defaultProps = {
 	type: null,
+	variant: "floating",
 	value: null,
 	name: null,
 	label: null,
