2c3e779156983248b7d8807f67667b07a4dd0576
Franz Heidl <franz@franzheidl.de>
2021-07-29 18:36:36 +0200

    add floating label to TextareaRow

--- libs/juno-ui-components/src/components/TextareaRow/TextareaRow.component.js
+++ libs/juno-ui-components/src/components/TextareaRow/TextareaRow.component.js
@@ -1,12 +1,40 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import PropTypes from "prop-types"
 import { Textarea } from "../Textarea/index.js"
 import { Label } from "../Label/index.js"
 
 
-const textarearow = `
+const stackedcontainerstyles = `
 	flex
 	flex-col
+`
+
+const floatingcontainerstyles = `
+	relative
+`
+
+const floatinglabelcontainerstyles = `
+	absolute
+	top-0
+	left-0
+	p-2.5
+	pointer-events-none
+	transform 
+	origin-top-left 
+	transition-all 
+	duration-100 
+	ease-in-out
+`
+
+const minimizedlabelcontainerstyles = `
+	scale-75
+	opacity-75
+	-translate-y-2.5
+	translate-x-1
+`
+
+const floatinginputstyles = `
+	placeholder-transparent
 `
 
 const helptextstyles = `
@@ -14,10 +42,30 @@ const helptextstyles = `
 	text-theme-disabled
 `
 
+const variantStyle = (variant, element) => {
+	switch (variant) {
+		case "floating":
+			switch (element) {
+				case "container":
+					return floatingcontainerstyles
+				case "labelcontainer":
+					return floatinglabelcontainerstyles
+				case "input":
+					return floatinginputstyles
+			}
+		case "stacked":
+			switch (element) {
+				case "container":
+					return stackedcontainerstyles
+			}
+	}
+}
+
 /** A textarea group containing a textarea, associated label, optional helptext, and structural markup */
 
 export const TextareaRow = ({
 	value,
+	variant,
 	name,
 	label,
 	id,
@@ -27,16 +75,52 @@ export const TextareaRow = ({
 	onChange,
 	...props
 }) => {
+	
+	const [val, setValue] = useState(null)
+	const [focus, setFocus] = useState(false)
+	
+	React.useEffect(() => {
+		setValue(value)
+	}, [value])
+	
+	const handleChange = (event) => {
+		setValue(event.target.value)
+		onChange()
+	}
+	
+	const minimizedLabel = (varint, value, focus) => {
+		if (variant === "floating") {
+			if (focus) {
+				return minimizedlabelcontainerstyles
+			} else if (value && value.length > 0) {
+				return minimizedlabelcontainerstyles
+			} else {
+				return ""
+			}
+		} else {
+			return ""
+		}
+	}
+	
 	return (
 		<div
-			className={`textarea-row ${textarearow}`}
+			className={`textarea-row ${variantStyle(variant, "container")} `}
 			{...props}
 		>
-			<div>
+			<div className={`input-container ${variantStyle(variant, "labelcontainer")} ${minimizedLabel(variant, val, focus)}`}>
 				<Label text={label} htmlFor={id} />
 			</div>
 			<div>
-				<Textarea name={name} onChange={onChange} id={id} value={value} placeholder={placeholder} className={className} />
+				<Textarea 
+					value={value} 
+					name={name} 
+					id={id}
+					placeholder={placeholder}
+					onChange={handleChange} 
+					onFocus={() => setFocus(true)}
+					onBlur={() => setFocus(false)}
+					className={`${variantStyle(variant, "input")} ${className}`} 
+				/>
 				{helptext ? <p className={`${helptextstyles}`}>{helptext}</p> : ""}
 			</div>
 		</div>
@@ -54,16 +138,19 @@ TextareaRow.propTypes = {
 	id: PropTypes.string,
 	/** Help text */
 	helptext: PropTypes.string,
-	/** Placeholder */
+	/** Placeholder for the text input. Will not be visible on floating label inputs. */
 	placeholder: PropTypes.string,
 	/** Pass a classnName to the Textarea */
 	className: PropTypes.string,
+	/** Floating (default) or stacked layout variant */
+	variant: PropTypes.oneOf(["floating", "stacked"]),
 	/** Pass a handler to the checkbox element */
 	onChange: PropTypes.func,
 }
 
 TextareaRow.defaultProps = {
 	value: null,
+	variant: "floating",
 	name: null,
 	label: null,
 	id: null,
