98332ab910af4d8fa9d9bdaec621a9586c7740ee
d038721 <esther.schmitz@sap.com>
2021-07-29 12:46:00 +0200

    add expand more/less icons

--- libs/juno-ui-components/src/components/Icon/Icon.component.js
+++ libs/juno-ui-components/src/components/Icon/Icon.component.js
@@ -4,6 +4,8 @@ import PropTypes from "prop-types"
 /* Import Icons here. The icon svgs in the icons folder correspond to the respective "xyz_24px.svg" from material-ui icons. 
 */
 import Help from "./icons/help.svg"
+import ExpandMore from "./icons/expand_more.svg"
+import ExpandLess from "./icons/expand_less.svg"
 
 /**
 Generic Icon component.
@@ -13,6 +15,10 @@ const getColoredSizedIcon = (icon, color, size) => {
 	  switch (icon) {
 		case "help":
 		  return <Help fill={color} width={size} height={size} />
+		case "expandLess":
+		  return <ExpandLess fill={color} width={size} height={size} />
+		case "expandMore":
+		  return <ExpandMore fill={color} width={size} height={size} />
 		default:
 		  return <Help fill={color} width={size} height={size} />
 	  }
