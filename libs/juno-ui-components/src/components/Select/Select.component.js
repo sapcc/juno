10b5d9dc6078a41e19906d8b8928c8ef3f7ad710
Franz Heidl <franz@franzheidl.de>
2021-08-04 11:14:31 +0200

    align select styles with text input

--- libs/juno-ui-components/src/components/Select/Select.component.js
+++ libs/juno-ui-components/src/components/Select/Select.component.js
@@ -1,6 +1,21 @@
 import React from "react"
 import PropTypes from "prop-types"
 
+const selectstyles = `
+	bg-theme-select
+	text-theme-high
+	appearance-none
+	text-base
+	px-3
+	h-11
+	border
+	border-transparent
+	rounded-3px
+	focus:outline-none
+	focus:border-theme-focus
+	focus:border-theme-focus
+`
+
 /*+ A basic, uncontrolled Select. Takes SelectOption and SelectOptionGroup as children. */
 export const Select = ({
 	name,
@@ -12,7 +27,7 @@ export const Select = ({
 	return (
 		<select 
 			name={name || "unnamed select"}
-			className={className}
+			className={`${selectstyles} ${className}`}
 			onChange={onChange}
 			{...props}
 		>
