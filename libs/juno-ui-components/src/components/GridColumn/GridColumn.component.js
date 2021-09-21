b5f2b674c6e3cbcd612e88c2aa0c9b00eeb1680d
Franz Heidl <franz@franzheidl.de>
2021-09-21 11:57:38 +0200

    add grid column widths based on cols

--- libs/juno-ui-components/src/components/GridColumn/GridColumn.component.js
+++ libs/juno-ui-components/src/components/GridColumn/GridColumn.component.js
@@ -7,14 +7,93 @@ const gridcolumnstyles = `
 	flex-basis-0
 `
 
+const col1 = `
+	w-grid-col-1
+`
+
+const col2 = `
+	w-grid-col-2
+`
+
+const col3 = `
+	w-grid-col-3
+`
+
+const col4 = `
+	w-grid-col-4
+`
+
+const col5 = `
+	w-grid-col-5
+`
+
+const col6 = `
+	w-grid-col-6
+`
+
+const col7 = `
+	w-grid-col-7
+`
+
+const col8 = `
+	w-grid-col-8
+`
+
+const col9 = `
+	w-grid-col-9
+`
+
+const col10 = `
+	w-grid-col-10
+`
+
+const col11 = `
+	w-grid-col-11
+`
+
+const col12 = `
+	w-grid-col-12
+	
+`
+const colstyles = (col) => {
+	switch (col) {
+		case 1:
+			return col1
+		case 2:
+			return col2
+		case 3:
+			return col3
+		case 4:
+			return col4
+		case 5:
+			return col5
+		case 6:
+			return col6
+		case 7:
+			return col7
+		case 8:
+			return col8
+		case 9:
+			return col9
+		case 10:
+			return col10
+		case 11:
+			return col11
+		case 12:
+			return col12
+		default:
+			return ``
+	}
+}
+
 export const GridColumn = ({
 	width,
-	span,
+	col,
 	children,
 	...props
 }) => {
 	return (
-		<div className={`grid-column ${gridcolumnstyles}`}>
+		<div className={`grid-column ${gridcolumnstyles} ${colstyles(col)}`}>
 			{children}
 		</div>
 	)
@@ -22,13 +101,13 @@ export const GridColumn = ({
 
 
 GridColumn.propTypes = {
-	/** The width in percentages for auto-layout grids. If a width is given, it will override the span prop. */
+	/** The width in percentages for auto-layout grids. If a width is given, it will override the "col" prop. */
 	width: PropTypes.number,
 	/** The number of columns in a fixed grid to span */
-	span: PropTypes.node,
+	col: PropTypes.number,
 }
 
 GridColumn.defaultProps = {
 	width: null,
-	span: null,
+	col: null,
 }
