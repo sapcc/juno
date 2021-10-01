64b6900983a2912f0dc1e43067a64855cf9ba3e3
d038721 <esther.schmitz@sap.com>
2021-10-01 10:53:51 +0200

    debug log props

--- apps/whois/src/bootstrap.js
+++ apps/whois/src/bootstrap.js
@@ -5,6 +5,7 @@ import styles from "./styles.css"
 import StyleProvider from "juno-ui-components"
 
 export const init = (wrapper, props) => {
+  console.log("WHOIS props: ", props);
   ReactDOM.render(
     <StyleProvider stylesWrapper="shadowRoot" theme={`${props.theme ? props.theme : "theme-dark"}`}>
       {/* load styles inside the shadow dom */}
