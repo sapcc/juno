8eabd8eb454fb4c42ca4fa1f642a7894c7470c00
d038721 <esther.schmitz@sap.com>
2021-10-27 14:15:44 +0200

    header

--- apps/dashboard/src/components/layout/PageHead.js
+++ apps/dashboard/src/components/layout/PageHead.js
@@ -2,7 +2,7 @@ import React from "react"
 
 import { PageHeader } from "juno-ui-components"
 
-import { Link } from "react-router-dom"
+// import { Link } from "react-router-dom"
 
 
 const PageHead = () => {
--- apps/dashboard/src/pages/home.js
+++ apps/dashboard/src/pages/home.js
@@ -1,12 +1,20 @@
 import React from "react"
-// import { ReactComponent as CCPlusOneLogo } from "../assets/images/CCplusOne_logo.svg"
+
+import CCPlusOneLogo from "../assets/images/CCplusOne_logo.svg"
+import { Stack } from "juno-ui-components"
 
 
 const Home = () => {
   return (
-    <>
-      Home
-    </>
+    <div className="container mx-auto">
+      <CCPlusOneLogo className="-ml-7" />
+
+      <Stack>
+        <div className="text-2xl">
+        Engage eyeballs collaborative: best-of-breed applications out-of-the-box dynamic next-generation optimize, B2B. Solutions markets benchmark methodologies syndicate out-of-the-box reinvent disintermediate users e-enable front-end.
+        </div>
+      </Stack>
+    </div>
   )
 }
 
