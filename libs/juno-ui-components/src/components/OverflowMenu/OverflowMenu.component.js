e30dfd1de2062b588712f70a56db8b3864392d2b
Franz Heidl <franz@franzheidl.de>
2021-11-02 11:16:48 +0100

    add custom className, aria-haspopup

--- libs/juno-ui-components/src/components/OverflowMenu/OverflowMenu.component.js
+++ libs/juno-ui-components/src/components/OverflowMenu/OverflowMenu.component.js
@@ -3,18 +3,22 @@ import PropTypes from "prop-types"
 import { ClickableIcon } from "../ClickableIcon/index.js"
 
 export const OverflowMenu = ({
-	disabled
+	disabled,
+	className
 }) => {
 	return (
-		<ClickableIcon disabled={disabled} />
+		<ClickableIcon disabled={disabled} className={className} aria-haspopup/>
 	)
 }
 
 OverflowMenu.propTypes = {
 	/** Whether the menu is disabled */
 	disabled: PropTypes.bool,
+	/** Add custom classes to the toggle */
+	className: PropTypes.string,
 }
 
 OverflowMenu.defaultProps = {
 	disabled: false,
+	classname: null,
 }
--- libs/juno-ui-components/src/components/OverflowMenu/OverflowMenu.test.js
+++ libs/juno-ui-components/src/components/OverflowMenu/OverflowMenu.test.js
@@ -4,14 +4,24 @@ import { OverflowMenu } from "./index"
 
 describe("OverflowMenu", () => {
 	
-	test("renders a ClickableIcon as a toggle for the mneu", async () => {
+	test("renders a ClickableIcon as a toggle for the menu", async () => {
 		render(<OverflowMenu />)
 		expect(screen.getByRole("button")).toBeInTheDocument()
+	})
+	
+	test("renders a ClickableIcon with aria-haspopup", async () => {
+		render(<OverflowMenu />)
+		expect(screen.getByRole("button")).toHaveAttribute("aria-haspopup")
 	})
 	
 	test("renders a disabled OverflowMenu as passed", async () => {
 		render(<OverflowMenu disabled/>)
 		expect(screen.getByRole("button")).toBeDisabled()
+	})
+	
+	test("renders a custom classname as passed", async () => {
+		render(<OverflowMenu className={"my-custom-class"}/>)
+		expect(screen.getByRole("button")).toHaveClass("my-custom-class")
 	})
 	
 })
