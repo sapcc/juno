227e75b85dc72c55bddf1e527876671a191550c9
d038721 <esther.schmitz@sap.com>
2021-11-09 15:59:26 +0100

    add login button to pagehead

--- apps/dashboard/src/components/layout/PageHead.js
+++ apps/dashboard/src/components/layout/PageHead.js
@@ -1,13 +1,21 @@
 import React from "react"
 
-import { PageHeader } from "juno-ui-components"
+import { Button, Icon, PageHeader } from "juno-ui-components"
 
 // import { Link } from "react-router-dom"
 
 
 const PageHead = () => {
   return (
-    <PageHeader />
+    <PageHeader>
+      
+      <div className="ml-auto">
+        <Button variant="primary" title="Log in">
+          <Icon icon="manageAccounts" color="text-white" className="mr-3" />
+          Log in
+        </Button>
+      </div>
+    </PageHeader>
   )
 }
 
