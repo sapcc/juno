546674b3c24f2f70f0b35f8299cc145b83bf4b8f
Franz Heidl <franz@franzheidl.de>
2021-11-23 14:21:41 +0100

    [ui] improve FormSection story

--- libs/juno-ui-components/src/components/FormSection/FormSection.stories.js
+++ libs/juno-ui-components/src/components/FormSection/FormSection.stories.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { FormSection } from "./index.js"
-import { Default as DefaultTextInputRow } from "../TextInputRow/TextInputRow.stories"
+import { TextInputRow } from "../TextInputRow/"
+import { Default as DefaultTextInputRowStory } from "../TextInputRow/TextInputRow.stories"
 
 export default {
   title: "Design System/Forms/FormSection",
@@ -8,22 +9,32 @@ export default {
   argTypes: {},
 }
 
-const Template = (args) => <FormSection {...args}>
-  {args.children.map((child, i) => (
-    <DefaultTextInputRow {...child}  key={`input-${i}`} />
-  ))}
-</FormSection>
+const Template = ({ items, ...args }) => (
+  <FormSection {...args}>
+    {items.map((item, i) => (
+      <TextInputRow {...item}  key={`input-${i}`} />
+    ))}
+  </FormSection>
+)
 
 export const Default = Template.bind({})
 Default.args = {
-  children: [DefaultTextInputRow.args]
+  items: 
+    [
+      { ...DefaultTextInputRowStory.args, label: "Address 1", id: "d-1" },
+      { ...DefaultTextInputRowStory.args, label: "Address 2", id: "d-2" }
+    ]
 }
 
 
 export const WithTitle = Template.bind({})
 WithTitle.args = {
 	title: "Form Section With Title",
-  children: [DefaultTextInputRow.args]
+  items: 
+  [
+    { ...DefaultTextInputRowStory.args, label: "Address 1", id: "wt-1" },
+    { ...DefaultTextInputRowStory.args, label: "Address 2", id: "wt-2" }
+  ]
 }
 
 
