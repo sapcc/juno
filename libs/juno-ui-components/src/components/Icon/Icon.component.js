e9409609c44bec72107baa5cdb4689d5b89a508d
d038721 <esther.schmitz@sap.com>
2021-12-07 13:59:39 +0100

    [ui] add icons description, forum, insert_comment

--- libs/juno-ui-components/src/components/Icon/Icon.component.js
+++ libs/juno-ui-components/src/components/Icon/Icon.component.js
@@ -9,11 +9,14 @@ import Cancel from "./icons/cancel.svg"
 import Close from "./icons/close.svg"
 import Danger from "./icons/danger.svg"
 import Dangerous from "./icons/dangerous.svg"
+import Description from "./icons/description.svg"
 import Error from "./icons/error.svg"
 import ExpandLess from "./icons/expand_less.svg"
 import ExpandMore from "./icons/expand_more.svg"
+import Forum from "./icons/forum.svg"
 import Help from "./icons/help.svg"
 import Info from "./icons/info.svg"
+import InsertComment from "./icons/insert_comment.svg"
 import ManageAccounts from "./icons/manage_accounts.svg"
 import OpenInBrowser from "./icons/open_in_browser.svg"
 import OpenInNew from "./icons/open_in_new.svg"
@@ -30,27 +33,33 @@ const getColoredSizedIcon = ({icon, color, size, className, ...props}) => {
 		const iconClass = `juno-icon juno-icon-${icon} fill-current ${color} ${className || ""}`
 	  switch (icon) {
 			case "autoAwesomeMosaic":
-				return <AutoAwesomeMosaic width={size} height={size} className={iconClass} alt="mosaic" role="img" {...props}/>
+				return <AutoAwesomeMosaic width={size} height={size} className={iconClass} alt="mosaic" role="img" {...props} />
 			case "autoAwesomeMotion":
-				return <AutoAwesomeMotion width={size} height={size} className={iconClass} alt="items stacked behind each other" role="img" {...props}/>
+				return <AutoAwesomeMotion width={size} height={size} className={iconClass} alt="items stacked behind each other" role="img" {...props} />
 			case "cancel":
-				return <Cancel width={size} height={size} className={iconClass} alt="cancel" role="img" {...props}/>
+				return <Cancel width={size} height={size} className={iconClass} alt="cancel" role="img" {...props} />
 			case "close":
-				return <Close width={size} height={size} className={iconClass} alt="close" role="img" {...props}/>
+				return <Close width={size} height={size} className={iconClass} alt="close" role="img" {...props} />
 			case "danger":
-				return <Danger width={size} height={size} className={iconClass} alt="danger" role="img" {...props}/ >
+				return <Danger width={size} height={size} className={iconClass} alt="danger" role="img" {...props} />
 			case "dangerous":
 				return <Dangerous width={size} height={size} className={iconClass} alt="dangerous" role="img" {...props} />
+			case "description":
+				return <Description width={size} height={size} className={iconClass} alt="description" role="img" {...props} />
 			case "error":
 				return <Error width={size} height={size} className={iconClass} alt="error" role="img" {...props} />
 			case "expandLess":
 				return <ExpandLess width={size} height={size} className={iconClass} alt="expand less" role="img" {...props} />
 			case "expandMore":
 				return <ExpandMore width={size} height={size} className={iconClass} alt="expand more" role="img" {...props} />
+			case "forum":
+				return <Forum width={size} height={size} className={iconClass} alt="forum" role="img" {...props} />
 			case "help":
 				return <Help width={size} height={size} className={iconClass} alt="help" role="img" {...props} />
 			case "info":
 				return <Info width={size} height={size} className={iconClass} alt="info" role="img" {...props} />
+			case "insertComment":
+				return <InsertComment width={size} height={size} className={iconClass} alt="insert comment" role="img" {...props} />
 			case "manageAccounts":
 				return <ManageAccounts width={size} height={size} className={iconClass} alt="user account configuration" role="img" {...props} />
 			case "openInBrowser":
@@ -89,10 +98,13 @@ Icon.propTypes = {
 			"cancel", 
 			"close", 
 			"dangerous",
+			"description",
 			"error", 
 			"expandLess", 
 			"expandMore", 
+			"forum", 
 			"help", 
+			"insertComment", 
 			"info", 
 			"manageAccounts", 
 			"openInBrowser", 
