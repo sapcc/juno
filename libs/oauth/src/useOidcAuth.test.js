b6d19bcc3a2f09cbeea1cf7e29fb7a3a2dd7f79e
Andreas Pfau <andreas.pfau@sap.com>
2022-02-24 14:13:49 +0100

    [oauth]: do not throw errors. Instead return error objects (#121)

--- libs/oauth/src/useOidcAuth.test.js
+++ libs/oauth/src/useOidcAuth.test.js
@@ -6,27 +6,35 @@ import { renderHook } from "@testing-library/react-hooks"
 import useOidcAuth from "./useOidcAuth"
 
 describe("useOidcAuth", () => {
-  it("throws an error if params are missing", () => {
-    expect(() => {
-      const { result } = renderHook(() => useOidcAuth())
-      // read error to run this hook
-      const error = result.current.error
-    }).toThrow("Cannot read property 'clientID' of undefined")
+  it("returns an error if params are missing", () => {
+    const { result } = renderHook(() => useOidcAuth())
+    // read error to run this hook
+    expect(result.current.error).toEqual(
+      "clientID or issuerURL are undefined. Please provide a clientID and issuerURL."
+    )
   })
 
-  it("throws an error if clientID is missing", () => {
-    expect(() => {
-      const { result } = renderHook(() => useOidcAuth({}))
-      // read error to run this hook
-      const error = result.current.error
-    }).toThrow("clientID is undefined. Please provide a clientID.")
+  it("returns an error if clientID is missing", () => {
+    const { result } = renderHook(() => useOidcAuth({ issuerURL: "TEST" }))
+    // read error to run this hook
+    expect(result.current.error).toEqual(
+      "clientID or issuerURL are undefined. Please provide a clientID and issuerURL."
+    )
   })
 
-  it("throws an error if issuerURL is missing", () => {
-    expect(() => {
-      const { result } = renderHook(() => useOidcAuth({ clientID: "test" }))
-      // read error to run this hook
-      const error = result.current.error
-    }).toThrow("issuerURL is undefined. Please provide a issuerURL.")
+  it("returns an error if issuerURL is missing", () => {
+    const { result } = renderHook(() => useOidcAuth({ clientID: "TEST" }))
+    // read error to run this hook
+    expect(result.current.error).toEqual(
+      "clientID or issuerURL are undefined. Please provide a clientID and issuerURL."
+    )
+  })
+
+  it("returns an error if issuerURL is missing", () => {
+    const { result } = renderHook(() =>
+      useOidcAuth({ clientID: "TEST", issuerURL: "TEST" })
+    )
+    // read error to run this hook
+    expect(result.current.error).toBeNull
   })
 })
