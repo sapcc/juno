f8d72c206df2dd461fe51bb1310247087eef0b52
d038721 <esther.schmitz@sap.com>
2022-02-28 16:18:39 +0100

    [template] set default panel state to closed

--- apps/template/src/store.js
+++ apps/template/src/store.js
@@ -5,7 +5,7 @@ import {devtools} from "zustand/middleware"
 
 // global zustand store. See how this works here: https://github.com/pmndrs/zustand
 const useStore = create(devtools((set) => ({
-  newItemFormOpened:  true, // this is the state
+  newItemFormOpened:  false, // this is the state
   openNewItemForm:    () => set((state) => ({newItemFormOpened: true})),  // this is a reducer to change state
   closeNewItemForm:   () => set((state) => ({newItemFormOpened: false})), // this is a reducer to change state
 })))
