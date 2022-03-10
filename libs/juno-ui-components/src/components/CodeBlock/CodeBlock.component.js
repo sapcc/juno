b00b9c27c196e8759c9b28f55e582f8f100ae9ca
Franz Heidl <franz@franzheidl.de>
2022-03-10 10:41:01 +0100

    [ui] CodeBlock: show and hide visual Copy feedback

--- libs/juno-ui-components/src/components/CodeBlock/CodeBlock.component.js
+++ libs/juno-ui-components/src/components/CodeBlock/CodeBlock.component.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import PropTypes from "prop-types"
 import { Icon } from "../Icon/Icon.component.js"
 
@@ -26,12 +26,20 @@ export const CodeBlock = ({
 	...props
 }) => {
 	
+	const [isCopied, setIsCopied] = useState(false)
+	
+	
 	const copyTextToClipboard = () => {
 		navigator.clipboard.writeText(children)
+		setIsCopied(true)
+		// Start timer for visible feedback:
+		const timer = setTimeout( () => setIsCopied(false), 2000 )
+		return () => clearTimeout(timer)
 	}
 	
 	const copy = (
 		<div className={`flex justify-end px-4 pb-4`}>
+			<span className={`font-bold text-sm mr-4 mt-1`} >{ isCopied ? "Copied!" : "" }</span>
 			<Icon icon="contentCopy" onClick={copyTextToClipboard} />
 		</div>
 	)
