4207d87db24bd4166bc60c73d761f5d7ef4643f6
d063222 <a.reuschenbach.puncernau@sap.com>
2022-03-21 14:44:45 +0100

    add posibility to load env variables defined in env.local

--- apps/template/src/index.js
+++ apps/template/src/index.js
@@ -1,3 +1,5 @@
 Promise.all([import("./bootstrap")]).then(([app]) =>
-  app.init(document.getElementById("root"), {})
+  app.init(document.getElementById("root"), {
+    endpoint: process.env.ENDPOINT,
+  })
 )
