c5a006e8a2c7481e0c5f534b34b16f18a03eb80d
d063222 <a.reuschenbach.puncernau@sap.com>
2022-04-12 13:27:23 +0200

    [volta] dismissible messages

--- apps/volta/src/components/Messages.js
+++ apps/volta/src/components/Messages.js
@@ -1,15 +1,38 @@
 import React from "react"
-import { Message, Icon, Stack } from "juno-ui-components"
-import { useMessagesState } from "./MessagesProvider"
+import { Message } from "juno-ui-components"
+import { useMessagesState, useMessagesDispatch } from "./MessagesProvider"
+
+// possible variants are
+// 'info', 'success', 'warning', 'danger', 'error'
+
+const shouldAutoDismiss = (variant) => {
+  if (variant === "info" || variant === "success") return true
+  return false
+}
 
 const Messages = () => {
   const messagesState = useMessagesState()
+  const dispatchMessage = useMessagesDispatch()
+
+  const onDismiss = (id) => {
+    dispatchMessage({
+      type: "REMOVE_MESSAGE",
+      id: id,
+    })
+  }
+
   return (
     <>
       {messagesState?.items && (
         <>
           {messagesState?.items.map((item) => (
-            <Message key={item.id} variant={item.variant}>
+            <Message
+              key={item.id}
+              variant={item.variant}
+              dismissible={true}
+              autoDismiss={shouldAutoDismiss(item.variant)}
+              onDismiss={() => onDismiss(item.id)}
+            >
               {item.text}
             </Message>
           ))}
