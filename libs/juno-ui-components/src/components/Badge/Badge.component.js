6c11dbcdb1306654f3fb964d0fa49406a9f0ca4f
Franz Heidl <franz@franzheidl.de>
2022-04-28 11:40:03 +0200

    [ui] Badge: fix PropTypes, import icon WIP

--- libs/juno-ui-components/src/components/Badge/Badge.component.js
+++ libs/juno-ui-components/src/components/Badge/Badge.component.js
@@ -1,5 +1,6 @@
 import React from "react"
 import PropTypes from "prop-types"
+import { Icon } from "../Icon/index.js"
 
 const badgeBaseStyles = `
 `
@@ -44,8 +45,21 @@ export const Badge = ({
 	children,
 	...props
 }) => {
+	const getIcon = (icon, variant) => {
+		if (icon) {
+			// TODO:
+			// if icon is an available icon, return as passed:
+			// otherwise return icon as per variant:
+			// otherwise return default icon:
+			// return <Icon icon={variant} />
+			return "[icon] "
+		} else {
+			return null
+		}
+	}
 	return (
-		<span className={`juno-badge juno-badge-${variant} ${badgeBaseStyles}`} {...props} >
+		<span className={`juno-badge juno-badge-${variant} ${badgeBaseStyles} ${className}`} {...props} >
+			{icon ? getIcon(icon, variant) : null }
 			{children ? children : text}
 		</span>
 	)
@@ -53,7 +67,7 @@ export const Badge = ({
 
 Badge.propTypes = {
 	variant: PropTypes.oneOf(["default", "info", "success", "warning", "danger", "error"]),
-	icon: PropTypes.oneOf([PropTypes.bool, PropTypes.oneOf(["warning", "danger"])]),
+	icon: PropTypes.oneOfType([PropTypes.bool, PropTypes.oneOf(["info", "success", "warning", "danger", "error"])]),  // TODO: refactor to use const of existing icons exported from icon component
 	text: PropTypes.string,
 	className: PropTypes.string,
 	children: PropTypes.node,
@@ -63,6 +77,6 @@ Badge.defaultProps = {
 	variant: "default",
 	icon: false,
 	text: "",
-	classname: "",
+	className: "",
 	children: null,
 }
