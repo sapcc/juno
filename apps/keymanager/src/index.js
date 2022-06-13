a6fad4760741f481ec5d9bcfb8c72ab260e94162
D038721 <esther.schmitz@sap.com>
2022-06-13 17:10:11 +0200

    [keymanager] add 'embedded' option to env and pass it on to the correct component

--- apps/keymanager/src/index.js
+++ apps/keymanager/src/index.js
@@ -1,6 +1,7 @@
 Promise.all([import("./bootstrap")]).then(([app]) =>
   app.init(document.getElementById("root"), {
     endpoint: process.env.ENDPOINT,
-    theme: process.env.THEME
+    theme: process.env.THEME,
+    embedded: process.env.EMBEDDED
   })
 )
