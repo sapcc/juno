f9c3ffbb45d59df0da518064ce5b63175c35060c
d063222 <a.reuschenbach.puncernau@sap.com>
2022-06-22 13:24:01 +0200

    [volta]: added tests to the helper

--- apps/volta/src/helpers.test.js
+++ apps/volta/src/helpers.test.js
@@ -1,3 +1,21 @@
-import { isExpired } from "./helpers"
+import { parseError } from "./helpers"
 
-describe("Helpers", () => {})
+describe("Helpers", () => {
+  describe("parseError", () => {
+    test("return error as string if no object with message", () => {
+      expect(parseError({ error: "This is an error text" })).toEqual(
+        '{"error":"This is an error text"}'
+      )
+    })
+    test("return error message if object with message attr exists", () => {
+      expect(parseError({ message: "This is an error text" })).toEqual(
+        "This is an error text"
+      )
+    })
+    test("return error message if object message has attr msg", () => {
+      expect(
+        parseError({ message: '{ "msg": "This is an error text" }' })
+      ).toEqual("This is an error text")
+    })
+  })
+})
