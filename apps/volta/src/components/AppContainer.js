dcac415ec31ab50c8be6ea83d9313ee6346e5717
D063222 <a.reuschenbach.puncernau@sap.com>
2022-06-29 14:32:12 +0200

    add ca to the url and read from ca from the url

--- apps/volta/src/components/AppContainer.js
+++ apps/volta/src/components/AppContainer.js
@@ -2,8 +2,9 @@ import React, { useState } from "react"
 import CertificateList from "./CertificateList"
 import NewCertificate from "./NewCertificate"
 import { getCAs } from "../queries"
-import { useGlobalState, useDispatch } from "./StateProvider"
+import { useGlobalState } from "./StateProvider"
 import { useMessagesDispatch } from "./MessagesProvider"
+import { useParams, useNavigate } from "react-router-dom"
 import {
   Spinner,
   Stack,
@@ -13,14 +14,14 @@ import {
   TabPanel,
 } from "juno-ui-components"
 
-const CA_NAME = "galvani-pki"
-
 const AppContainer = () => {
   const dispatchMessage = useMessagesDispatch()
   const auth = useGlobalState().auth
   const endpoint = useGlobalState().globals.endpoint
   const showNewSSO = useGlobalState().globals.showNewSSO
   const [tabIndex, setTabIndex] = useState(0)
+  let params = useParams()
+  let navigate = useNavigate()
 
   // fetch the certificates
   const { isLoading, isError, data, error } = getCAs(
@@ -28,10 +29,34 @@ const AppContainer = () => {
     endpoint
   )
 
+  const updateUrlParam = React.useCallback(
+    (index) => {
+      // fetch the tab name
+      if (data && data.length > 0 && data[index]?.name) {
+        return navigate(`/${data[index]?.name}`)
+      }
+      navigate(`/`)
+    },
+    [data]
+  )
+
+  // read current url state and call main fetch method if state is presented
+  React.useEffect(() => {
+    if (data && data.length > 0 && params && params.ca) {
+      // update tab from url
+      const index = data.findIndex((e) => e.name == params.ca)
+      if (index >= 0) {
+        setTabIndex(index)
+      }
+    }
+  }, [data, params])
+
   const onSelectTab = (index) => {
     // control of the Tabs state and behaviour.
     if (!showNewSSO) {
+      // update URL state
       setTabIndex(index)
+      updateUrlParam(index)
     }
     // on change tab remove messages
     dispatchMessage({
