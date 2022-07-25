da2b08bceb69e031281a3803481491c878e36b94
D038721 <esther.schmitz@sap.com>
2022-07-25 13:41:23 +0200

    [ui] optional search input for DataGridToolbar. Fix search input test for Filters.

--- libs/juno-ui-components/src/components/DataGridToolbar/DataGridToolbar.test.js
+++ libs/juno-ui-components/src/components/DataGridToolbar/DataGridToolbar.test.js
@@ -1,6 +1,7 @@
 import * as React from "react"
 import { render, screen} from "@testing-library/react"
 import { DataGridToolbar } from "./index"
+import { SearchInput } from "../SearchInput/index"
 
 describe("DataGridToolbar", () => {
 	
@@ -8,11 +9,22 @@ describe("DataGridToolbar", () => {
 		render(<DataGridToolbar data-testid="my-datagridtoolbar" />)
 		expect(screen.getByTestId("my-datagridtoolbar")).toBeInTheDocument()
 	})
+
+	test("renders a SearchInput as passed", async () => {
+		render(<DataGridToolbar search={<SearchInput/>}></DataGridToolbar>)
+		expect(screen.getByRole("searchbox")).toBeInTheDocument()
+	})
 	
 	test("renders a custom className", async () => {
 		render(<DataGridToolbar data-testid="my-datagridtoolbar" className="my-custom-class" />)
 		expect(screen.getByTestId("my-datagridtoolbar")).toBeInTheDocument()
 		expect(screen.getByTestId("my-datagridtoolbar")).toHaveClass("my-custom-class")
+	})
+
+	test("renders all props as passed", async () => {
+		render(<DataGridToolbar data-testid="23" data-lolol={true}/>)
+		expect(screen.getByTestId("23")).toBeInTheDocument()
+		expect(screen.getByTestId("23")).toHaveAttribute('data-lolol')
 	})
 	
 })
