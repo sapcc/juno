bc2466c88a74023634b8041a4ec16b1facb2cb84
Franz Heidl <franz@franzheidl.de>
2022-08-03 13:07:09 +0200

    [ui] LoadingIndicator: add color docstring, fix default size
    * move component logic to jsx
    * add `progressbar` role
    * add color
    * add docstrings

--- libs/juno-ui-components/src/components/LoadingIndicator/LoadingIndicator.component.js
+++ libs/juno-ui-components/src/components/LoadingIndicator/LoadingIndicator.component.js
@@ -5,6 +5,7 @@ import Loading from "./loading-indicator.svg"
 
 export const LoadingIndicator = ({
 	size,
+	color,
 	className,
 	...props
 }) => {
@@ -12,18 +13,24 @@ export const LoadingIndicator = ({
 		<Loading 
 			width={size}
 			height={size}
-			className={`${className}`}
+			className={`${className} ${color}`}
+			role="progressbar"
 		/>
 	)
 }
 
 LoadingIndicator.propTypes = {
+	/** The size of the LoadingIndicator as a number of pixels (without "px": "100" will render an LoadingIndicator of 100px x 100px). default is 96. */
 	size: PropTypes.string,
+	/** By default, the LoadingIndicator will use the `color` of the current context. In order to use a different color just for the LoadingIndicator, a text color class can be passed. These begin with "jn-text-". You can pass any other class that contains a "color:" declaration as well.*/
+	color: PropTypes.string,
+	/** Pass a custom className */
 	className: PropTypes.string,
 }
 
 LoadingIndicator.defaultProps = {
-	size: "24",
+	size: "96",
+	color: "",
 	className: "",
 }
 
