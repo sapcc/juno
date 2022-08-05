10e2edace7aa0d2870cd03b6b2a3e812ce6cc19f
Franz Heidl <franz@franzheidl.de>
2022-08-05 10:04:55 +0200

    [ui]ContextMenu: add isOpen state

--- libs/juno-ui-components/src/components/ContextMenu/ContextMenu.component.js
+++ libs/juno-ui-components/src/components/ContextMenu/ContextMenu.component.js
@@ -1,19 +1,45 @@
-import React from "react"
+import React, { useState } from "react"
 import PropTypes from "prop-types"
 import { Icon } from "../Icon/index.js"
+import { Portal } from "../Portal/index.js"
+
+/*
+TODO:
+* close on [ESC]
+* build generic Menu component and use it?
+* comfortably find tragetNode for Portal (should be in StyleProvider?)
+* a11y
+* docstrings
+* Rename Portal API: "InPortal" -> "Portal"
+*/
 
 export const ContextMenu = ({
 	icon,
 	className,
 	children,
+	open,
 	...props
 }) => {
+	const [isOpen, setIsOpen] = useState(false)
+	
+	const toggleOpen = () => {
+		setIsOpen(!isOpen)
+	}
+	
 	return (
 		<>
-		<Icon />
-		<div>
-			{ children }
-		</div>
+			<Icon className={`juno-contextmenu-toggle`} onClick={toggleOpen} />
+			{ isOpen ?
+				/* <Portal targetNode={document.getElementById("root")} >*/
+				
+					<div className={`juno-contextmenu-menu`} role="menu">
+						{ children }
+					</div>
+					
+				/* </Portal> */
+			:
+				null	
+			}
 		</>
 	)
 }
@@ -21,9 +47,11 @@ export const ContextMenu = ({
 ContextMenu.propTypes = {
 	className: PropTypes.string,
 	children: PropTypes.node,
+	open: PropTypes.bool,
 }
 
 ContextMenu.defaultProps = {
 	className: "",
 	children: null,
+	open: false,
 }
