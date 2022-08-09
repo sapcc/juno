eba019690131efdb539823c3a7678599ec346f02
Franz Heidl <franz@franzheidl.de>
2022-08-09 16:26:41 +0200

    [ui] Menu: add some tests

--- libs/juno-ui-components/src/components/Menu/Menu.test.js
+++ libs/juno-ui-components/src/components/Menu/Menu.test.js
@@ -1,12 +1,36 @@
 import * as React from "react"
 import { render, screen } from "@testing-library/react"
 import { Menu } from "./index"
+import { MenuItem } from "../MenuItem/index.js"
 
 describe("Menu", () => {
 	
   test("renders a Menu", async () => {
-	render(<Menu />)
-	expect(screen.getByRole("menu")).toBeInTheDocument()
+	  render(<Menu />)
+	  expect(screen.getByRole("menu")).toBeInTheDocument()
+  })
+  
+  test("renders a Menu with Children as passed", async () => {
+    render(
+      <Menu>
+        <MenuItem label="Menu Item 1" href="#" />
+      </Menu>
+    )
+    expect(screen.getByRole("menuitem")).toBeInTheDocument()
+    expect(screen.getByRole("menuitem")).toHaveTextContent("Menu Item 1")
+    expect(screen.getByRole("menuitem")).toHaveAttribute("href", "#")
+  })
+  
+  test("renders a className as passed", async () => {
+    render(<Menu className="my-class" />)
+    expect(screen.getByRole("menu")).toBeInTheDocument()
+    expect(screen.getByRole("menu")).toHaveClass("my-class")
+  })
+  
+  test("renders props as passed", async () => {
+    render(<Menu data-lolol="1 2 3"/>)
+    expect(screen.getByRole("menu")).toBeInTheDocument()
+    expect(screen.getByRole("menu")).toHaveAttribute("data-lolol", "1 2 3")
   })
   
 })
