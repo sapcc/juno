eb652b308461846ecfee29879424ed68726afb2d
D063222 <a.reuschenbach.puncernau@sap.com>
2022-08-24 15:40:48 +0200

    [heureka] vulnerabillites overview updated to new design

--- apps/heureka/src/components/VulnerabilitiesOverview.js
+++ apps/heureka/src/components/VulnerabilitiesOverview.js
@@ -1,41 +1,57 @@
 import React from "react"
-import { Icon } from "juno-ui-components"
+import { Icon, Badge } from "juno-ui-components"
 
-const vulnerabilityCss = `
-  mr-4
+const badgeCss = `
+  mr-2
 `
 
-// Pass the vulnerabilities allready classified. Use of the helper method `classifyVulnerabilities`
+const badgeContainerCss = `
+  flex
+  items-center
+  min-w-[2.3rem]
+`
+
+const vulnerabilityText = `
+  text-center
+  w-full
+`
+
 const VulnerabilitiesOverview = ({ vulnerabilities }) => {
   return (
-    <>
-      <div className={vulnerabilityCss}>
-        <Icon className="mr-1" color="text-theme-success" icon="severityLow" />
-        <span>{vulnerabilities.low}</span>
-      </div>
-      <div className={vulnerabilityCss}>
-        <div>
-          <Icon
-            className="mr-1"
-            color="text-theme-warning"
-            icon="severityMedium"
-          />
-          <span>{vulnerabilities.medium}</span>
+    <div className="flex">
+      <Badge className={badgeCss}>
+        <div className={badgeContainerCss}>
+          <div className="flex">
+            <Icon className="mr-1" size="16" icon="severityLow" />
+          </div>
+          <span className={vulnerabilityText}>{vulnerabilities.low}</span>
         </div>
-      </div>
-      <div className={vulnerabilityCss}>
-        <Icon className="mr-1" color="text-theme-danger" icon="severityHigh" />
-        <span>{vulnerabilities.high}</span>
-      </div>
-      <div>
-        <Icon
-          className="mr-1"
-          color="text-theme-danger"
-          icon="severityCritical"
-        />
-        <span>{vulnerabilities.critical}</span>
-      </div>
-    </>
+      </Badge>
+      <Badge variant="warning" className={badgeCss}>
+        <div className={badgeContainerCss}>
+          <div className="flex">
+            <Icon className="mr-1" size="16" icon="severityMedium" />
+          </div>
+          <span className={vulnerabilityText}>{vulnerabilities.medium}</span>
+        </div>
+      </Badge>
+      <Badge variant="danger" className={badgeCss}>
+        <div className={badgeContainerCss}>
+          <div className="flex">
+            <Icon className="mr-1" size="16" icon="severityHigh" />
+          </div>
+          <span className={vulnerabilityText}>{vulnerabilities.high}</span>
+        </div>
+      </Badge>
+      <Badge variant="critical" className={badgeCss}>
+        <div className={badgeContainerCss}>
+          <div className="flex">
+            <Icon className="mr-1" size="16" icon="severityCritical" />
+          </div>
+          <span className={vulnerabilityText}>{vulnerabilities.critical}</span>
+        </div>
+      </Badge>
+    </div>
   )
 }
 
