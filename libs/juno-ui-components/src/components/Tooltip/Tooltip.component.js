a7c0b7352ea2d4c2e5944fbeabdc6b7929594c1e
Franz Heidl <franz@franzheidl.de>
2022-08-24 14:33:14 +0200

    Tooltip design update (#193)
    * [ui] Tooltip: update styles and implementation
    
    * update styles/design as to current design in Figma
    * render children or text (add `children` prop)
    * add semantic variants and respective stories
    * keep non-semantic variant as a default
    * add `open` prop so the Tooltip can be programmatically opened from outside the component
    * resolve local/nested component
    
    * [ui] Tooltip: add tests for semantic variants
    
    * [ui] Tooltip: remove obsolete comment
    
    * [ui] Tooltip: accept and execute onClick handler
    
    * [ui] Tooltip: remove obsolete css variables
    
    * [ui] Tooltip: rmeove obsolete margin on popover

--- libs/juno-ui-components/src/components/Tooltip/Tooltip.component.js
+++ libs/juno-ui-components/src/components/Tooltip/Tooltip.component.js
@@ -4,74 +4,103 @@ import PropTypes from "prop-types"
 import { Icon } from "../Icon/index.js"
 
 /* Styles */
-
-const popover = `
+const popoverStyles = `
+	jn-bg-theme-background-lvl-1
+	jn-text-theme-high 
+	jn-inline-flex	
+	jn-items-start
 	jn-p-2
 	jn-rounded
-	jn-text-xs
-	jn-inline-block
-	jn-absolute
-	jn-bg-theme-tooltip-popover
-	jn-text-theme-tooltip-popover
 `
 
-/* Create tooltip popover with content as passed */
-const TooltipPopover = ({
-	text,
-	isOpen
-}) => {
-	if (!isOpen) return null 
-	return (
-		<div className={`juno-tooltip-popover ${popover}`}>
-			<p>{text}</p>
-		</div>
-	)
+const popoverTextStyles = `
+	jn-mx-4
+	jn-max-w-full
+`
+
+const popoverIconStyles = `
+	jn-shrink-0
+`
+
+const getIcon = (variant) => {
+	switch (variant) {
+		case "error":
+			return "dangerous"
+		default:
+			return variant
+	}
 }
 
-/* Portal to render Tooltip contents. Could be replaced with a generic portal component should we make one */
-const InPortal = ({
-	children
-}) => {
-	return ( ReactDOM.createPortal( children, document.body ))
-}
-
-/** 
-An all purpose Tooltip component. 
-TODO: Use InPortal component.
-TODO: Can render an icon or will be triggered by any children wrapped inside / passed to it.
-*/
 export const Tooltip = ({
-	position,
+	variant,
+	children,
 	text,
 	className,
 	disabled,
+	open,
+	onClick,
 	...props
 }) => {
-	const [open, setOpen] = useState(false)
+	const [isOpen, setIsOpen] = useState(false)
+	
+	React.useEffect(() => {
+		setIsOpen(open)
+	}, [open])
+	
+	const handleClick = (event) => {
+		setIsOpen(!isOpen)
+		onClick && onClick(event)
+	}
+	
 	return (		
-		<span className={`tooltip`} {...props}>
-			<Icon onClick={() => setOpen(!open)} className={className} disabled={disabled}/>
-			<InPortal>
-				<TooltipPopover text={text} isOpen={open} />
-			</InPortal>
+		<span className={`juno-tooltip`} {...props}>
+			<Icon 
+				onClick={handleClick} 
+				className={`${className}`} 
+				disabled={disabled}
+			/>
+			{ isOpen ?
+				<div className={`juno-tooltip-popover juno-tooltip-popover-${variant} ${popoverStyles}`}>
+					{ variant ? 
+						<Icon 
+							icon={getIcon(variant)}
+							color={"jn-text-theme-" + variant}
+							className={`juno-tooltip-popover-icon ${popoverIconStyles}`}
+						/>
+					:
+						null
+					}
+					<p className={`${popoverTextStyles}`}>
+						{ children || text }
+					</p>
+				</div>
+			:
+				null
+			}
 		</span>
 	)
 }
 
 Tooltip.propTypes = {
-	/** TODO: Position of the tooltip relative to the trigger */
-	position: PropTypes.oneOf(["top", "right", "bottom", "left"]),
+	/** The semantic variant of the tooltip, or `plain` */
+	variant: PropTypes.oneOf(["info", "warning", "danger", "error", "success"]),
+	/** Pass child nodes to display in the tooltip */
+	children: PropTypes.node,
 	/** Text to display in the tooltip */
-	text: PropTypes.string.isRequired,
+	text: PropTypes.node,
 	/** Pass a className to render to the icon button*/
 	className: PropTypes.string,
 	/** Disable the tooltip */
 	disabled: PropTypes.bool,
+	/** Whether the Tooltip is open */
+	open: PropTypes.bool,
 }
 
 Tooltip.defaultProps = {
-	position: "top",
-	text: null,
+	variant: null,
+	children: null,
+	text: "",
 	className: "",
 	disabled: null,
+	open: false,
 }
--- libs/juno-ui-components/src/components/Tooltip/Tooltip.stories.js
+++ libs/juno-ui-components/src/components/Tooltip/Tooltip.stories.js
@@ -14,6 +14,36 @@ Default.args = {
   text: "A default tooltip",
 }
 
+export const InfoTooltip = Template.bind({})
+InfoTooltip.args = {
+  variant: "info",
+  text: "An Info Tooltip"
+}
+
+export const WarningTooltip = Template.bind({})
+WarningTooltip.args = {
+  variant: "warning",
+  text: "A Warning Tooltip"
+}
+
+export const ErrorTooltip = Template.bind({})
+ErrorTooltip.args = {
+  variant: "error",
+  text: "An Error Tooltip"
+}
+
+export const DangerTooltip = Template.bind({});
+DangerTooltip.args = {
+  variant: 'danger',
+  text: 'A Danger Tooltip.',
+};
+
+export const SuccessTooltip = Template.bind({});
+SuccessTooltip.args = {
+  variant: 'success',
+  text: 'A Success Tooltip',
+};
+
 export const Disabled = Template.bind({})
 Disabled.args = {
   text: "A disabled tooltip",
