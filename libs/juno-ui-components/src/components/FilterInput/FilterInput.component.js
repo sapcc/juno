58127e145e45873f18d369d68a1cd1ce1a9ab1e3
Arturo Reuschenbach <a.reuschenbach.puncernau@sap.com>
2022-09-21 15:00:35 +0200

    [ui] onFilter handler returns input value (#204)

--- libs/juno-ui-components/src/components/FilterInput/FilterInput.component.js
+++ libs/juno-ui-components/src/components/FilterInput/FilterInput.component.js
@@ -39,155 +39,162 @@ const iconWrapperStyles = `
 A special Input to select key and value of a filter.
 */
 export const FilterInput = ({
-	keyLabel,
-	options,
-	valueLabel,
-	className,
-	selectedFilterKey,
-	onSelectedFilterKeyChange,
-	filterValue,
-	valuePlaceholder,
-	onFilterValueChange,
-	onClear,
-	onKeyPress,
-	onFilter,
-	loading,
-	...props
+  keyLabel,
+  options,
+  valueLabel,
+  className,
+  selectedFilterKey,
+  onSelectedFilterKeyChange,
+  filterValue,
+  valuePlaceholder,
+  onFilterValueChange,
+  onClear,
+  onKeyPress,
+  onFilter,
+  loading,
+  ...props
 }) => {
-	
-	const [selectedFilter, setSelectedFilter] = useState(selectedFilterKey)
-	const [value, setValue] = useState(filterValue)
-	const [isLoading, setIsLoading] = useState( options.length < 1 || loading)
-	
-	useEffect(() => {
-		setValue(filterValue)
-	}, [filterValue])
-	
-	useEffect(() => {
-		setSelectedFilter(selectedFilterKey)
-	}, [selectedFilterKey])
-	
-	// Reset the (text input) value whenever the component is loading:
-	useEffect(() => {
-		if ( options.length < 1 || loading ) {
-			setIsLoading(true)
-			setValue("")
-		} else {
-			setIsLoading(false)
-		}
-	}, [options, loading])
-	
-	// Reset the (text input) value whenever the selected Filter key changes:
-	const handleSelectedFilterChange = (event) => {
-		setSelectedFilter(event.target.value)
-		setValue("")
-		onSelectedFilterKeyChange && onSelectedFilterKeyChange(event)
-	}
-	
-	const handleFilterValueChange = (event) => {
-		setValue(event.target.value)
-		onFilterValueChange && onFilterValueChange(event)
-	}
-	
-	const handleClearClick = (event) => {
-		setValue("")
-		onClear && onClear(event)
-	}
-	
-	const handleFilterClick = (event) => {
-		onFilter && onFilter(event)
-	}
-	
-	const handleKeyPress = (event) => {
-		if (event.key === "Enter" && onFilter) { 
-			onFilter && onFilter(event) 
-		}
-		onKeyPress && onKeyPress(event)
-	 }
-	
-	return (
-		<div className={`juno-filter-input ${wrapperStyles} ${className}`} {...props} >
-			<div>
-				<Select 
-					className={`juno-filter-input-select ${selectStyles}`} 
-					aria-label={keyLabel}
-					value={selectedFilter}
-					onChange={handleSelectedFilterChange}
-					loading={isLoading}
-				>
-					// First "Placeholder" option:
-					<SelectOption label={keyLabel || "Select Filter"} value="" />
-					// Options representing actual filter key values:
-					{options.map((option, i) => (
-						<SelectOption 
-							label={option.label} 
-							value={option.key}
-							key={`${i}`}
-							{...option}
-						/>))}
-				</Select>
-			</div>
-			<TextInput 
-				value={value} 
-				className={`${textInputStyles}`} 
-				aria-label={valueLabel} 
-				onChange={handleFilterValueChange}
-				onKeyPress={handleKeyPress}
-				disabled={isLoading}
-				placeholder={ isLoading ? "Loading Filter Options…" : valuePlaceholder }
-			/>
-			<div className={`${iconWrapperStyles}`}>
-				{ value && value.length ?
-					<Icon icon="close" title="Clear" size="18" className={`jn-mr-2`} onClick={handleClearClick} />
-					:
-					null
-				}
-				<Icon icon="filterAlt" title="Filter" onClick={handleFilterClick} />
-			</div>
-		</div>
-	)
+  const [selectedFilter, setSelectedFilter] = useState(selectedFilterKey)
+  const [value, setValue] = useState(filterValue)
+  const [isLoading, setIsLoading] = useState(options.length < 1 || loading)
+
+  useEffect(() => {
+    setValue(filterValue)
+  }, [filterValue])
+
+  useEffect(() => {
+    setSelectedFilter(selectedFilterKey)
+  }, [selectedFilterKey])
+
+  // Reset the (text input) value whenever the component is loading:
+  useEffect(() => {
+    if (options.length < 1 || loading) {
+      setIsLoading(true)
+      setValue("")
+    } else {
+      setIsLoading(false)
+    }
+  }, [options, loading])
+
+  // Reset the (text input) value whenever the selected Filter key changes:
+  const handleSelectedFilterChange = (event) => {
+    setSelectedFilter(event.target.value)
+    setValue("")
+    onSelectedFilterKeyChange && onSelectedFilterKeyChange(event)
+  }
+
+  const handleFilterValueChange = (event) => {
+    setValue(event.target.value)
+    onFilterValueChange && onFilterValueChange(event)
+  }
+
+  const handleClearClick = (event) => {
+    setValue("")
+    onClear && onClear(event)
+  }
+
+  const handleFilterClick = () => {
+    onFilter && onFilter(value)
+  }
+
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter" && onFilter) {
+      onFilter && onFilter(value)
+    }
+    onKeyPress && onKeyPress(event)
+  }
+
+  return (
+    <div
+      className={`juno-filter-input ${wrapperStyles} ${className}`}
+      {...props}
+    >
+      <div>
+        <Select
+          className={`juno-filter-input-select ${selectStyles}`}
+          aria-label={keyLabel}
+          value={selectedFilter}
+          onChange={handleSelectedFilterChange}
+          loading={isLoading}
+        >
+          // First "Placeholder" option:
+          <SelectOption label={keyLabel || "Select Filter"} value="" />
+          // Options representing actual filter key values:
+          {options.map((option, i) => (
+            <SelectOption
+              label={option.label}
+              value={option.key}
+              key={`${i}`}
+              {...option}
+            />
+          ))}
+        </Select>
+      </div>
+      <TextInput
+        value={value}
+        className={`${textInputStyles}`}
+        aria-label={valueLabel}
+        onChange={handleFilterValueChange}
+        onKeyPress={handleKeyPress}
+        disabled={isLoading}
+        placeholder={isLoading ? "Loading Filter Options…" : valuePlaceholder}
+      />
+      <div className={`${iconWrapperStyles}`}>
+        {value && value.length ? (
+          <Icon
+            icon="close"
+            title="Clear"
+            size="18"
+            className={`jn-mr-2`}
+            onClick={handleClearClick}
+          />
+        ) : null}
+        <Icon icon="filterAlt" title="Filter" onClick={handleFilterClick} />
+      </div>
+    </div>
+  )
 }
 
 FilterInput.propTypes = {
-	/** The label to display on the Filter Key Select */
-	keyLabel: PropTypes.string,
-	/** The options for the Filter Select: `[{Label: "Filter 1", key: "filter-1"}, {...}]`
+  /** The label to display on the Filter Key Select */
+  keyLabel: PropTypes.string,
+  /** The options for the Filter Select: `[{Label: "Filter 1", key: "filter-1"}, {...}]`
 	The array MUST have a length in order for the component to render.
 	*/
-	options: PropTypes.arrayOf(PropTypes.object), 
-	/** The key of the current filter */
-	selectedFilterKey: PropTypes.string,
-	/** Pass a handler to be executed when the filter key changes */
-	onSelectedFilterKeyChange: PropTypes.func,
-	/** The aria-label of the Filter Value Text Input */
-	valueLabel: PropTypes.string, // TODO -> valueLabel
-	/** The current value of the Filter Input */
-	filterValue: PropTypes.string,
-	/** Optional: pass a placeholder for the filter value text input */
-	valuePlaceholder: PropTypes.string,
-	/** Pass a handler to be executed when the filter value changes */
-	onFilterValueChange: PropTypes.func,
-	/** Pass a handler to execute when the Filter Value Clear button is clicked */
-	onClear: PropTypes.func,
-	/** Whether the filter is currently loading */
-	loading: PropTypes.bool,
-	/** Pass a className to the wrapping element */
-	className: PropTypes.string,
-	/** Pass a handler to execute when the Filter Value Filter button is clicked */
-	onFilter: PropTypes.func,
+  options: PropTypes.arrayOf(PropTypes.object),
+  /** The key of the current filter */
+  selectedFilterKey: PropTypes.string,
+  /** Pass a handler to be executed when the filter key changes */
+  onSelectedFilterKeyChange: PropTypes.func,
+  /** The aria-label of the Filter Value Text Input */
+  valueLabel: PropTypes.string, // TODO -> valueLabel
+  /** The current value of the Filter Input */
+  filterValue: PropTypes.string,
+  /** Optional: pass a placeholder for the filter value text input */
+  valuePlaceholder: PropTypes.string,
+  /** Pass a handler to be executed when the filter value changes */
+  onFilterValueChange: PropTypes.func,
+  /** Pass a handler to execute when the Filter Value Clear button is clicked */
+  onClear: PropTypes.func,
+  /** Whether the filter is currently loading */
+  loading: PropTypes.bool,
+  /** Pass a className to the wrapping element */
+  className: PropTypes.string,
+  /** Pass a handler to execute when the Filter Value Filter button is clicked */
+  onFilter: PropTypes.func,
 }
 
 FilterInput.defaultProps = {
-	keyLabel: "Select Filter",
-	options: [],
-	selectedFilterKey: "",
-	onSelectedFilterKeyChange: undefined,
-	valueLabel: "Filter by Value",
-	filterValue: "",
-	valuePlaceholder: "",
-	onFilterValueChange: undefined,
-	onClear: undefined,
-	onFilter: undefined,
-	loading: false,
-	className: "",
+  keyLabel: "Select Filter",
+  options: [],
+  selectedFilterKey: "",
+  onSelectedFilterKeyChange: undefined,
+  valueLabel: "Filter by Value",
+  filterValue: "",
+  valuePlaceholder: "",
+  onFilterValueChange: undefined,
+  onClear: undefined,
+  onFilter: undefined,
+  loading: false,
+  className: "",
 }
