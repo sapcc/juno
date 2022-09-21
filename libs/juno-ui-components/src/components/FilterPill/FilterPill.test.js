db1a7422273dde59965838190e2bbfd78190f545
D063222 <a.reuschenbach.puncernau@sap.com>
2022-09-21 12:33:00 +0200

    [ui] fix warning when testing pills

--- libs/juno-ui-components/src/components/FilterPill/FilterPill.test.js
+++ libs/juno-ui-components/src/components/FilterPill/FilterPill.test.js
@@ -4,46 +4,83 @@ import { FilterPill } from "./index"
 
 describe("FilterPill", () => {
   test("renders a FilterPill", async () => {
-    render(<FilterPill data-testid="my-filterpill" />)
+    render(
+      <FilterPill
+        data-testid="my-filterpill"
+        filterKey="TheRequiredKey"
+        filterValue="TheRequiredValue"
+      />
+    )
     expect(screen.getByTestId("my-filterpill")).toBeInTheDocument()
     expect(screen.getByTestId("my-filterpill")).toHaveClass("juno-filterpill")
   })
 
   test("renders a filter key label as passed", async () => {
-    render(<FilterPill filterKeyLabel="My FilterPill Key" />)
+    render(
+      <FilterPill
+        filterKeyLabel="My FilterPill Key"
+        filterKey="TheRequiredKey"
+        filterValue="TheRequiredValue"
+      />
+    )
     expect(screen.getByText("My FilterPill Key")).toBeInTheDocument()
   })
 
   test("renders a filter key if filter key label missing", async () => {
-    render(<FilterPill filterKey="my_filterPill_key" />)
+    render(
+      <FilterPill
+        filterKey="my_filterPill_key"
+        filterValue="TheRequiredValue"
+      />
+    )
     expect(screen.getByText("my_filterPill_key")).toBeInTheDocument()
   })
 
   test("renders nothing if filter key label not set", async () => {
-    render(<FilterPill data-testid="23" />)
+    // the attribute filterKey and filterValue are required keep them empty to test render nothing
+    render(<FilterPill data-testid="23" filterKey="" filterValue="" />)
     expect(screen.getByTestId("23")).toBeInTheDocument()
     expect(screen.getByTestId("23")).toHaveTextContent("")
   })
 
   test("renders a filter value label as passed", async () => {
-    render(<FilterPill filterValueLabel="My FilterPill Value" />)
+    render(
+      <FilterPill
+        filterValue="TheRequiredValue"
+        filterValueLabel="My FilterPill Value"
+        filterKey="TheRequiredKey"
+      />
+    )
     expect(screen.getByText("My FilterPill Value")).toBeInTheDocument()
   })
 
-  test("renders a filter value key if value label missing", async () => {
-    render(<FilterPill filterValue="my_filterPill_value" />)
+  test("renders a filter value if value label missing", async () => {
+    render(
+      <FilterPill
+        filterKey="TheRequiredKey"
+        filterValue="my_filterPill_value"
+      />
+    )
     expect(screen.getByText("my_filterPill_value")).toBeInTheDocument()
   })
 
-  test("renders nothing if filter value or value label not set", async () => {
-    render(<FilterPill data-testid="23" />)
+  test("renders nothing if filter value or value label not given", async () => {
+    // the attribute filterKey and filterValue are required keep them empty to test render nothing
+    render(<FilterPill data-testid="23" filterKey="" filterValue="" />)
     expect(screen.getByTestId("23")).toBeInTheDocument()
     expect(screen.getByTestId("23")).toHaveTextContent("")
   })
 
   test("an onClose handler is called as passed and returns the uid", () => {
     const handleClose = jest.fn()
-    render(<FilterPill uid="uidAbc" onClose={handleClose} />)
+    render(
+      <FilterPill
+        uid="uidAbc"
+        filterKey="TheRequiredKey"
+        filterValue="TheRequiredValue"
+        onClose={handleClose}
+      />
+    )
     screen.getByRole("button").click()
     expect(handleClose).toHaveBeenCalledTimes(1)
     expect(handleClose).toHaveBeenCalledWith("uidAbc")
@@ -51,7 +88,13 @@ describe("FilterPill", () => {
 
   test("an onClose handler is called as passed and returns the filterKey if uid missing", () => {
     const handleClose = jest.fn()
-    render(<FilterPill filterKey="abc" onClose={handleClose} />)
+    render(
+      <FilterPill
+        filterKey="abc"
+        filterValue="TheRequiredValue"
+        onClose={handleClose}
+      />
+    )
     screen.getByRole("button").click()
     expect(handleClose).toHaveBeenCalledTimes(1)
     expect(handleClose).toHaveBeenCalledWith("abc")
@@ -59,14 +102,26 @@ describe("FilterPill", () => {
 
   test("renders a custom className", async () => {
     render(
-      <FilterPill data-testid="my-filterpill" className="my-custom-class" />
+      <FilterPill
+        data-testid="my-filterpill"
+        filterKey="TheRequiredKey"
+        filterValue="TheRequiredValue"
+        className="my-custom-class"
+      />
     )
     expect(screen.getByTestId("my-filterpill")).toBeInTheDocument()
     expect(screen.getByTestId("my-filterpill")).toHaveClass("my-custom-class")
   })
 
   test("renders all props as passed", async () => {
-    render(<FilterPill data-testid="23" data-lolol={true} />)
+    render(
+      <FilterPill
+        data-testid="23"
+        filterKey="TheRequiredKey"
+        filterValue="TheRequiredValue"
+        data-lolol={true}
+      />
+    )
     expect(screen.getByTestId("23")).toBeInTheDocument()
     expect(screen.getByTestId("23")).toHaveAttribute("data-lolol")
   })
