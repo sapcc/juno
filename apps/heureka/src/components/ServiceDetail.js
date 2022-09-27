0b01f0bd5445d9b90c9e102de42acbd6bc977328
D063222 <a.reuschenbach.puncernau@sap.com>
2022-09-27 13:41:23 +0200

    [heureka] fixed dead reference

--- apps/heureka/src/components/ServiceDetail.js
+++ apps/heureka/src/components/ServiceDetail.js
@@ -11,7 +11,6 @@ import {
   DataGridCell,
   Container,
 } from "juno-ui-components"
-import ServiceVulnerabilitiesList from "./ServiceVulnerabilitiesList"
 import {
   DetailSection,
   DetailSectionBox,
