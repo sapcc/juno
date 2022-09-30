e3e6f87ea76c1ee61a8f0c8c5c75cab639bddbbf
D038721 <esther.schmitz@sap.com>
2022-09-30 15:00:49 +0200

    [example] initial example app

---
+++ apps/exampleapp/src/index.js
@@ -0,0 +1,7 @@
+Promise.all([import("./bootstrap")]).then(([app]) =>
+  app.init(document.getElementById("root"), {
+    endpoint: process.env.ENDPOINT,
+    theme: process.env.THEME,
+    embedded: process.env.EMBEDDED
+  })
+)
