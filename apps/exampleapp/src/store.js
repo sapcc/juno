42f3d3e0ae519274c030fc5a772976eecfd83415
Franz Heidl <franz@franzheidl.de>
2022-10-18 11:53:13 +0200

    [example app] store: remove obsolete

--- apps/exampleapp/src/store.js
+++ apps/exampleapp/src/store.js
@@ -5,9 +5,6 @@ import {devtools} from "zustand/middleware"
 
 // global zustand store. See how this works here: https://github.com/pmndrs/zustand
 const useStore = create(devtools((set) => ({
-  newItemFormOpened:    false, // this is the state
-  openNewItemForm:      () => set((state) => ({newItemFormOpened: true})),  // this is a reducer to change state
-  closeNewItemForm:     () => set((state) => ({newItemFormOpened: false})), // this is a reducer to change state
   editItemPanelOpened:  false, // state of the edit panel
   openEditItemPanel:    () => set((state) => ({editItemPanelOpened: true})),
   closeEditItemPanel:   () => set((state) => ({editItemPanelOpened: false})),
