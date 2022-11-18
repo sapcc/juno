0ee0e6562f1f6d6344686b53f8d9799ab102d456
D063222 <a.reuschenbach.puncernau@sap.com>
2022-11-18 14:29:19 +0100

    [template]: Mock import with resource queries since it is not supported

--- apps/template/jest.config.js
+++ apps/template/jest.config.js
@@ -7,5 +7,11 @@ module.exports = {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
       require.resolve("./__mocks__/fileMock"),
     "\\.(css|less)$": require.resolve("./__mocks__/styleMock"),
+    // Mock import with resource queries since it is not supported
+    // https://github.com/facebook/jest/issues/4181
+    // To differentiate between svgs that are to be used as bg images and those that are to be loaded
+    // as components we have to add a query parameter `?url` to the images to be loaded as url for use in background images in jsx files
+    // see webpack.config.js for more information
+    "\\.(svg)(\\?.+)?$": require.resolve("./__mocks__/fileMock"),
   },
 }
