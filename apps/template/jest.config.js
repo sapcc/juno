f7dd767221b9bd4fcb0920816f9bcbd7bb0bf089
D063222 <a.reuschenbach.puncernau@sap.com>
2022-11-18 15:31:17 +0100

    merge resource with query params with the old expression. Extended all apps

--- apps/template/jest.config.js
+++ apps/template/jest.config.js
@@ -4,14 +4,11 @@ module.exports = {
   setupFilesAfterEnv: ["<rootDir>/setupTests.js"],
   transformIgnorePatterns: ["/node_modules/", "\\.pnp\\.[^\\/]+$"],
   moduleNameMapper: {
-    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+    // Jest currently doesn't support resources with query parameters.
+    // Therefore we add the optional query parameter matcher at the end
+    // https://github.com/facebook/jest/issues/4181
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)(\\?.+)?$":
       require.resolve("./__mocks__/fileMock"),
     "\\.(css|less)$": require.resolve("./__mocks__/styleMock"),
-    // Mock import with resource queries since it is not supported
-    // https://github.com/facebook/jest/issues/4181
-    // To differentiate between svgs that are to be used as bg images and those that are to be loaded
-    // as components we have to add a query parameter `?url` to the images to be loaded as url for use in background images in jsx files
-    // see webpack.config.js for more information
-    "\\.(svg)(\\?.+)?$": require.resolve("./__mocks__/fileMock"),
   },
 }
