98625c4b81cbb51df1f5dc3f9be506bcad7653c7
d064310 <andreas.pfau@sap.com>
2022-12-07 08:24:45 +0100

    switch widget loader to importmaps

--- apps/widget-loader/src/index.js
+++ apps/widget-loader/src/index.js
@@ -1,43 +1,6 @@
 window.__junoBootstrap =
   window.__junoBootstrap ||
   async function () {
-    // //return import("./apps/shell/index.js")
-    window.process = { env: { NODE_ENV: "production" } }
-    // window.esmsInitOptions = {
-    //   shimMode: true,
-    // }
-    // if (
-    //   !document.querySelector(
-    //     'script[src="https://ga.jspm.io/npm:es-module-shims@1.6.2/dist/es-module-shims.js"]'
-    //   )
-    // ) {
-    //   const shimScriptTag = document.createElement("script")
-    //   shimScriptTag.setAttribute("async", true)
-    //   shimScriptTag.setAttribute(
-    //     "src",
-    //     "https://ga.jspm.io/npm:es-module-shims@1.6.2/dist/es-module-shims.js"
-    //   )
-    //   document.head.append(shimScriptTag)
-    // }
-    // if (!document.querySelector("script[data-juno-importmap]")) {
-    //   const importmapScriptTag = await fetch(
-    //     "https://cdn.ap.ws2.eu-nl-1.cloud.sap/v2/importmap.json"
-    //   )
-    //     .then((r) => r.json())
-    //     .then((importmap) => {
-    //       const script = document.createElement("script")
-    //       script.setAttribute("type", "importmap-shim")
-    //       script.setAttribute("data-juno-importmap", true)
-    //       script.text = JSON.stringify(importmap)
-    //       return script
-    //     })
-    //   document.head.append(importmapScriptTag)
-    // }
-  }
-
-window.__junoWidgetLoader =
-  window.__junoWidgetLoader ||
-  async function () {
     let currentScript = document.currentScript
 
     // fall back for IE
@@ -46,21 +9,63 @@ window.__junoWidgetLoader =
       currentScript = scripts[scripts.length - 1]
     }
 
-    // TODO: fetch manifest from assets server to get the right path
+    const currentURL = new URL(
+      "https://cdn.ap.ws2.eu-nl-1.cloud.sap/apps/widget-loader@latest/build/app.js"
+    ) //new URL(currentScript.src)
+
+    // //return import("./apps/shell/index.js")
+    //https://cdn.ap.ws2.eu-nl-1.cloud.sap/apps/widget-loader@latest/build/app.js
+    window.process = { env: { NODE_ENV: "production" } }
+    window.esmsInitOptions = {
+      shimMode: true,
+    }
+
+    let loadShim = new Promise((resolve, reject) => {
+      const url =
+        "https://ga.jspm.io/npm:es-module-shims@1.6.2/dist/es-module-shims.js"
+      if (document.querySelector(`script[src="${url}"]`)) return resolve(true)
+
+      const shimScriptTag = document.createElement("script")
+      shimScriptTag.setAttribute("async", true)
+      shimScriptTag.setAttribute("src", url)
+      shimScriptTag.onload = (e) => resolve(true)
+      shimScriptTag.onerror = () => reject("Could not load es-module-shim")
+      document.head.append(shimScriptTag)
+    })
+
+    let loadImportmap = new Promise((resolve, reject) => {
+      if (document.querySelector("script[data-juno-importmap]"))
+        return resolve(true)
+
+      fetch(`${currentURL.origin}/importmap.json`)
+        .then((r) => r.json())
+        .then((importmap) => {
+          const script = document.createElement("script")
+          script.setAttribute("type", "importmap-shim")
+          script.setAttribute("data-juno-importmap", true)
+          script.text = JSON.stringify(importmap)
+          document.head.append(script)
+          resolve(true)
+        })
+        .catch((error) => reject(error))
+    })
+
+    return Promise.all([loadShim, loadImportmap]).then(() => currentScript)
+  }
+
+window.__junoWidgetLoader =
+  window.__junoWidgetLoader ||
+  async function (currentScript) {
     const currentURL = new URL(currentScript.src)
     // console.log("==================", currentURL)
     const { name, version, url, debug, ...props } = currentScript.dataset
-
-    const appURL = url
-      ? new URL(url)
-      : new URL(currentURL.origin + `/${name}/${version}/index.js`)
 
     if (debug) {
       console.log("===WIDGET LOADER")
       console.log("name:", name, "url:", url, "props: ", props)
     }
 
-    let appProps = { currentHost: appURL?.origin }
+    let appProps = { currentHost: url || currentURL.origin }
     for (let key in props) {
       if (key.indexOf("props") === 0)
         appProps[key.replace("props", "").toLowerCase()] = props[key]
@@ -68,15 +73,20 @@ window.__junoWidgetLoader =
 
     // import(appURL).then((app) => console.log("==APP", app))
     const appWrapper = document.createElement("div")
-    appWrapper.setAttribute("data-juno-app", name || appURL)
+    appWrapper.setAttribute("data-juno-app", name || url)
     appWrapper.setAttribute("style", "height: 100%;")
 
-    import(appURL.href + "?" + Date.now()) //importShim(appURL + "?" + Date.now())
+    const endpoint = url
+      ? url + "?" + Date.now()
+      : `@juno/${name}@${version || "latest"}`
+
+    importShim(endpoint)
       .then((app) => {
         const mount = app.mount || app.default
         mount(appWrapper, { props: appProps })
       })
       .catch((error) => console.warn(error.message))
+
     currentScript.replaceWith(appWrapper)
   }
 
