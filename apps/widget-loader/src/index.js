5ccb665e54f1e2851d19933dcc2b7c54d561a9a5
d064310 <andreas.pfau@sap.com>
2022-12-09 17:06:16 +0100

    improve widget loader

--- apps/widget-loader/src/index.js
+++ apps/widget-loader/src/index.js
@@ -1,88 +1,106 @@
-window.__junoBootstrap =
-  window.__junoBootstrap ||
-  async function () {
-    let currentScript = document.currentScript
+window.__junoWidgetLoader = { ...window.__junoWidgetLoader }
 
-    // fall back for IE
-    if (!currentScript) {
-      let scripts = document.getElementsByTagName("script")
-      currentScript = scripts[scripts.length - 1]
-    }
+let currentScript = document.currentScript
 
-    const currentURL = new URL(currentScript.src)
+// fall back for IE
+if (!currentScript) {
+  let scripts = document.getElementsByTagName("script")
+  currentScript = scripts[scripts.length - 1]
+}
 
-    window.process = { env: { NODE_ENV: "production" } }
-    window.esmsInitOptions = {
-      shimMode: true,
-    }
+const currentURL = new URL(currentScript.src)
+let { name, version, url, origin, importmapOnly, debug, ...props } =
+  currentScript.dataset
 
-    let loadShim = new Promise((resolve, reject) => {
-      const url =
-        "https://ga.jspm.io/npm:es-module-shims@1.6.2/dist/es-module-shims.js"
-      if (document.querySelector(`script[src="${url}"]`)) return resolve(true)
+origin = origin || currentURL.origin
 
-      const shimScriptTag = document.createElement("script")
-      shimScriptTag.setAttribute("async", true)
-      shimScriptTag.setAttribute("src", url)
-      shimScriptTag.onload = (e) => resolve(true)
-      shimScriptTag.onerror = () => reject("Could not load es-module-shim")
-      document.head.append(shimScriptTag)
-    })
+if (debug) {
+  console.log("===WIDGET LOADER")
+  console.log(
+    "name:",
+    name,
+    "url:",
+    url,
+    "props: ",
+    "origin: ",
+    origin,
+    "importmapOnly: ",
+    importmapOnly,
+    props
+  )
+}
 
-    let loadImportmap = new Promise((resolve, reject) => {
-      if (document.querySelector("script[data-juno-importmap]"))
-        return resolve(true)
+// load shim
+let loadShim = new Promise((resolve, reject) => {
+  const url =
+    "https://ga.jspm.io/npm:es-module-shims@1.6.2/dist/es-module-shims.js"
+  if (document.querySelector(`script[src="${url}"]`)) return resolve(true)
 
-      fetch(`${currentURL.origin}/importmap.json`)
-        .then((r) => r.json())
-        .then((importmap) => {
-          const script = document.createElement("script")
-          script.setAttribute("type", "importmap-shim")
-          script.setAttribute("data-juno-importmap", true)
-          script.text = JSON.stringify(importmap)
-          document.head.append(script)
-          resolve(true)
-        })
-        .catch((error) => reject(error))
-    })
-
-    return Promise.all([loadShim, loadImportmap]).then(() => currentScript)
+  window.process = { env: { NODE_ENV: "production" } }
+  window.esmsInitOptions = {
+    shimMode: true,
+    mapOverrides: true,
   }
 
-window.__junoWidgetLoader =
-  window.__junoWidgetLoader ||
-  async function (currentScript) {
-    const currentURL = new URL(currentScript.src)
-    // console.log("==================", currentURL)
-    const { name, version, url, debug, ...props } = currentScript.dataset
+  const shimScriptTag = document.createElement("script")
+  shimScriptTag.setAttribute("async", true)
+  shimScriptTag.setAttribute("src", url)
+  shimScriptTag.onload = (e) => resolve(true)
+  shimScriptTag.onerror = () => reject("Could not load es-module-shim")
+  document.head.append(shimScriptTag)
+})
 
-    if (debug) {
-      console.log("===WIDGET LOADER")
-      console.log("name:", name, "url:", url, "props: ", props)
-    }
+// load importmap
+let loadImportmap = new Promise((resolve, reject) => {
+  if (document.querySelector(`script[data-juno-importmap="${origin}"]`))
+    return resolve(true)
 
-    let appProps = { currentHost: url || currentURL.origin }
-    for (let key in props) {
-      if (key.indexOf("props") === 0)
-        appProps[key.replace("props", "").toLowerCase()] = props[key]
-    }
+  fetch(`${origin}/importmap.json`)
+    .then((r) => r.json())
+    .then((importmap) => {
+      const script = document.createElement("script")
+      script.setAttribute("type", "importmap-shim")
+      script.setAttribute("data-juno-importmap", origin)
+      script.text = JSON.stringify(importmap)
+      document.head.append(script)
+      resolve(true)
+    })
+    .catch((error) => reject(error))
+})
 
-    const appWrapper = document.createElement("div")
-    appWrapper.setAttribute("data-juno-app", name || url)
-    appWrapper.setAttribute("style", "height: 100%;")
+// load widget after importmap has been loaded
+Promise.all([loadShim, loadImportmap]).then(async () => {
+  if (importmapOnly === "true") return
 
-    const endpoint = url
-      ? url + "?" + Date.now()
-      : `@juno/${name}@${version || "latest"}`
-
-    importShim(endpoint)
-      .then((app) => {
-        const mount = app.mount || app.default
-        mount(appWrapper, { props: appProps })
-      })
-      .catch((error) => console.warn(error.message))
-
-    currentScript.replaceWith(appWrapper)
+  if (!url && !window.__junoWidgetLoader[origin]) {
+    await fetch(`${origin}/manifest.json`)
+      .then((res) => res.json())
+      .then(
+        (manifest) => (window.__junoWidgetLoader[origin].manifest = manifest)
+      )
   }
 
-window.__junoBootstrap().then(window.__junoWidgetLoader)
+  let appURL =
+    url ||
+    window.__junoWidgetLoader[origin].manifest[name][version || "latest"]
+      .entryFile
+
+  let appProps = { currentHost: url || origin }
+  for (let key in props) {
+    if (key.indexOf("props") === 0)
+      appProps[key.replace("props", "").toLowerCase()] = props[key]
+  }
+
+  const appWrapper = document.createElement("div")
+  appWrapper.setAttribute("data-juno-app", name || url)
+  appWrapper.setAttribute("style", "height: 100%;")
+
+  importShim(appURL + "?" + Date.now())
+    .then((app) => {
+      const mount = app.mount || app.default
+      mount(appWrapper, { props: appProps })
+    })
+    .catch((error) => console.warn(error.message))
+
+  currentScript.replaceWith(appWrapper)
+})
