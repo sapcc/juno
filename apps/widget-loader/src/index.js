b25b5bdc5967097c8b8f79bdbdc5031449f0ba83
d064310 <andreas.pfau@sap.com>
2022-12-12 09:39:05 +0100

    fix error in widget loader

--- apps/widget-loader/src/index.js
+++ apps/widget-loader/src/index.js
@@ -13,19 +13,23 @@ let { name, version, url, origin, importmapOnly, debug, ...props } =
   currentScript.dataset
 
 origin = origin || currentURL.origin
+version = version || "latest"
+debug = debug === "true"
 
 if (debug) {
-  console.log("===WIDGET LOADER")
   console.log(
+    "===WIDGET LOADER: ",
     "name:",
     name,
-    "url:",
+    ", version: ",
+    version,
+    ", url:",
     url,
-    "props: ",
-    "origin: ",
+    ", origin: ",
     origin,
-    "importmapOnly: ",
+    ", importmapOnly: ",
     importmapOnly,
+    ", props: ",
     props
   )
 }
@@ -69,36 +73,69 @@ let loadImportmap = new Promise((resolve, reject) => {
 })
 
 // load widget after importmap has been loaded
-Promise.all([loadShim, loadImportmap]).then(async () => {
-  if (importmapOnly === "true") return
+Promise.all([loadShim, loadImportmap])
+  .then(async () => {
+    // return here if only the importmap was requested
+    if (importmapOnly === "true") return
 
-  if (!url && !window.__junoWidgetLoader[origin]) {
-    await fetch(`${origin}/manifest.json`)
-      .then((res) => res.json())
-      .then((manifest) => (window.__junoWidgetLoader[origin] = { manifest }))
-  }
+    // het the app URL
+    // url can be given via data-url attribute on the widget script tag or
+    // if not the widget loader tries to load get it from the manifest.json
+    let appURL = url
+    if (!appURL) {
+      // window.__junoWidgetLoader[origin].manifest is the cache for the current
+      // widget loader
+      if (
+        !window.__junoWidgetLoader[origin] ||
+        !window.__junoWidgetLoader[origin].manifest
+      ) {
+        // load mainfest.json once
+        await fetch(`${origin}/manifest.json`)
+          .then((res) => res.json())
+          .then(
+            (manifest) => (window.__junoWidgetLoader[origin] = { manifest })
+          )
+      }
 
-  let appURL =
-    url ||
-    window.__junoWidgetLoader[origin].manifest[name][version || "latest"]
-      .entryFile
+      if (
+        debug &&
+        (!window.__junoWidgetLoader[origin].manifest ||
+          !window.__junoWidgetLoader[origin].manifest[name] ||
+          !window.__junoWidgetLoader[origin].manifest[name][version])
+      ) {
+        console.log(
+          `===WIDGET LOADER: could not find app ${name} with version ${version} in manifest`
+        )
+      }
+      // final url
+      appURL = new URL(
+        window.__junoWidgetLoader[origin].manifest[name][version].entryFile,
+        origin
+      ).href
+    }
 
-  let appProps = { currentHost: url || origin }
-  for (let key in props) {
-    if (key.indexOf("props") === 0)
-      appProps[key.replace("props", "").toLowerCase()] = props[key]
-  }
+    // get app props from the data-props-* arguments
+    let appProps = { currentHost: url || origin }
+    for (let key in props) {
+      if (key.indexOf("props") === 0)
+        appProps[key.replace("props", "").toLowerCase()] = props[key]
+    }
 
-  const appWrapper = document.createElement("div")
-  appWrapper.setAttribute("data-juno-app", name || url)
-  appWrapper.setAttribute("style", "height: 100%;")
+    // create a wrapper for the app
+    const appWrapper = document.createElement("div")
+    appWrapper.setAttribute("data-juno-app", name || url)
+    appWrapper.setAttribute("style", "height: 100%;")
 
-  importShim(appURL + "?" + Date.now())
-    .then((app) => {
-      const mount = app.mount || app.default
-      mount(appWrapper, { props: appProps })
-    })
-    .catch((error) => console.warn(error.message))
+    // load the app via importShim
+    if (debug) console.log(`===WIDGET LOADER: load ${appURL}`)
+    importShim(appURL + "?" + Date.now())
+      .then((app) => {
+        const mount = app.mount || app.default
+        mount(appWrapper, { props: appProps })
+      })
+      .catch((error) => console.warn(error.message))
 
-  currentScript.replaceWith(appWrapper)
-})
+    // finally replace the script tag with the wrapper element
+    currentScript.replaceWith(appWrapper)
+  })
+  .catch((error) => console.log("===WIDGET LOADER", error))
