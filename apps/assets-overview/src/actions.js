ea5c8271109d0fb76c678588d6c5133621fc56f8
d064310 <andreas.pfau@sap.com>
2022-12-13 10:09:27 +0100

    add overview app

---
+++ apps/assets-overview/src/actions.js
@@ -0,0 +1,47 @@
+const ENDPOINT = "https://endpoint-url-here.com"
+
+class HTTPError extends Error {
+  constructor(code, message) {
+    super(message || code)
+    this.name = "HTTPError"
+    this.statusCode = code
+  }
+}
+
+const encodeUrlParamsFromObject = (options) => {
+  if (!options) return ""
+  let encodedOptions = Object.keys(options)
+    .map((k) => `${encodeURIComponent(k)}=${encodeURIComponent(options[k])}`)
+    .join("&")
+  return `&${encodedOptions}`
+}
+
+// Check response status
+const checkStatus = (response) => {
+  if (response.status < 400) {
+    return response
+  } else {
+    return response.text().then((message) => {
+      var error = new HTTPError(response.status, message || response.statusText)
+      error.statusCode = response.status
+      return Promise.reject(error)
+    })
+  }
+}
+
+// Example fetch call. Adjust as needed for your API
+export const fetchAssetsManifest = ({ queryKey }) => {
+  const [_key, manifestUrl, options] = queryKey
+  const query = encodeUrlParamsFromObject(options)
+  return fetch(`${manifestUrl}?${query}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+  })
+    .then(checkStatus)
+    .then((response) => {
+      return response.json()
+    })
+}
