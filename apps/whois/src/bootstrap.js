7294197aabdf6b40b99faa224d5c743b30ce8c9c
d064310 <andreas.pfau@sap.com>
2022-12-20 18:28:41 +0100

    remove react-json-view from peer devs

---
+++ apps/whois/src/bootstrap.js
@@ -0,0 +1,19 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./App"
+import styles from "./styles.scss"
+import StyleProvider from "juno-ui-components"
+
+export const init = (wrapper, props) => {
+  ReactDOM.render(
+    <StyleProvider
+      stylesWrapper="shadowRoot"
+      theme={`${props.theme ? props.theme : "theme-dark"}`}
+    >
+      {/* load styles inside the shadow dom */}
+      <style>{styles.toString()}</style>
+      <App {...props} />
+    </StyleProvider>,
+    wrapper
+  )
+}
