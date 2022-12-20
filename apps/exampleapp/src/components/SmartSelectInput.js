c0150b206bd668989cd786c1eb9ef87526681ad3
D063222 <a.reuschenbach.puncernau@sap.com>
2022-12-20 14:18:11 +0100

    [example] working basic select

--- apps/exampleapp/src/components/SmartSelectInput.js
+++ apps/exampleapp/src/components/SmartSelectInput.js
@@ -1,31 +1,24 @@
-import React, { useMemo, useState, useRef, useEffect } from "react"
+import React, { useMemo, useState, useEffect } from "react"
 import {
-  SelectRow,
-  SelectOption,
+  useFloating,
+  autoUpdate,
+  offset,
+  flip,
+  shift,
+  useDismiss,
+  useRole,
+  useClick,
+  useInteractions,
+  FloatingFocusManager,
+  useId,
+} from "@floating-ui/react"
+import {
   TextInputRow,
   DataGrid,
   DataGridRow,
   DataGridCell,
+  Badge,
 } from "juno-ui-components"
-
-const useOnClickOutside = (ref, handler) => {
-  useEffect(() => {
-    const listener = (event) => {
-      // Do nothing if clicking ref's element or descendent elements
-      // if (!ref.current || ref.current.contains(event.target)) {
-      //   return
-      // }
-
-      console.log("ref: ", ref.current, event)
-
-      handler(event)
-    }
-    window.addEventListener("click", listener)
-    return () => {
-      window.removeEventListener("click", listener)
-    }
-  }, [ref, handler])
-}
 
 const optionsContainer = (isOpen) => {
   return `
@@ -34,72 +27,157 @@ const optionsContainer = (isOpen) => {
     outline-none
     ring-2
     ring-theme-focus
-    opacity-0
-    transition
-    ease-out
-    duration-300
-			${isOpen && `opacity-100`}
+    bg-theme-textinput
 		`
     .replace(/\n/g, " ")
     .replace(/\s+/g, " ")
 }
 
-const SmartSelectInput = ({ options }) => {
-  const [displayOptions, setDisplayOptions] = useState(false)
-  const ref = useRef()
+const optionsRow = `
+  hover:text-theme-accent
+`
 
-  useOnClickOutside(ref, () => {
-    console.log("useOnClickOutside")
-    // setDisplayOptions(false)
-  })
+const fakeInputText = `
+  text-theme-textinput
+  bg-theme-textinput
+  min-h-[2.5rem]
+  rounded-3px
+  p-2
+`
+
+const fakeInputTextPlaceholder = `
+  opacity-50
+`
+
+const fakeInputTextOptions = `
+  mr-1
+`
+
+const SmartSelectInput = ({ options }) => {
+  const [open, setOpen] = useState(false)
+  const [selectedOptions, setSelectedOptions] = useState([])
+  const [displayOptions, setDisplayOptions] = useState(false)
 
   options = useMemo(() => {
     if (!options) return []
     return options
   }, [options])
 
-  const onFocus = (event) => {
-    console.log("onfocus", event)
-    setDisplayOptions(true)
+  useEffect(() => {
+    if (options) {
+      const difference = options.filter(
+        ({ value: id1 }) =>
+          !selectedOptions.some(({ value: id2 }) => id2 === id1)
+      )
+      setDisplayOptions(difference)
+    }
+  }, [selectedOptions, options])
+
+  const { x, y, reference, floating, strategy, context } = useFloating({
+    open,
+    placement: "bottom-start",
+    onOpenChange: setOpen,
+    middleware: [offset(5), shift()],
+    whileElementsMounted: autoUpdate,
+  })
+
+  const click = useClick(context)
+  const dismiss = useDismiss(context)
+  const role = useRole(context)
+
+  const { getReferenceProps, getFloatingProps } = useInteractions([
+    click,
+    dismiss,
+    role,
+  ])
+
+  const headingId = useId()
+
+  // ===============
+
+  const onChange = (event) => {
+    setOpen(true)
   }
-  const onBlur = (event) => {
-    console.log("onBlur", event)
-    // setDisplayOptions(false)
-    event.currentTarget.focus()
-    event.preventDefault()
+
+  const onOptionClicked = (option) => {
+    console.log("onOptionClicked: ", option)
+    setSelectedOptions([...selectedOptions, option])
   }
+
+  const onOptionDeselected = (option) => {
+    const index = selectedOptions.findIndex(
+      (item) => item.value == option.value
+    )
+    if (index < 0) {
+      return
+    }
+    let newOptions = selectedOptions.slice()
+    newOptions.splice(index, 1)
+    setSelectedOptions(newOptions)
+  }
+
+  console.log("selectedOptions: ", selectedOptions)
 
   return (
-    <>
-      <SelectRow label="Select Row" onChange={function noRefCheck() {}}>
-        <SelectOption label="Option 1" value="d-1" />
-        <SelectOption label="Option 2" value="d-2" />
-      </SelectRow>
-
-      <TextInputRow
-        label="Smart Select"
-        onChange={function noRefCheck() {}}
-        onFocus={onFocus}
-        onBlur={onBlur}
-      />
-      <div ref={ref}>
-        <div
-          className={`options-container ${optionsContainer(displayOptions)}`}
-        >
-          <DataGrid columns={1}>
-            {options.length > 0 && (
-              <>
-                {options.map((option, i) => (
-                  <DataGridRow key={i}>
-                    <DataGridCell>{option.value}</DataGridCell>
-                  </DataGridRow>
-                ))}
-              </>
-            )}
-          </DataGrid>
+    <div>
+      <div ref={reference} {...getReferenceProps()}>
+        {/* <TextInputRow
+          label="Smart Select"
+          onChange={onChange}
+          // onFocus={onFocus}
+          onBlur={onBlur}
+        /> */}
+        <div className={fakeInputText}>
+          {selectedOptions.length > 0 ? (
+            <>
+              {selectedOptions.map((item, index) => (
+                <Badge
+                  className={fakeInputTextOptions}
+                  key={index}
+                  icon="deleteForever"
+                  text={item.label}
+                  variant="info"
+                  onClick={() => onOptionDeselected(item)}
+                />
+              ))}
+            </>
+          ) : (
+            <div className={fakeInputTextPlaceholder}>Select...</div>
+          )}
         </div>
       </div>
-    </>
+      {open && (
+        <FloatingFocusManager context={context} modal={false}>
+          <div
+            ref={floating}
+            className={`options-container ${optionsContainer(open)}`}
+            style={{
+              position: strategy,
+              top: y ?? 0,
+              left: x ?? 0,
+            }}
+            aria-labelledby={headingId}
+            {...getFloatingProps()}
+          >
+            <DataGrid id={headingId} columns={1}>
+              {displayOptions.length > 0 && (
+                <>
+                  {displayOptions.map((option, i) => (
+                    <DataGridRow
+                      key={i}
+                      onClick={() => onOptionClicked(option)}
+                      className={optionsRow}
+                    >
+                      <DataGridCell>{option.value}</DataGridCell>
+                    </DataGridRow>
+                  ))}
+                </>
+              )}
+            </DataGrid>
+          </div>
+        </FloatingFocusManager>
+      )}
+    </div>
   )
 }
 
