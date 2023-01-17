55f0574458060cfff45fe9c879b7938893ec79df
d064310 <andreas.pfau@sap.com>
2023-01-17 14:17:15 +0100

    ci: improve integrity check

--- scripts/check_file_integrity.sh
+++ scripts/check_file_integrity.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
+# array of (registered) files or directories to be there
 set -- "$@"
 ok=1
 
+# Check whether all files in the array also exist in the folder
 for f in "$@"; do
   [ -f "$f" ] || [ -d "$f" ] || (ok=0 && echo "$f: not found")
 done
 
+# Check if there are files or directories that are not in the array
 for f in *; do
   found=0
   # echo "===: $f"
