81904a97f8c50a33fbb0eed7db83e7cf8c2f9c86
d064310 <andreas.pfau@sap.com>
2023-01-18 15:29:52 +0100

    assets-overview: fix unit test

--- apps/assets-overview/src/App.test.js
+++ apps/assets-overview/src/App.test.js
@@ -10,7 +10,11 @@ jest.mock("./actions.js")
 actions.fetchAssetsManifest = jest.fn().mockResolvedValue({})
 
 test("renders app", async () => {
-  await act(() => render(<App />))
+  await act(() =>
+    render(
+      <App manifestUrl="https://assets.juno.qa-de-1.cloud.sap/manifest.json" />
+    )
+  )
 
   let loginTitle = await screen.queryAllByShadowText(/Converged Cloud/i)
   expect(loginTitle.length > 0).toBe(true)
