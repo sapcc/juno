2a1dfc1488f19589ff50883ce7ea8bf056fce10b
d064310 <andreas.pfau@sap.com>
2023-01-18 17:22:03 +0100

    assets-overview: migrate zustand to v4

--- apps/assets-overview/src/store.js
+++ apps/assets-overview/src/store.js
@@ -1,19 +1,16 @@
-import create from "zustand"
-import { devtools } from "zustand/middleware"
+import { create } from "zustand"
 
 // global zustand store. See how this works here: https://github.com/pmndrs/zustand
-const useStore = create(
-  devtools((set) => ({
-    manifestUrl: "",
-    setManifestUrl: (newUrl) => set((state) => ({ manifestUrl: newUrl })),
-    origin: "",
-    setOrigin: (newOrigin) => set((state) => ({ origin: newOrigin })),
-    region: "",
-    setRegion: (newRegion) => set((state) => ({ region: newRegion })),
-    urlStateKey: "",
-    setUrlStateKey: (newUrlStateKey) =>
-      set((state) => ({ urlStateKey: newUrlStateKey })),
-  }))
-)
+const useStore = create((set) => ({
+  manifestUrl: "",
+  setManifestUrl: (newUrl) => set((state) => ({ manifestUrl: newUrl })),
+  origin: "",
+  setOrigin: (newOrigin) => set((state) => ({ origin: newOrigin })),
+  region: "",
+  setRegion: (newRegion) => set((state) => ({ region: newRegion })),
+  urlStateKey: "",
+  setUrlStateKey: (newUrlStateKey) =>
+    set((state) => ({ urlStateKey: newUrlStateKey })),
+}))
 
 export default useStore
