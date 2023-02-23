cb414c5c25194aa882af679fb5bef7eb6043cb64
Franz Heidl <franz@franzheidl.de>
2023-02-23 14:20:07 +0100

    [ui] Select: add more tests, test stubs

--- libs/juno-ui-components/src/components/Select/Select.test.js
+++ libs/juno-ui-components/src/components/Select/Select.test.js
@@ -1,9 +1,28 @@
 import * as React from "react"
 import { render, screen, fireEvent } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
 import { act } from 'react-dom/test-utils'
 import { Select } from "./index"
 import { SelectOption } from "../SelectOption/index"
 
+/** 
+Testing user interactions
+---
+Testing user interactions with radix and jest seems hard and requires some mocking / polyfilling fpr ResizeObserver as per the below issues.
+Otherwise tests will fail with "ResizeObserver is not defined".
+
+https://github.com/radix-ui/primitives/issues/856
+https://github.com/ZeeCoder/use-resize-observer/issues/40
+https://stackoverflow.com/questions/64558062/how-to-mock-resizeobserver-to-work-in-unit-tests-using-react-testing-library
+
+*/
+
+// Mock ResizeObserver
+class ResizeObserver {
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+}
 
 describe("Select", () => {
   
@@ -33,10 +52,11 @@ describe("Select", () => {
   })
   
   test("renders an open Select as passed", async () => {
-    render(<Select open>
+    // Using the cheap mock above:
+    window.ResizeObserver = ResizeObserver;
+    render(<Select open onOpenChange={ () => {} }>
       <SelectOption value="1"></SelectOption>
     </Select>)
-    expect(screen.getByRole("combobox")).toBeInTheDocument()
     expect(screen.getByRole("listbox")).toBeInTheDocument()
   })
   
@@ -44,6 +64,63 @@ describe("Select", () => {
     render(<Select placeholder="my-placeholder" />)
     expect(screen.getByRole("combobox")).toBeInTheDocument()
     expect(screen.getByRole("combobox")).toHaveTextContent("my-placeholder")
+  })
+  
+  test("sets a value for a controlled Select as passed", async () => {
+    render(
+      <Select value="2">
+        <SelectOption value="1">Option 1</SelectOption>
+        <SelectOption value="2">Option 2</SelectOption>
+        <SelectOption value="3">Option 3</SelectOption>
+      </Select>
+    )
+    expect(screen.getByRole("combobox")).not.toHaveTextContent("Option 1")
+    expect(screen.getByRole("combobox")).toHaveTextContent("Option 2")
+    expect(screen.getByRole("combobox")).not.toHaveTextContent("Option 3")    
+  })
+  
+  test("sets a defaultValue for an uncontrolled Select as passed", async () => {
+    render(
+      <Select defaultValue="2">
+        <SelectOption value="1">Option 1</SelectOption>
+        <SelectOption value="2">Option 2</SelectOption>
+        <SelectOption value="3">Option 3</SelectOption>
+      </Select>
+    )
+    expect(screen.getByRole("combobox")).not.toHaveTextContent("Option 1")
+    expect(screen.getByRole("combobox")).toHaveTextContent("Option 2")
+    expect(screen.getByRole("combobox")).not.toHaveTextContent("Option 3")    
+  })
+  
+  test.skip("allows user to open a Select by clicking the select trigger", async () => {
+    render(
+      <Select>
+        <SelectOption value="1">Option 1</SelectOption>
+        <SelectOption value="2">Option 2</SelectOption>
+        <SelectOption value="3">Option 3</SelectOption>
+      </Select>
+    )
+    expect(screen.getByRole("combobox")).toBeInTheDocument()
+    expect(screen.queryByRole("listbox")).not.toBeInTheDocument()
+    await userEvent.click(screen.getByRole("combobox"))
+    // Test does not find an element with role="listbox"
+    expect(screen.getByRole("listbox")).toBeInTheDocument()
+  })
+  
+  test.skip("allows user to select a value on an uncontrolled Select", async () => {
+    
+  })
+  
+  test.skip("allows user to select a value on a controlled Select", async () => {
+    
+  })
+  
+  test.skip("allows user to change a value on an uncontrolled Select", async () => {
+    
+  })
+  
+  test.skip("allows user to change a value on a controlled Select", async () => {
+    
   })
   
 })
