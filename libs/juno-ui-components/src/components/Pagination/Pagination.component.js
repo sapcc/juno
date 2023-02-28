89f785264af557eeba69424eafabb7af39ae7be1
Franz Heidl <franz@franzheidl.de>
2023-02-28 15:35:34 +0100

    [ui] Pagination: Update prop name to work with new Select

--- libs/juno-ui-components/src/components/Pagination/Pagination.component.js
+++ libs/juno-ui-components/src/components/Pagination/Pagination.component.js
@@ -46,7 +46,7 @@ const renderPaginationInnards = (
       break
     case "select":
       return (
-        <Select name="pages" width="auto" defaultValue={currentPage} onChange={handleSelectChange}>
+        <Select name="pages" width="auto" defaultValue={currentPage} onValueChange={handleSelectChange}>
           { selectOptions(pages) }
         </Select>
       )
