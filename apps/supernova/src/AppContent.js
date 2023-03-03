512aca7386df8a9ee758eea6093f96d5af03ee8a
Arturo Reuschenbach <a.reuschenbach.puncernau@sap.com>
2023-03-03 15:45:57 +0100

    [supernova] Smart grid test (supernova supersonic) (#288)
    * supernova: use virtualized table for alerts
    
    * everything beauty
    
    ---------
    
    Co-authored-by: d064310 <andreas.pfau@sap.com>

--- apps/supernova/src/AppContent.js
+++ apps/supernova/src/AppContent.js
@@ -1,4 +1,4 @@
-import React, {useMemo} from "react"
+import React, { useMemo } from "react"
 import {
   Button,
   ContentAreaToolbar,
@@ -14,7 +14,6 @@ import { currentState, push } from "url-state-provider"
 import AlertsList from "./components/alerts/AlertsList"
 
 const AppContent = (props) => {
-  
   const urlStateKey = useStore((state) => state.urlStateKey)
 
   const { isLoading, isError, data, error } = queryAlerts()
@@ -24,18 +23,30 @@ const AppContent = (props) => {
   //   push(urlStateKey, { ...urlState, newItemFormOpened: true })
   // }
 
-  const {totalCount, criticalCount, warningCount, infoCount} = useMemo(() => {
-    if (!data) return {totalCount: 0, criticalCount: 0, warningCount: 0, infoCount: 0}
+  const { totalCount, criticalCount, warningCount, infoCount } = useMemo(() => {
+    if (!data)
+      return { totalCount: 0, criticalCount: 0, warningCount: 0, infoCount: 0 }
     let totalCount = data.length
-    let criticalCount = data.reduce((critical, item) => item.labels?.severity === "critical" ? ++critical : critical, 0)
-    let warningCount = data.reduce((warning, item) => item.labels?.severity === "warning" ? ++warning : warning, 0)
-    let infoCount = data.reduce((info, item) => item.labels?.severity === "info" ? ++info : info, 0)
+    let criticalCount = data.reduce(
+      (critical, item) =>
+        item.labels?.severity === "critical" ? ++critical : critical,
+      0
+    )
+    let warningCount = data.reduce(
+      (warning, item) =>
+        item.labels?.severity === "warning" ? ++warning : warning,
+      0
+    )
+    let infoCount = data.reduce(
+      (info, item) => (item.labels?.severity === "info" ? ++info : info),
+      0
+    )
 
-    return {totalCount, criticalCount, warningCount, infoCount}
+    return { totalCount, criticalCount, warningCount, infoCount }
   }, [data])
 
   return (
-    <Container px py>
+    <Container px py className="h-full">
       {isError && (
         <Message variant="danger" className="mb-4">
           {`${error.statusCode}, ${error.message}`}
@@ -43,12 +54,12 @@ const AppContent = (props) => {
       )}
 
       {/* Add a toolbar  */}
-      {data && 
+      {data && (
         <div className="bg-theme-background-lvl-2 py-1.5 px-4">
           <span className="text-theme-high pr-2">{`${totalCount} alerts`}</span>
           <span>{`(${criticalCount} critical, ${warningCount} warning, ${infoCount} info)`}</span>
         </div>
-      }
+      )}
 
       {isLoading && (
         <Stack gap="2">
@@ -58,7 +69,6 @@ const AppContent = (props) => {
       )}
 
       <AlertsList />
-
     </Container>
   )
 }
