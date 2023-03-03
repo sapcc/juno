a9bd8da31ebf21ab2b2d32a11b30e499aa986ade
Arturo Reuschenbach <a.reuschenbach.puncernau@sap.com>
2023-03-03 15:45:57 +0100

    [supernova] Smart grid test (supernova supersonic) (#288)
    * supernova: use virtualized table for alerts
    
    * everything beauty
    
    ---------
    
    Co-authored-by: d064310 <andreas.pfau@sap.com>

--- alerts/ui/src/components/alerts/Alert.js
+++ alerts/ui/src/components/alerts/Alert.js
@@ -3,91 +3,94 @@ import { DateTime } from "luxon"
 import { Markup } from "interweave"
 import {
   Badge,
-  Button, 
-  DataGridCell, 
-  DataGridRow, 
-  Icon, 
+  Button,
+  DataGridCell,
+  DataGridRow,
+  Icon,
   Stack,
-  Tooltip, 
-  TooltipContent, 
-  TooltipTrigger } from "juno-ui-components"
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "juno-ui-components"
 
-const Alert = ({alert}) => {
-
+const Alert = ({ alert }) => {
   const descriptionParsed = (text) => {
     if (!text) return ""
     // urls in descriptions follow the schema: <URL|URL-NAME>
     // Parse description and replace urls with a-tags
     const regexUrl = /<(http[^>|]+)\|([^>]+)>/g
     const urlParsed = text.replace(regexUrl, `<a href="$1">$2</a>`)
-  
+
     // replace text wrapped in *..* by strong tags
     const regexBold = /\*(.*)\*/g
     const boldParsed = urlParsed.replace(regexBold, `<strong>$1</strong>`)
-  
+
     const regexCode = /`(.*)`/g
     return boldParsed.replace(regexCode, `<code class="inline-code">$1</code>`)
   }
 
-  const dateTimeFormat = {...DateTime.DATETIME_FULL, month: 'short' }
+  const dateTimeFormat = { ...DateTime.DATETIME_FULL, month: "short" }
   const startTime = DateTime.fromISO(alert.startsAt)
-  var daysFiring = DateTime.now().diff(startTime, 'days');
+  var daysFiring = DateTime.now().diff(startTime, "days")
 
   return (
     <DataGridRow>
       <DataGridCell>
-        { alert.labels?.severity === "critical" ? 
+        {alert.labels?.severity === "critical" ? (
           <Icon icon="danger" color="text-theme-danger" />
-          :
-          alert.labels?.severity.match(/^(warning|info)$/) ?
-            <Icon icon={alert.labels?.severity} color={`text-theme-${alert.labels?.severity}`} />
-            :
-            <Icon icon="errorOutline" />
-        }
+        ) : alert.labels?.severity.match(/^(warning|info)$/) ? (
+          <Icon
+            icon={alert.labels?.severity}
+            color={`text-theme-${alert.labels?.severity}`}
+          />
+        ) : (
+          <Icon icon="errorOutline" />
+        )}
       </DataGridCell>
       <DataGridCell>
         {alert.labels?.region}
-        {alert.labels?.region !== alert.labels?.cluster &&
+        {alert.labels?.region !== alert.labels?.cluster && (
           <>
             <br />
             <span className="text-theme-disabled">{alert.labels?.cluster}</span>
           </>
-        }
+        )}
+      </DataGridCell>
+      <DataGridCell>{alert.labels?.service}</DataGridCell>
+      <DataGridCell>
+        <div className="overflow-y-scroll max-h-12">
+          <span className="text-theme-high">{alert.annotations?.summary}</span>
+          <br />
+          <Markup
+            content={descriptionParsed(
+              alert.annotations?.description?.replace(
+                /`([^`]+)`/g,
+                "<code class='inline-code'>$1</code>"
+              )
+            )}
+            tagName="span"
+            className="text-sm"
+          />
+        </div>
       </DataGridCell>
       <DataGridCell>
-      {alert.labels?.service}
-      </DataGridCell>
-      <DataGridCell>
-        <span className="text-theme-high">{alert.annotations?.summary}</span><br />
-        <Markup
-          content={descriptionParsed(
-            alert.annotations?.description?.replace(
-              /`([^`]+)`/g,
-              "<code class='inline-code'>$1</code>"
-            )
-          )}
-          tagName="span"
-          className="text-sm"
-        />
-      </DataGridCell>
-      <DataGridCell>
-        <Stack alignment="end" gap="2" >
+        <Stack alignment="end" gap="2">
           <div>{startTime.toLocaleString(dateTimeFormat)}</div>
-          {daysFiring.days > 7 &&
+          {daysFiring.days > 7 && (
             <Tooltip variant="warning" triggerEvent="hover">
               <TooltipTrigger asChild>
                 <Icon icon="warning" color="text-theme-warning" />
               </TooltipTrigger>
               <TooltipContent>
-                {`Alert has been firing for ${Math.round(daysFiring.days)} days`}
+                {`Alert has been firing for ${Math.round(
+                  daysFiring.days
+                )} days`}
               </TooltipContent>
             </Tooltip>
-          }
+          )}
         </Stack>
       </DataGridCell>
-      <DataGridCell>
-        {alert.status?.state}
-      </DataGridCell>
+      <DataGridCell>{alert.status?.state}</DataGridCell>
       <DataGridCell>
         <Button size="small">Silence</Button>
       </DataGridCell>
