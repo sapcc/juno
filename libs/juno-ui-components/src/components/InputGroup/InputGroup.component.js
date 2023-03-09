b8ba21072acddd727cded6e9f500a849879be69f
Franz Heidl <franz@franzheidl.de>
2023-03-09 18:01:23 +0100

    [ui] InputGroup: add InputGroup component WIP

---
+++ libs/juno-ui-components/src/components/InputGroup/InputGroup.component.js
@@ -0,0 +1,51 @@
+import React from "react"
+import { Stack } from "../Stack/index"
+import "./input-group.scss"
+import PropTypes from "prop-types"
+
+
+
+/** A component to visually group Buttons, TextInputs, and Select elements. */
+export const InputGroup = ({
+  children,
+  className,
+  variant,
+  disabled,
+  ...props
+}) => {
+  
+  const modifiedChildren = () => {
+    return React.Children.map(children, (child) => {
+      const ownVariant = child.props.variant || variant
+      const ownDisabled = child.props.disabled || disabled
+      return React.cloneElement(child, { 
+        variant: ownVariant,
+        disabled: ownDisabled
+      })
+    })
+  }
+  
+  return (
+    <Stack className={`juno-input-group juno-input-group-${variant} ${ disabled ? "juno-input-group-disabled" : "" }`}>
+      { modifiedChildren() }
+    </Stack>
+  )
+}
+
+InputGroup.propTypes = {
+  /** The children to render */
+  children: PropTypes.node,
+  /** Pass a className to the group */
+  className: PropTypes.string,
+  /** Passing a variant prop to the group will set all child Buttons and Select elements to use that variant, unless specified otherwise on the individual child component */
+  variant: PropTypes.oneOf(["default", "primary", "primary-danger", "subdued"]),
+  /** Disable all elements in the InputGroup */
+  disabled: PropTypes.bool,
+}
+
+InputGroup.defaultProps = {
+  children: null,
+  className: undefined,
+  variant: "default",
+  disabled: false,
+}
---
+++ libs/juno-ui-components/src/components/InputGroup/InputGroup.stories.js
@@ -0,0 +1,125 @@
+import React from "react"
+import { InputGroup } from "./index.js"
+import { Button } from "../Button/index"
+import { Select } from "../Select/index"
+import { SelectOption } from "../SelectOption/index"
+import { TextInput } from "../TextInput/index"
+
+export default {
+  title: "Components/InputGroup",
+  component: InputGroup,
+  argTypes: {},
+}
+
+const Template = (args) => <InputGroup {...args} />
+
+export const Default = Template.bind({})
+Default.args = {
+  children: [
+    <Button label="Button 1"/>,
+    <Button>Button 2</Button>,
+    <Button label="Button 3"/>
+  ]
+}
+
+export const PrimaryInputGroup = Template.bind({})
+PrimaryInputGroup.args = {
+  variant: "primary",
+  children: [
+    <Button label="Primary Button" />,
+    <Button label="Primary Button" />,
+    <Button label="Primary Button" />
+  ]
+}
+
+export const PrimaryDangerInputGroup = Template.bind({})
+PrimaryDangerInputGroup.args = {
+  variant: "primary-danger",
+  children: [
+    <Button label="Primary Danger Button" />,
+    <Button label="Primary Danger Button" variant="primary" />,
+    <Button label="Primary Danger Button" />
+  ]
+}
+
+export const SubduedInputGroup = Template.bind({})
+SubduedInputGroup.args = {
+  variant: "subdued",
+  children: [
+    <Button label="Subdued Button" />,
+    <Button label="Subdued Button" />,
+    <Button label="Subdued Button" />
+  ]
+}
+
+export const Disabled = Template.bind({})
+Disabled.args = {
+  disabled: true,
+  children: [
+    <Button label="Button" />,
+    <Button label="Button" />,
+    <Button label="Button" />
+  ]
+}
+
+export const IconButtons = Template.bind({})
+IconButtons.args = {
+  children: [
+    <Button icon="home" />,
+    <Button icon="help" />,
+    <Button icon="forum" />
+  ]
+}
+
+export const TextInputWithButton = Template.bind({})
+TextInputWithButton.args = {
+  children: [
+    <TextInput placeholder="Enter a value…" />,
+    <Button label="Submit"/>
+  ]
+}
+
+export const MultipleTextInputsWithButton = Template.bind({})
+MultipleTextInputsWithButton.args = {
+  children: [
+    <TextInput placeholder="First Name" />,
+    <TextInput placeholder="Last Name" />,
+    <Button label="Submit"/>
+  ]
+}
+
+export const ButtonWithOptions = Template.bind({})
+ButtonWithOptions.args = {
+  children: [
+    <Button label="Button with Options" />,
+    <Select>
+      <SelectOption value="1" label="Action 1"/>
+      <SelectOption value="2" label="Action 2"/>
+    </Select>
+  ]
+}
+
+export const SelectWithTextInput = Template.bind({})
+SelectWithTextInput.args = {
+  children: [
+    <Select>
+      <SelectOption value="1" label="Action 1"/>
+      <SelectOption value="2" label="Action 2"/>
+    </Select>,
+    <TextInput placeholder="Value…"/>
+  ]
+}
+
+export const SelectWithSelect = Template.bind({})
+SelectWithSelect.args = {
+  children: [
+    <Select>
+      <SelectOption value="1" label="Action 1"/>
+      <SelectOption value="2" label="Action 2"/>
+    </Select>,
+    <Select>
+      <SelectOption value="2-1" label="Action 1"/>
+      <SelectOption value="2-2" label="Action 2"/>
+    </Select>,
+  ]
+}
