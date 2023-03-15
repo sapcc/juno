82dfa8b5b8b727f95c231f8e62180fdf80b757b6
D063222 <a.reuschenbach.puncernau@sap.com>
2023-03-15 12:14:40 +0100

    [supernova] rewrite jest mock for custom hooks

--- apps/supernova/src/App.test.js
+++ apps/supernova/src/App.test.js
@@ -1,9 +1,16 @@
 import React from "react"
 import { render, act } from "@testing-library/react"
+
 // support shadow dom queries
 // https://reactjsexample.com/an-extension-of-dom-testing-library-to-provide-hooks-into-the-shadow-dom/
 import { screen } from "shadow-dom-testing-library"
-jest.mock("./hooks/useAlertmanagerAPI")
+
+jest.mock("./hooks/useCommunication", () => {
+  return jest.fn(() => ({}))
+})
+jest.mock("./hooks/useAlertmanagerAPI", () => {
+  return jest.fn(() => ({}))
+})
 
 import App from "./App"
 
