69c24cb1f1b9d50c0c95fcae41200c5111f40d6d
D038721 <esther.schmitz@sap.com>
2023-03-17 18:22:34 +0100

    [supernova] render links for alerts. adjust startsAt rendering to be two rows

---
+++ alerts/ui/src/components/alerts/AlertLinks.js
@@ -0,0 +1,39 @@
+import React from "react"
+
+import { Stack } from "juno-ui-components"
+
+const AlertLinks = ({alert, className}) => {
+  return (
+    <Stack gap="2" className={className}>
+      {alert.generatorURL && 
+        <a className="underline" href={alert.generatorURL} target="_blank" rel="noopener noreferrer">Prometheus</a>
+      }
+      {alert.labels?.playbook && 
+        <a className="underline" href={`https://operations.global.cloud.sap/${alert.labels?.playbook}`} target="_blank" rel="noopener noreferrer">Playbook</a>
+      }
+      {alert.labels?.kibana && 
+        <a className="underline" href={`https://logs.${alert.labels?.region}.cloud.sap/${alert.labels?.kibana}`} target="_blank" rel="noopener noreferrer">Logs</a>
+      }
+      {alert.labels?.dashboard && 
+        <a className="underline" href={`https://grafana.${alert.labels?.region}.cloud.sap/d/${alert.labels?.dashboard}`} target="_blank" rel="noopener noreferrer">Grafana</a>
+      }
+      {alert.labels?.spc && 
+        <a className="underline" href={`https://spc.ondemand.com/ticket_create/?${alert.labels?.spc}`} target="_blank" rel="noopener noreferrer">SPC Ticket</a>
+      }
+      {alert.labels?.sentry && 
+        <a className="underline" href={`https://sentry.${alert.labels?.region}.cloud.sap/monsoon/${alert.labels?.sentry}`} target="_blank" rel="noopener noreferrer">Sentry</a>
+      }
+      {alert.labels?.cloudops && 
+        <a className="underline" href={`https://dashboard.${alert.labels?.region}.cloud.sap/ccadmin/cloud_admin/cloudops#/universal-search/${alert.labels?.cloudops}`} target="_blank" rel="noopener noreferrer">CloudOps</a>
+      }
+      {alert.labels?.report && 
+        <a className="underline" href={`https://dashboard.${alert.labels?.region}.cloud.sap/${alert.labels?.report}`} target="_blank" rel="noopener noreferrer">Report</a>
+      }
+      {alert.annotations?.mail_subject && 
+        <a className="underline" href={`mailto:?subject=${encodeURIComponent(alert.annotations?.mail_subject)}&body=${encodeURIComponent(alert.annotations?.mail_body)}`} rel="noopener noreferrer">Email Owner</a>
+      }
+    </Stack>
+  )
+}
+
+export default AlertLinks
