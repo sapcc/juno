8299e3e4354ec684105b0ba3e6e2b17ba79e2965
D063222 <a.reuschenbach.puncernau@sap.com>
2023-03-22 09:09:28 +0100

    [supernova]: changed watcher name for user activity events

--- apps/supernova/src/hooks/useCommunication.js
+++ apps/supernova/src/hooks/useCommunication.js
@@ -3,16 +3,16 @@ import { broadcast, get, watch } from "communicator"
 import useStore from "./useStore"
 
 const useCommunication = () => {
-  console.log("useCommunication setup")
+  console.log("[supernova] useCommunication setup")
   const setIsActive = useStore((state) => state.userActivity.setIsActive)
 
   useEffect(() => {
     // watch for user activity updates messages
     // with the watcher we get the user activity object when this app is loaded before the Auth app
     const unwatch = watch(
-      "USERACTIVITY_UPDATE_DATA",
+      "USER_ACTIVITY_UPDATE_DATA",
       (data) => {
-        console.log("got message USERACTIVITY_UPDATE_DATA: ", data)
+        console.log("got message USER_ACTIVITY_UPDATE_DATA: ", data)
         setIsActive(data?.isActive)
       },
       { debug: true }
