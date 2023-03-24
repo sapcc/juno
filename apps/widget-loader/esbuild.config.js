5735b24de1f53b531303de0f19566c1c89d87394
d064310 <andreas.pfau@sap.com>
2023-03-24 12:08:16 +0100

    widget-loader: adjust esbuild config

--- apps/widget-loader/esbuild.config.js
+++ apps/widget-loader/esbuild.config.js
@@ -1,6 +1,8 @@
 const esbuild = require("esbuild")
-
+const fs = require("node:fs/promises")
 const pkg = require("./package.json")
+// this function generates app props based on package.json and propSecrets.json
+const appProps = require("../../helpers/appProps")
 
 if (!/.+\/.+\.js/.test(pkg.main))
   throw new Error(
@@ -9,10 +11,17 @@ if (!/.+\/.+\.js/.test(pkg.main))
 
 const isProduction = process.env.NODE_ENV === "production"
 const IGNORE_EXTERNALS = process.env.IGNORE_EXTERNALS === "true"
-const DEV_FOLDER = "dev"
-const outfile = isProduction ? pkg.main : `${DEV_FOLDER}/app.js`
+const outfile = isProduction ? pkg.main : `public/${pkg.main}`
+// get output from outputfile
+let outdir = outfile.slice(0, outfile.lastIndexOf("/"))
 const args = process.argv.slice(2)
 const watch = args.indexOf("--watch") >= 0
+const serve = args.indexOf("--serve") >= 0
+
+// helpers for console log
+const green = "\x1b[32m%s\x1b[0m"
+const yellow = "\x1b[33m%s\x1b[0m"
+const clear = "\033c"
 
 esbuild
   .context({
@@ -26,18 +35,41 @@ esbuild
       isProduction && !IGNORE_EXTERNALS
         ? Object.keys(pkg.peerDependencies || {})
         : [],
+    plugins: [
+      {
+        name: "start/end",
+        setup(build) {
+          build.onStart(() => {
+            console.log(clear)
+            console.log(yellow, "Compiling...")
+          })
+          build.onEnd(() => console.log(green, "Done!"))
+        },
+      },
+    ],
   })
   .then(async (ctx) => {
-    if (watch) {
-      ctx.watch()
-      console.log("watching...")
-      ctx
-        .serve({
+    // delete build folder and re-create it as an empty folder
+    await fs.rm(outdir, { recursive: true, force: true })
+    await fs.mkdir(outdir, { recursive: true })
+
+    // watch and serve
+    if (watch || serve) {
+      if (watch) await ctx.watch()
+      if (serve) {
+        // generate app props based on package.json and secretProps.json
+        await fs.writeFile(
+          `./${outdir}/appProps.js`,
+          `export default ${JSON.stringify(appProps())}`
+        )
+
+        let { host, port } = await ctx.serve({
           host: "0.0.0.0",
           port: parseInt(process.env.APP_PORT),
-          servedir: DEV_FOLDER,
+          servedir: "public",
         })
-        .then(({ host, port }) => console.log("serve on", `${host}:${port}`))
+        console.log("serve on", `${host}:${port}`)
+      }
     } else {
       await ctx.rebuild()
       await ctx.dispose()
