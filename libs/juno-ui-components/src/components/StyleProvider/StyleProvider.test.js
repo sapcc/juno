068fee04f812ff6872f683e49810659ae1ef4d2a
d064310 <andreas.pfau@sap.com>
2023-03-29 10:16:57 +0200

    juno-ui-components: fix StyleProvider styles

--- libs/juno-ui-components/src/components/StyleProvider/StyleProvider.test.js
+++ libs/juno-ui-components/src/components/StyleProvider/StyleProvider.test.js
@@ -2,18 +2,18 @@ import * as React from "react"
 import { render, screen } from "@testing-library/react"
 import { StyleProvider } from "./index"
 
-
 describe("StyleProvider", () => {
-  
   test("renders a StyleProvider wrapper div with 'theme-dark' theme class by default", async () => {
-		const { container } = render(<StyleProvider></StyleProvider>)
-    expect(container.firstChild).toHaveClass('theme-dark')
-	})
+    const { container } = render(<StyleProvider></StyleProvider>)
+    expect(container.querySelector("div.juno-app-body")).toHaveClass(
+      "theme-dark"
+    )
+  })
 
   test("renders a StyleProvider wrapper div with theme class as passed", async () => {
-		const { container } = render(<StyleProvider theme="my-theme"></StyleProvider>)
-    expect(container.firstChild).toHaveClass('my-theme')
-	})
-
-  
+    const { container } = render(
+      <StyleProvider theme="my-theme"></StyleProvider>
+    )
+    expect(container.querySelector("div.juno-app-body")).toHaveClass("my-theme")
+  })
 })
