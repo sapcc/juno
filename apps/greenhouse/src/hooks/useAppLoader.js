21324c2a1b0086997ecb9c0d50bb5402a94fa6e9
d064310 <andreas.pfau@sap.com>
2023-03-29 17:37:05 +0200

    greenhouse: improve navigation switch (detach/attach dom elements)

--- apps/greenhouse/src/hooks/useAppLoader.js
+++ apps/greenhouse/src/hooks/useAppLoader.js
@@ -1,37 +1,48 @@
-import { useEffect, useCallback, useState } from "react"
+import { useEffect, useCallback, useState, useMemo, useRef } from "react"
 import useStore from "./useStore"
 
+// load widget-loader with importmap-only attribute to
+// ensure the importmap is loaded
+const importmaps = {}
+const loadImportmap = (assetsHost) => {
+  importmaps[assetsHost] =
+    importmaps[assetsHost] ||
+    new Promise((resolve, reject) => {
+      // this promise is resolved once!
+      console.log("===PROMISE", assetsHost)
+      const url = new URL("/apps/widget-loader@latest/build/app.js", assetsHost)
+      const script = document.createElement("script")
+      script.src = url.href
+      script.setAttribute("data-importmap-only", "true")
+
+      document.head.append(script)
+      window.addEventListener("JUNO_IMPORTMAP_LOADED", (e) => {
+        //setLoaded(true)
+        resolve(true)
+      })
+    })
+
+  return importmaps[assetsHost]
+}
+
 const useAppLoader = () => {
-  const [loaded, setLoaded] = useState()
   const assetsHost = useStore((state) => state.assetsHost)
 
-  // ensure Importmap is loaded!
-  useEffect(() => {
-    if (!assetsHost) return
-    const url = new URL("/apps/widget-loader@latest/build/app.js", assetsHost)
-    const script = document.createElement("script")
-    script.src = url.href
-    script.setAttribute("data-importmap-only", "true")
-
-    document.head.append(script)
-    window.addEventListener("JUNO_IMPORTMAP_LOADED", (e) => {
-      setLoaded(true)
-    })
-  }, [setLoaded, assetsHost])
-
   const mount = useCallback(
-    (container, appData) => {
-      if (!loaded) return null
-      console.log("==============MOUNT", appData?.name)
-      let url = appData.url
-        ? appData.url
-        : `@juno/${appData.name}@${appData.version || "latest"}`
-      return importShim(url).then((app) => {
-        app.mount(container, { props: { ...appData.props, embedded: true } })
-        return app.unmount
+    (container, config) => {
+      if (!assetsHost) return null
+      return loadImportmap(assetsHost).then(() => {
+        console.log("==============MOUNT", config?.name)
+        let url = config.url
+          ? config.url
+          : `@juno/${config.name}@${config.version || "latest"}`
+        return importShim(url).then((app) => {
+          app.mount(container, { props: { ...config.props, embedded: true } })
+          return app.unmount
+        })
       })
     },
-    [loaded]
+    [assetsHost]
   )
   return { mount }
 }
