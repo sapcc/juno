ad3e7183120f6f3f3a60c72ef784fa15953e7d84
d064310 <andreas.pfau@sap.com>
2023-04-06 16:44:38 +0200

    juno-ui-components: fix tests

--- libs/juno-ui-components/src/components/AppShellProvider/AppShellProvider.test.js
+++ libs/juno-ui-components/src/components/AppShellProvider/AppShellProvider.test.js
@@ -5,16 +5,14 @@ import { AppShellProvider } from "./index"
 /** Tests need to be adjusted once we know how this will look */
 describe("AppShellProvider", () => {
   test.skip("renders a AppShellProvider wrapper div with 'theme-dark' theme class by default", async () => {
-    const { container } = render(<AppShellProvider></AppShellProvider>)
+    const { container } = render(<AppShellProvider />)
     expect(container.querySelector("div.juno-app-body")).toHaveClass(
       "theme-dark"
     )
   })
 
   test.skip("renders a AppShellProvider wrapper div with theme class as passed", async () => {
-    const { container } = render(
-      <AppShellProvider theme="my-theme"></AppShellProvider>
-    )
+    const { container } = render(<AppShellProvider theme="my-theme" />)
     expect(container.querySelector("div.juno-app-body")).toHaveClass("my-theme")
   })
 })
