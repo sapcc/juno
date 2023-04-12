10049f8a22320db0c00f57858940f28b0e9e6aea
Franz Heidl <franz@franzheidl.de>
2023-04-12 10:13:24 +0200

    [ui] Select: test semantic variants

--- libs/juno-ui-components/src/components/Select/Select.test.js
+++ libs/juno-ui-components/src/components/Select/Select.test.js
@@ -88,6 +88,24 @@ describe("Select", () => {
     expect(screen.getByRole("combobox")).toHaveClass("juno-select-error")
   })
   
+  test("renders a subdued Select variant as passed", async () => {
+    render(<Select variant="subdued" />)
+    expect(screen.getByRole("combobox")).toBeInTheDocument()
+    expect(screen.getByRole("combobox")).toHaveClass("juno-select-subdued")
+  })
+  
+  test("renders a primary Select variant as passed", async () => {
+    render(<Select variant="primary" />)
+    expect(screen.getByRole("combobox")).toBeInTheDocument()
+    expect(screen.getByRole("combobox")).toHaveClass("juno-select-primary")
+  })
+  
+  test("renders a primary-danger Select variant as passed", async () => {
+    render(<Select variant="primary-danger" />)
+    expect(screen.getByRole("combobox")).toBeInTheDocument()
+    expect(screen.getByRole("combobox")).toHaveClass("juno-select-primary-danger")
+  })
+  
   test("renders an open Select as passed", async () => {
     render(<Select open onOpenChange={ () => {} }>
       <SelectOption value="1"></SelectOption>
