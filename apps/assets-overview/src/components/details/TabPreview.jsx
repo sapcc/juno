cef5822b24be380922bca2c66ec2b399f92f523d
D063222 <a.reuschenbach.puncernau@sap.com>
2023-05-15 14:57:33 +0200

    preview with loading state

--- apps/assets-overview/src/components/details/TabPreview.jsx
+++ apps/assets-overview/src/components/details/TabPreview.jsx
@@ -1,13 +1,15 @@
-import React, { useEffect, useMemo } from "react"
+import React, { useEffect, useMemo, useState } from "react"
 import useAppLoader from "../../hooks/useAppLoader"
 import { useRef } from "react"
-import { Box } from "juno-ui-components"
+import { Box, Stack, Spinner } from "juno-ui-components"
 
 const TabPreview = ({ config }) => {
   const { mount } = useAppLoader()
   const holder = useRef()
   const app = useRef(document.createElement("div"))
   const mounted = useRef(false)
+  const [isLoading, setIsLoading] = useState(false)
+  const [isMounted, setIsMounted] = useState(false)
 
   // create a promise to mount the app
   // this promise is resolved once
@@ -15,24 +17,44 @@ const TabPreview = ({ config }) => {
     () =>
       new Promise((resolve) => {
         const a = mount(app.current, config)
+        setIsLoading(true)
+        setIsMounted(false)
         if (!a) resolve(false)
-        else a.then(() => (mounted.current = true)).then(() => resolve(true))
+        else
+          a.then(() => (mounted.current = true)).then(() => {
+            setIsLoading(false)
+            setIsMounted(true)
+            return resolve(true)
+          })
       }),
     [mount, config]
   )
 
   useEffect(() => {
-    if (!config) return
+    if (!isMounted) return
 
     mountApp.then((loaded) => {
       if (!loaded) return
       holder.current.appendChild(app.current)
     })
-  }, [])
+  }, [isMounted])
 
   return (
     <Box className="p-8">
-      <div data-app={config.name} ref={holder}></div>
+      {isLoading ? (
+        <Stack className="pt-2" alignment="center">
+          <Spinner variant="primary" />
+          Loading...
+        </Stack>
+      ) : (
+        <>
+          <p className="mb-6">
+            This is a preview of the {config.name} micro frontend without{" "}
+            <b>any specific configuration.</b>
+          </p>
+          <div data-app={config.name} ref={holder}></div>
+        </>
+      )}
     </Box>
   )
 }
