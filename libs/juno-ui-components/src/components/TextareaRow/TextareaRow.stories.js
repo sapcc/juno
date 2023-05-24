b2659d22adf607186520307367e31bc5527cde23
Franz Heidl <franz@franzheidl.de>
2023-05-24 10:07:35 +0200

    [ui] TextareaRow: render props table in storybook

--- libs/juno-ui-components/src/components/TextareaRow/TextareaRow.stories.js
+++ libs/juno-ui-components/src/components/TextareaRow/TextareaRow.stories.js
@@ -18,17 +18,53 @@ const Template = (args) => <TextareaRow {...args} />
 
 export const Default = Template.bind({})
 Default.args = {
+  value: "",
+  name: "",
+  id: "",
+  placeholder: "",
+  required: false,
+  invalid: false,
+  errortext: "",
+  valid: false,
+  successtext: "",
+  helptext: "",
+  className: "",
+  disabled: false,
+  onChange: undefined,
   label: "Textarea Row",
 }
 
 export const Disabled = Template.bind({})
 Disabled.args = {
+  value: "",
+  name: "",
+  id: "",
+  placeholder: "",
+  required: false,
+  invalid: false,
+  errortext: "",
+  valid: false,
+  successtext: "",
+  helptext: "",
+  className: "",
+  onChange: undefined,
   label: "Disabled Textarea Row",
   disabled: true,
 }
 
 export const WithHelpText = Template.bind({})
 WithHelpText.args = {
+  value: "",
+  id: "",
+  placeholder: "",
+  required: false,
+  invalid: false,
+  errortext: "",
+  valid: false,
+  successtext: "",
+  className: "",
+  disabled: false,
+  onChange: undefined,
   name: "my-input",
   label: "Textarea Row with Helptext",
   helptext: "Oh so helpful helptext",
@@ -36,6 +72,17 @@ WithHelpText.args = {
 
 export const WithHelpTextWithLink = Template.bind({})
 WithHelpTextWithLink.args = {
+  value: "",
+  id: "",
+  placeholder: "",
+  required: false,
+  invalid: false,
+  errortext: "",
+  valid: false,
+  successtext: "",
+  className: "",
+  disabled: false,
+  onChange: undefined,
   name: "my-input",
   label: "Textarea Row with Helptext",
   helptext: <>Helptext with a <a href="#">Link</a></>,
@@ -43,18 +90,53 @@ WithHelpTextWithLink.args = {
 
 export const Required = Template.bind({})
 Required.args = {
+  value: "",
+  name: "",
+  id: "",
+  placeholder: "",
+  invalid: false,
+  errortext: "",
+  valid: false,
+  successtext: "",
+  helptext: "",
+  className: "",
+  disabled: false,
+  onChange: undefined,
   label: "Required Textarea",
   required: true,
 }
 
 export const Invalid = Template.bind({})
 Invalid.args = {
+  value: "",
+  name: "",
+  id: "",
+  placeholder: "",
+  required: false,
+  errortext: "",
+  valid: false,
+  successtext: "",
+  helptext: "",
+  className: "",
+  disabled: false,
+  onChange: undefined,
   label: "Invalid TextareaRow",
   invalid: true,
 }
 
 export const WithErrorText = Template.bind({})
 WithErrorText.args = {
+  value: "",
+  name: "",
+  id: "",
+  placeholder: "",
+  required: false,
+  invalid: false,
+  valid: false,
+  successtext: "",
+  className: "",
+  disabled: false,
+  onChange: undefined,
   label: "Textarea Row with Error Text",
   helptext: "Oh so helpful helptext",
   errortext: "When passed an errortext prop, the TextareaRow will be set to invalid automatically.",
@@ -70,12 +152,36 @@ WithErrorText.parameters = {
 
 export const Valid = Template.bind({})
 Valid.args = {
+  value: "",
+  name: "",
+  id: "",
+  placeholder: "",
+  required: false,
+  invalid: false,
+  errortext: "",
+  successtext: "",
+  helptext: "",
+  className: "",
+  disabled: false,
+  onChange: undefined,
   label: "Valid TextareaRow",
   valid: true,
 }
 
 export const WithSuccessText = Template.bind({})
 WithSuccessText.args = {
+  value: "",
+  name: "",
+  id: "",
+  placeholder: "",
+  required: false,
+  invalid: false,
+  errortext: "",
+  valid: false,
+  helptext: "",
+  className: "",
+  disabled: false,
+  onChange: undefined,
   label: "Textarea Row with Success Text",
   helptext: "Oh so helpful helptext",
   successtext: "When passed a successtext prop, the TextareaRow will be set to valid automatically.",
