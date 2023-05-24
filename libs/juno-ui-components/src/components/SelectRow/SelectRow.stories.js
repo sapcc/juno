8ddb409f04c20b2e85124730bbae8b31d7e60387
Franz Heidl <franz@franzheidl.de>
2023-05-24 11:25:06 +0200

    [ui] [ui] SelectRow: render props table in storybook

--- libs/juno-ui-components/src/components/SelectRow/SelectRow.stories.js
+++ libs/juno-ui-components/src/components/SelectRow/SelectRow.stories.js
@@ -55,6 +55,23 @@ const ControlledTemplate = ({value, children, ...args}) => {
 
 export const Default = Template.bind({})
 Default.args = {
+  name: "",
+  placeholder: "Select…",
+  id: "",
+  required: false,
+  className: "",
+  helptext: "",
+  disabled: false,
+  invalid: false,
+  errortext: "",
+  value: undefined,
+  onValueChange: undefined,
+  onChange: undefined,
+  onOpenChange: undefined,
+  defaultValue: undefined,
+  open: undefined,
+  error: undefined,
+  loading: undefined,
   label: "Select Row",
   items: [
     { ...DefaultSelectOptionStory.args, value: "d-1", label: "Option 1" },
@@ -64,6 +81,21 @@ Default.args = {
 
 export const Controlled = ControlledTemplate.bind({})
 Controlled.args = {
+  name: "",
+  placeholder: "Select…",
+  id: "",
+  required: false,
+  className: "",
+  helptext: "",
+  disabled: false,
+  invalid: false,
+  errortext: "",
+  onChange: undefined,
+  onOpenChange: undefined,
+  defaultValue: undefined,
+  open: undefined,
+  error: undefined,
+  loading: undefined,
   label: "Controlled SelectRow",
   value: "2",
   children: [
@@ -75,6 +107,21 @@ Controlled.args = {
 
 export const WithHelpTextAndPlaceholder = Template.bind({})
 WithHelpTextAndPlaceholder.args = {
+  name: "",
+  id: "",
+  required: false,
+  className: "",
+  disabled: false,
+  invalid: false,
+  errortext: "",
+  value: undefined,
+  onValueChange: undefined,
+  onChange: undefined,
+  onOpenChange: undefined,
+  defaultValue: undefined,
+  open: undefined,
+  error: undefined,
+  loading: undefined,
   label: "Select Row with Helptext and placeholder",
   helptext: "Select one",
   placeholder: "Select an option…", 
@@ -86,6 +133,22 @@ WithHelpTextAndPlaceholder.args = {
 
 export const WithHelpTextWithLink = Template.bind({})
 WithHelpTextWithLink.args = {
+  name: "",
+  placeholder: "Select…",
+  id: "",
+  required: false,
+  className: "",
+  disabled: false,
+  invalid: false,
+  errortext: "",
+  value: undefined,
+  onValueChange: undefined,
+  onChange: undefined,
+  onOpenChange: undefined,
+  defaultValue: undefined,
+  open: undefined,
+  error: undefined,
+  loading: undefined,
   label: "Select Row with Helptext",
   helptext: <>Helptext with a <a href="#">Link</a></>,
   items: [
