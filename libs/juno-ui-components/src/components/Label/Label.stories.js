bce4c763e71d85302af8a6c184b3c0adeb9b7631
Franz Heidl <franz@franzheidl.de>
2023-05-30 10:46:57 +0200

    [ui] Label: add more stories

--- libs/juno-ui-components/src/components/Label/Label.stories.js
+++ libs/juno-ui-components/src/components/Label/Label.stories.js
@@ -2,7 +2,7 @@ import React from "react"
 import { Label } from "./index.js"
 
 export default {
-  title: "Forms/Base Elements/Label",
+  title: "Forms/Label",
   component: Label,
   argTypes: {},
 }
@@ -12,4 +12,16 @@ const Template = (args) => <Label {...args} />
 export const Default = Template.bind({})
 Default.args = {
   text: "My Label",
+}
+
+export const Disabled = Template.bind({})
+Disabled.args = {
+  text: "My disabled label",
+  disabled: true,
+}
+
+export const Required = Template.bind({})
+Required.args = {
+  text: "My required label",
+  required: true,
 }
