cada1d932183f5c7baed61380af05e5a85d97adb
D063222 <a.reuschenbach.puncernau@sap.com>
2023-06-06 16:46:38 +0200

    [supernova] add predefined filter

--- apps/supernova/src/components/filters/Filters.jsx
+++ apps/supernova/src/components/filters/Filters.jsx
@@ -1,8 +1,14 @@
-import React from "react"
+import React, { useLayoutEffect } from "react"
 import { Stack } from "juno-ui-components"
 
 import FilterSelect from "./FilterSelect"
 import FilterPills from "./FilterPills"
+
+import {
+  useAuthData,
+  useFilterLabels,
+  useFilterActions,
+} from "../../hooks/useStore"
 
 const filtersStyles = `
   bg-theme-background-lvl-2
@@ -12,6 +18,29 @@ const filtersStyles = `
 `
 
 const Filters = () => {
+  const authData = useAuthData()
+  const filterLabels = useFilterLabels()
+  const { addActiveFilter } = useFilterActions()
+
+  // load predefined filters
+  // TODO: rewrite this process when filters coming per url
+  useLayoutEffect(() => {
+    if (!authData || !Array.isArray(filterLabels) || filterLabels?.length <= 0)
+      return
+
+    // if no support_group (teams) do nothing
+    if (!authData?.parsed?.teams || !Array.isArray(authData?.parsed?.teams))
+      return
+
+    // if there is no label defined return
+    // TODO: change when mapping available
+    const label = "support_group"
+    if (!filterLabels.includes(label)) return
+
+    authData.parsed.teams.forEach((team) => {
+      addActiveFilter(label, team)
+    })
+  }, [filterLabels, authData, addActiveFilter])
 
   return (
     <Stack direction="vertical" gap="4" className={`filters ${filtersStyles}`}>
