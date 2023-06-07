0d29f079e3ec7a5ba7b94d7852644e89cf80e72d
D063222 <a.reuschenbach.puncernau@sap.com>
2023-06-07 12:52:34 +0200

    let it run just once

--- apps/supernova/src/hooks/usePredefinedFilters.js
+++ apps/supernova/src/hooks/usePredefinedFilters.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 import {
   useAuthData,
@@ -15,26 +15,41 @@ const usePredefinedFilters = () => {
   const activeFilters = useActiveFilters()
   const isUrlStateSetup = useGlobalsIsUrlStateSetup()
 
+  const [isRunOnce, setIsRunOnce] = useState(false)
+
   // load predefined filters
   useEffect(() => {
     // wait until state from Url is set
     if (!isUrlStateSetup) return
 
+    // let this run once
+    if (isRunOnce) return
+    setIsRunOnce(true)
+
     // if filters already set through url state provider do nothing
     if (activeFilters && Object.keys(activeFilters).length > 0) return
 
     // if no support_group of labels available return
-    if (authData?.parsed?.teams?.length <= 0 || filterLabels?.length <= 0)
-      return
-
-    // if there is no label defined return
     const label = "support_group"
-    if (!filterLabels.includes(label)) return
+    if (
+      authData?.parsed?.teams?.length <= 0 ||
+      filterLabels?.length <= 0 ||
+      !filterLabels.includes(label)
+    )
+      return
 
     authData.parsed.teams.forEach((team) => {
       addActiveFilter(label, team)
     })
-  }, [filterLabels, authData, addActiveFilter, isUrlStateSetup])
+  }, [
+    isUrlStateSetup,
+    isRunOnce,
+    filterLabels,
+    authData,
+    addActiveFilter,
+    ,
+    activeFilters,
+  ])
 }
 
 export default usePredefinedFilters
