89331bef1fd8120a00ed993caa4ca038da91836e
D063222 <a.reuschenbach.puncernau@sap.com>
2023-06-16 16:44:33 +0200

    form validation

--- apps/supernova/src/components/alerts/AlertSilence.jsx
+++ apps/supernova/src/components/alerts/AlertSilence.jsx
@@ -10,11 +10,14 @@ import {
   SelectOption,
   Stack,
   Icon,
+  ModalFooter,
+  ButtonRow,
 } from "juno-ui-components"
 import AlertLabels from "./AlertLabels"
 import { Markup } from "interweave"
 import { descriptionParsed } from "../../lib/utils"
 import { useAuthData } from "../../hooks/useStore"
+import { post } from "../../api/client"
 
 const detailsCss = (show) => {
   return `      
@@ -28,15 +31,48 @@ const detailsCss = (show) => {
     .replace(/\s+/g, " ")
 }
 
+const validateForm = (values) => {
+  const invalidItems = {}
+  if (values?.comment?.length <= 3) {
+    if (!invalidItems["comment"]) invalidItems["comment"] = []
+    invalidItems["comment"].push(`Description can't be blank`)
+  }
+
+  return invalidItems
+}
+
+const errorHelpText = (messages) => {
+  return messages.map((msg, i) => (
+    <span key={i} className="block text-theme-danger ">
+      {msg}
+    </span>
+  ))
+}
+
+const DEFAULT_FORM_VALUES = { duration: "2", comment: "" }
+
 const AlertSilence = ({ alert }) => {
   const [displayNewSilence, setDisplayNewSilence] = useState(false)
   const [showDetails, setShowDetails] = useState(false)
+  const [formState, setFormState] = useState(DEFAULT_FORM_VALUES)
+  const [showValidation, setShowValidation] = useState({})
   const authData = useAuthData()
 
   const onCloseModal = () => {
     // reset state
     setDisplayNewSilence(false)
     setShowDetails(false)
+    setFormState(DEFAULT_FORM_VALUES)
+  }
+
+  const onSubmitForm = () => {
+    const formValidation = validateForm(formState)
+    setShowValidation(formValidation)
+    if (Object.keys(formValidation).length > 0) return
+  }
+
+  const onInputChanged = ({ key, value }) => {
+    setFormState({ ...formState, [key]: value })
   }
 
   const name = useMemo(() => {
@@ -56,74 +92,76 @@ const AlertSilence = ({ alert }) => {
           title="New Silence"
           size="large"
           open={true}
+          confirmButtonLabel="Save"
           onCancel={onCloseModal}
-          onConfirm={null}
+          onConfirm={onSubmitForm}
         >
           <span className="text-lg">
             Alert <b>{alert?.labels?.alertname}</b>
           </span>
 
-          <Stack className="mb-4" alignment="center" distribution="end">
-            <div
-              className="cursor-pointer"
-              onClick={() => setShowDetails(!showDetails)}
-            >
-              <Stack alignment="center">
-                <Icon
-                  color="jn-global-text"
-                  icon={showDetails ? "expandLess" : "expandMore"}
-                />
-                Show details
-              </Stack>
-            </div>
-          </Stack>
+          {/* <Stack className="mb-4" alignment="center" distribution="end"> */}
+          <div
+            className="cursor-pointer mt-2"
+            onClick={() => setShowDetails(!showDetails)}
+          >
+            <Stack alignment="center">
+              Show details
+              <Icon
+                color="jn-global-text"
+                icon={showDetails ? "expandLess" : "expandMore"}
+              />
+            </Stack>
+          </div>
+          {/* </Stack> */}
 
           <div className="overflow-hidden">
             <div className={detailsCss(showDetails)}>
-              <AlertLabels alert={alert} />
-              <Box className="mt-4">
-                <Markup
-                  content={descriptionParsed(
-                    alert.annotations?.description?.replace(
-                      /`([^`]+)`/g,
-                      "<code class='inline-code'>$1</code>"
-                    )
-                  )}
-                  tagName="div"
-                  className="text-theme-light"
-                />
-              </Box>
+              <div className="pt-4">
+                <AlertLabels alert={alert} />
+                <Box className="mt-4">
+                  <Markup
+                    content={descriptionParsed(
+                      alert.annotations?.description?.replace(
+                        /`([^`]+)`/g,
+                        "<code class='inline-code'>$1</code>"
+                      )
+                    )}
+                    tagName="div"
+                    className="text-theme-light"
+                  />
+                </Box>
+              </div>
             </div>
           </div>
 
-          <Form className="mt-4">
-            <TextInputRow
-              required
-              label="Silenced by"
-              value={name}
-              disabled
-              onBlur={function noRefCheck() {}}
-              onChange={function noRefCheck() {}}
-              onFocus={function noRefCheck() {}}
-            />
+          <Form className="mt-8">
+            <TextInputRow required label="Silenced by" value={name} disabled />
             <TextareaRow
               label="Description"
-              onChange={function noRefCheck() {}}
+              value={formState.comment}
+              onChange={(e) =>
+                onInputChanged({ key: "comment", value: e.target.value })
+              }
+              helptext={
+                showValidation["comment"] &&
+                errorHelpText(showValidation["comment"])
+              }
               required
             />
             <SelectRow
               required
               label="Duration"
-              defaultValue="2h"
-              onChange={function noRefCheck() {}}
-              onOpenChange={function noRefCheck() {}}
-              onValueChange={function noRefCheck() {}}
+              defaultValue={formState.duration}
+              onValueChange={(e) =>
+                onInputChanged({ key: "duration", value: e })
+              }
             >
-              <SelectOption label="2 hours" value="2h" />
-              <SelectOption label="12 hours" value="12h" />
-              <SelectOption label="1 day" value="1d" />
-              <SelectOption label="3 days" value="3d" />
-              <SelectOption label="7 days" value="7d" />
+              <SelectOption label="2 hours" value="2" />
+              <SelectOption label="12 hours" value="12" />
+              <SelectOption label="1 day" value="24" />
+              <SelectOption label="3 days" value="72" />
+              <SelectOption label="7 days" value="168" />
             </SelectRow>
           </Form>
         </Modal>
