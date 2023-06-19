8f6340173da4bd84b3366432c9857d60b3cb04df
D058266 <hans-georg.winkler@sap.com>
2023-06-19 15:41:37 +0200

    check old versions bevore build

--- ci/scripts/build_asset.sh
+++ ci/scripts/build_asset.sh
@@ -9,8 +9,9 @@ if [ ! -f "CODEOWNERS" ]; then
 fi
 
 function help() {
-  echo "Usage: build_assets.sh --asset-path||-ap --asset-name||-sn --asset-type||-at --output-path||-op
+  echo "Usage: build_assets.sh --asset-path||-ap --asset-name||-sn --asset-type||-at --output-path||-op --last-build-path||-lbp
     Example: ./ci/scripts/build_asset.sh --asset-name auth --asset-path ./apps/auth/ --output-path /tmp/juno-build
+    --last-build-path is optional and only used when asset-type = lib is used to find out there was a new version found
     --output-path is optional default is ./build-result"
   exit
 }
@@ -32,8 +33,13 @@ while [[ $# -gt 0 ]]; do
     shift # past argument
     shift # past value
     ;;
-  --output-type | -at)
+  --asset-type | -at)
     ASSET_TYPE="$2"
+    shift # past argument
+    shift # past value
+    ;;
+  --last-build-path | -lbp)
+    LAST_BUILD_PATH="$2"
     shift # past argument
     shift # past value
     ;;
@@ -75,8 +81,32 @@ echo "use ASSET_PATH  = $ASSET_PATH"
 echo "use OUTPUT_PATH = $OUTPUT_PATH"
 echo "----------------------------------"
 
+if [[ -n "${string}" ]]; then
+  echo "string is not empty"
+fi
+
+# check for new versions from last build
+if [[ "$ASSET_TYPE" == "lib" ]] && [[ -n "$LAST_BUILD_PATH" ]]; then
+  # Version handling, this is only relevant for lib
+  echo "use LAST_BUILD_PATH = $LAST_BUILD_PATH"
+  echo "Check Version..."
+  CURRENT_VERSION=$(jq -r .version "$ASSET_PATH/package.json")
+  echo "Current Version: $CURRENT_VERSION"
+  LAST_VERSION=$(jq -r .version "$LAST_BUILD_PATH/libs/$ASSET_NAME/package.json")
+  echo "Last build version: $LAST_VERSION"
+  if [[ "$CURRENT_VERSION" != "$LAST_VERSION" ]]; then
+    echo "New Version found! This is good 🙂"
+  else
+    # this is a flag that in the upload task nothing new is uploaded
+    echo "No new version found. Nothing to build"
+    mkdir -p "$OUTPUT_PATH/$ASSET_PATH"
+    echo "DO-NOTHING: no new version for lib $ASSET_NAME was found" >"$OUTPUT_PATH/$ASSET_PATH/swift-action"
+    exit
+  fi
+fi
+
 echo "generate COMMUNICATOR.md in $ASSET_PATH"
-node ci/scripts/generate_communication_readme.mjs --path=$ASSET_PATH
+node ci/scripts/generate_communication_readme.mjs --path="$ASSET_PATH"
 
 # install and build libs
 npm run build-libs
@@ -86,9 +116,9 @@ npm run build-libs
 # This is the case if the jspm cdn is unreachable!!!
 echo "----------------------------------"
 echo "run Tests for ...."
-ASSET_NAME=$(jq -r .name $ASSET_PATH/package.json)
-npm --workspace $ASSET_NAME run test --if-present
-NODE_ENV=production IGNORE_EXTERNALS=false npm --workspace $ASSET_NAME run build --if-present
+ASSET_NAME=$(jq -r .name "$ASSET_PATH/package.json")
+npm --workspace "$ASSET_NAME" run test --if-present
+NODE_ENV=production IGNORE_EXTERNALS=false npm --workspace "$ASSET_NAME" run build --if-present
 
 # get BUILD_DIR from package.json
 # strip `leading` slash from BUILD_DIR and split by / and use first part
@@ -98,12 +128,12 @@ NODE_ENV=production IGNORE_EXTERNALS=false npm --workspace $ASSET_NAME run build
 echo "----------------------------------"
 if [[ -z "$BUILD_DIR" ]]; then
   echo "Look for module in package.json"
-  BUILD_DIR=$(jq -r .module $ASSET_PATH/package.json)
+  BUILD_DIR=$(jq -r .module "$ASSET_PATH/package.json")
 fi
 
 if [[ "$BUILD_DIR" == "null" ]]; then
   echo "No module found, look for main in package.json"
-  BUILD_DIR=$(jq -r .main $ASSET_PATH/package.json)
+  BUILD_DIR=$(jq -r .main "$ASSET_PATH/package.json")
 fi
 
 if [[ "$BUILD_DIR" == "null" ]]; then
@@ -112,7 +142,7 @@ if [[ "$BUILD_DIR" == "null" ]]; then
 else
   # $BUILD_DIR = build/index.js -> BUILD_DIR = build
   echo "Module or main found, extract BUILD_DIR"
-  BUILD_DIR="$(echo $BUILD_DIR | sed -e 's/^\///' | cut -d/ -f1)"
+  BUILD_DIR="$(echo "$BUILD_DIR" | sed -e 's/^\///' | cut -d/ -f1)"
 fi
 
 if [[ -z "$BUILD_DIR" ]]; then
@@ -136,24 +166,6 @@ echo "create $OUTPUT_PATH/$ASSET_PATH/package.tgz"
 cd "$OUTPUT_PATH/$ASSET_PATH"
 # todo use npm-pack instead https://docs.npmjs.com/cli/v6/commands/npm-pack
 tar --exclude="package.tgz" -czf package.tgz .
-
-if [[ "$ASSET_TYPE" == "lib" ]]; then
-  # Version handling, this is only relevant for lib
-  echo "Check Version..."
-  VERSION=$(jq -r .version package.json)
-  echo "Version: $VERSION"
-  if [ -f "last-build-version" ]; then
-    LAST_VERSION=$(cat last-build-version)
-    echo "Last build version: $LAST_VERSION"
-    if [[ "$VERSION" != "$LAST_VERSION" ]]; then
-      echo "New Version found! This is good 🙂"
-      echo "$VERSION" >last-build-version
-    else
-      # this is a flag that in the upload task nothing new is uploaded
-      echo "DO-NOTHING: no new version for lib $ASSET_NAME was found" >swift-action
-    fi
-  fi
-fi
 
 echo "----------------------------------"
 echo "Build for $ASSET_NAME done 🙂"
