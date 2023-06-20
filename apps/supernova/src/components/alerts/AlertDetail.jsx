d29b1382ae0fc68d7a37b0265ff8247f83f9aa26
D063222 <a.reuschenbach.puncernau@sap.com>
2023-06-20 13:50:49 +0200

    [supernova] add posibility of add excluded matchers

--- apps/supernova/src/components/alerts/AlertDetail.jsx
+++ apps/supernova/src/components/alerts/AlertDetail.jsx
@@ -1,12 +1,27 @@
 import React from "react"
-import { DataGrid, DataGridRow, DataGridCell, Panel, DataGridHeadCell, PanelFooter, Stack, PanelBody, Spinner } from "juno-ui-components"
-import { useShowDetailsFor, useGlobalsActions, useAlertsActions, useAlertsIsLoading } from "../../hooks/useStore"
+import {
+  DataGrid,
+  DataGridRow,
+  DataGridCell,
+  Panel,
+  DataGridHeadCell,
+  PanelFooter,
+  Stack,
+  PanelBody,
+  Spinner,
+} from "juno-ui-components"
+import {
+  useShowDetailsFor,
+  useGlobalsActions,
+  useAlertsActions,
+  useAlertsIsLoading,
+} from "../../hooks/useStore"
 import AlertIcon from "./shared/AlertIcon"
 import AlertTimestamp from "./shared/AlertTimestamp"
 import AlertDescription from "./shared/AlertDescription"
 import AlertLinks from "./shared/AlertLinks"
 import AlertLabels from "./shared/AlertLabels"
-import AlertSilence from "./AlertSilence"
+import SilenceNew from "../silences/SilenceNew"
 import AlertStatus from "./AlertStatus"
 import AlertRegion from "./shared/AlertRegion"
 
@@ -35,77 +50,68 @@ const AlertDetail = () => {
       size="large"
     >
       <PanelBody>
-        { !alert ? 
-          isAlertsLoading ?
+        {!alert ? (
+          isAlertsLoading ? (
             <Stack gap="2">
               <span>Loading</span>
               <Spinner variant="primary" />
             </Stack>
-            : 
+          ) : (
             "Not found - the alert is probably not firing at the moment"
-          
-          :
+          )
+        ) : (
           <DataGrid columns={2}>
             <DataGridRow>
-              <DataGridHeadCell>
-                Status
-              </DataGridHeadCell>
+              <DataGridHeadCell>Status</DataGridHeadCell>
               <DataGridCell>
                 <AlertStatus status={alert?.labels?.status} />
               </DataGridCell>
             </DataGridRow>
             <DataGridRow>
-              <DataGridHeadCell>
-                Firing Since
-              </DataGridHeadCell>
+              <DataGridHeadCell>Firing Since</DataGridHeadCell>
               <DataGridCell>
                 <AlertTimestamp startTimestamp={alert?.startsAt} />
               </DataGridCell>
             </DataGridRow>
             <DataGridRow>
-              <DataGridHeadCell>
-                Service
-              </DataGridHeadCell>
+              <DataGridHeadCell>Service</DataGridHeadCell>
               <DataGridCell>{alert?.labels?.service}</DataGridCell>
             </DataGridRow>
             <DataGridRow>
-              <DataGridHeadCell>
-                Region
-              </DataGridHeadCell>
+              <DataGridHeadCell>Region</DataGridHeadCell>
               <DataGridCell>
-                <AlertRegion region={alert?.labels?.region} cluster={alert?.labels?.cluster} />
+                <AlertRegion
+                  region={alert?.labels?.region}
+                  cluster={alert?.labels?.cluster}
+                />
               </DataGridCell>
             </DataGridRow>
             <DataGridRow>
-              <DataGridHeadCell>
-                Description
-              </DataGridHeadCell>
+              <DataGridHeadCell>Description</DataGridHeadCell>
               <DataGridCell>
-                <AlertDescription description={alert?.annotations?.description} />
+                <AlertDescription
+                  description={alert?.annotations?.description}
+                />
               </DataGridCell>
             </DataGridRow>
             <DataGridRow>
-              <DataGridHeadCell>
-                Links
-              </DataGridHeadCell>
+              <DataGridHeadCell>Links</DataGridHeadCell>
               <DataGridCell>
                 <AlertLinks alert={alert} />
               </DataGridCell>
             </DataGridRow>
             <DataGridRow>
-              <DataGridHeadCell>
-                Labels
-              </DataGridHeadCell>
+              <DataGridHeadCell>Labels</DataGridHeadCell>
               <DataGridCell>
                 <AlertLabels alert={alert} />
               </DataGridCell>
             </DataGridRow>
           </DataGrid>
-        }
+        )}
       </PanelBody>
 
       <PanelFooter>
-        <AlertSilence alert={alert} />
+        <SilenceNew alert={alert} />
       </PanelFooter>
     </Panel>
   )
