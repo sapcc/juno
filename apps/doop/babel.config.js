8cb7dbdff241589bbb1a5876356be22c749086eb
d064310 <andreas.pfau@sap.com>
2023-06-26 11:41:33 +0200

    remove doop app

--- apps/doop/babel.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  env: {
-    test: {
-      presets: ["@babel/preset-env", "@babel/preset-react"],
-      plugins: [["babel-plugin-transform-import-meta", { module: "ES6" }]],
-    },
-  },
-}
