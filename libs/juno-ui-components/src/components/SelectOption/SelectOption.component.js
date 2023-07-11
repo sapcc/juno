15dfcff44704d96b78cc1b64521c13e616d6b89f
Franz Heidl <franz@franzheidl.de>
2023-07-11 17:12:38 +0200

    [ui] truncate SelectOptions, SelectTrigger
    * add prop whether to truncate SelectOptions or not
    * default false
    * always truncate SelectTrigger text if too long

--- libs/juno-ui-components/src/components/SelectOption/SelectOption.component.js
+++ libs/juno-ui-components/src/components/SelectOption/SelectOption.component.js
@@ -1,6 +1,8 @@
-import React from "react"
+import React, { useContext } from "react"
 import * as RadixSelect from "@radix-ui/react-select"
 import PropTypes from "prop-types"
+import {SelectContext} from "../Select/Select.component.js"
+import "./selectOption.scss"
 
 const optionStyles = `
   jn-text-theme-default
@@ -13,7 +15,6 @@ const optionStyles = `
   jn-cursor-pointer
   jn-select-none
   jn-bg-clip-padding
-  jn-truncate
   jn-text-left
   jn-bg-theme-background-lvl-1
   focus:jn-outline-none
@@ -21,6 +22,10 @@ const optionStyles = `
   focus:jn-ring-inset
   focus:jn-ring-theme-focus
   hover:jn-bg-theme-background-lvl-3
+`
+
+const truncateStyles = `
+  jn-truncate
 `
 
 /** An individual select option. Use as child of Select. */
@@ -34,8 +39,16 @@ export const SelectOption = React.forwardRef(
     className, 
     ...props}, 
   forwardedRef) => {
+    
+    const selectContext = useContext(SelectContext)
+    const {
+      truncateOptions: truncateOptions,
+    } = selectContext || {}
+    
+    console.log(truncateOptions.toString())
+    
     return (
-      <RadixSelect.Item className={`juno-select-option ${optionStyles} ${disabled ? "jn-opacity-50 jn-cursor-not-allowed" : ""} ${className}`} ref={forwardedRef} value={value} disabled={disabled} {...props} >
+      <RadixSelect.Item className={`juno-select-option ${optionStyles} ${disabled ? "jn-opacity-50 jn-cursor-not-allowed" : ""} ${ truncateOptions === true ? "juno-select-option-truncate" : "" } ${className}`} ref={forwardedRef} value={value} disabled={disabled} {...props} >
         <RadixSelect.ItemText>
           {children || label || value }
         </RadixSelect.ItemText>
