154023e385b06dddb2419aa710dfc6813c6d752b
Arturo Reuschenbach Puncernau <a.reuschenbach.puncernau@sap.com>
2023-07-18 11:24:50 +0200

    [supernova] fixed allowing to click all matchers

--- alerts/ui/src/components/silences/SilenceMatchers.jsx
+++ alerts/ui/src/components/silences/SilenceMatchers.jsx
@@ -4,12 +4,16 @@ import { Pill, Stack } from "juno-ui-components"
 
 // Matchers can be clicked if the matcher is configurable
 const SilenceMatchers = ({ matchers, onClickCallback, closeable }) => {
+  console.log("matchers:::::", matchers)
+
   // sort matchers by exclude flag
   const sortedMatchers = useMemo(() => {
     if (!matchers || matchers?.length <= 0) return []
     return matchers.sort((a, b) => a.configurable - b.configurable)
   }, [matchers])
 
+  // cloaseable --> display x in the pill: is true if the matcher is configurable and just in case is excluded
+  // clickable --> is true if the matcher is configurable
   return (
     <Stack gap="2" alignment="start" wrap={true}>
       {sortedMatchers.length > 0 ? (
@@ -19,7 +23,7 @@ const SilenceMatchers = ({ matchers, onClickCallback, closeable }) => {
               className={matcher.configurable ? "opacity-100" : "opacity-70"}
               closeable={matcher.configurable && closeable}
               onClose={() => onClickCallback(matcher)}
-              onClick={() => onClickCallback(matcher)}
+              onClick={() => matcher.configurable && onClickCallback(matcher)}
               key={matcher.name}
               pillKey={matcher.name}
               pillValue={matcher.value}
