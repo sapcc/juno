a67b2e65174706d20c353b34404ee3fb99fdd7ed
Franz Heidl <franz@franzheidl.de>
2023-07-25 17:26:43 +0200

    [ui] ComboBox: add some basic styling, manage width

--- libs/juno-ui-components/src/components/ComboBox/ComboBox.component.js
+++ libs/juno-ui-components/src/components/ComboBox/ComboBox.component.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from "react"
 import PropTypes from "prop-types"
 import { Combobox } from "@headlessui/react"
+import { Label } from "../Label/index.js"
 import { Icon } from "../Icon/index.js"
 
 const inputWrapperStyles = `
   jn-relative
-  jn-inline-block
   jn-bg-theme-textinput
   jn-text-theme-textinput
   jn-border
@@ -13,24 +13,45 @@ const inputWrapperStyles = `
 `
 
 const inputStyles = `
+  jn-bg-theme-textinput
+  jn-text-theme-textinput
+  jn-border
+  jn-border-theme-textinput-default
   jn-text-base
   jn-leading-4
+  jn-w-full
   jn-px-4
   jn-h-textinput
+  jn-text-left
+  jn-pt-[0.4rem]
   focus:jn-outline-none
   focus:jn-ring-2
   focus:jn-ring-theme-focus
-  disabled:jn-opacity-50
+  data-[headlessui-state=disabled]:jn-opacity-50
+`
+
+const labelStyles = `
+  jn-pointer-events-none
+  jn-top-2
+  jn-left-4
 `
 
 const buttonStyles = `
   jn-absolute
+  jn-top-0
   jn-right-0
   jn-h-textinput
-  jn-top-0
   jn-w-6
   jn-h-4
-  jn-border
+  jn-border-transparent
+  jn-appearance-none
+  jn-bg-theme-textinput
+  jn-text-theme-textinput
+`
+
+const menuStyles = `
+  jn-rounded
+  jn-bg-theme-background-lvl-1
 `
 
 export const ComboBox = ({
@@ -39,9 +60,15 @@ export const ComboBox = ({
   label,
   value,
   placeholder,
+  truncateOptions,
   width,
   ...props
 }) => {
+  
+  const isNotEmptyString = (str) => {
+    return !(typeof str === 'string' && str.trim().length === 0)
+  }
+  
   const [searchStr, setSearchStr] = useState("")
   
   const handleInputChange = (event) => {
@@ -56,25 +83,49 @@ export const ComboBox = ({
       )
   
   return (
-    <div>
-      <Combobox>
-        <div className={`${inputWrapperStyles}`}>
-          <Combobox.Input 
-            disabled={disabled} 
-            placeholder={placeholder} 
-            onChange={handleInputChange}
-            className={`${inputStyles}`} 
-          />
-          <Combobox.Button disabled={disabled} className={`${buttonStyles}`}>
-            {({open}) => (
-              <Icon icon={ open ? "expandLess": "expandMore"} />
-            )}
-          </Combobox.Button>
-        </div>
-        <Combobox.Options>
-          { filteredChildren }
-        </Combobox.Options>
-      </Combobox>
+    <div className={`
+      juno-combobox-wrapper
+      jn-relative
+      ${ width == "auto" ? "jn-inline-block" : "jn-block" }
+      ${ width == "auto" ? "jn-w-auto" : "jn-w-full" }
+    `}>
+      
+      
+        { label && isNotEmptyString(label) ?
+            <Label 
+              text={label}
+              className={`${labelStyles}`}
+              floating
+              minimized
+            />
+          :
+            ""
+        }
+        
+        <Combobox>
+          <div className={`${inputWrapperStyles}`}>
+            <Combobox.Input 
+              disabled={disabled} 
+              placeholder={placeholder} 
+              onChange={handleInputChange}
+              className={`
+                juno-combobox-input 
+                ${inputStyles}`
+              } 
+            />
+            <Combobox.Button disabled={disabled} className={`juno-combobox-toggle ${buttonStyles}`}>
+              {({open}) => (
+                <Icon icon={ open ? "expandLess": "expandMore"} />
+              )}
+            </Combobox.Button>
+          </div>
+          <Combobox.Options className={`juno-combobox-options ${menuStyles}`} >
+            { filteredChildren }
+          </Combobox.Options>
+        </Combobox>
+        
+        
+        
     </div>
   )
 }
--- libs/juno-ui-components/src/components/ComboBoxOption/ComboBoxOption.component.js
+++ libs/juno-ui-components/src/components/ComboBoxOption/ComboBoxOption.component.js
@@ -1,12 +1,56 @@
-import React, { useState, useEffect } from "react"
+import React, { Fragment, useState, useEffect } from "react"
 import PropTypes from "prop-types"
 import { Combobox } from "@headlessui/react"
+import { Icon } from "../Icon/" 
+import "./ComboBoxOption.scss"
+
+const optionStyles = `
+  jn-text-theme-default
+  jn-pt-[0.6875rem]
+  jn-pb-[0.5rem]
+  jn-px-[0.875rem]
+  data-[headlessui-state=active]:jn-outline-none
+  data-[headlessui-state=active]:jn-ring-2
+  data-[headlessui-state=active]:jn-ring-inset
+  data-[headlessui-state=active]:jn-ring-theme-focus
+  data-[headlessui-state=active]:jn-bg-theme-background-lvl-3
+  data-[headlessui-state=selected]:text-green
+`
 
 export const ComboBoxOption = ({
   value,
+  className,
   ...props
 }) => {
   return (
-    <Combobox.Option value={value} {...props} />
+    <Combobox.Option
+      value={value}
+      as={Fragment}
+    >
+      <li 
+        className={`
+          juno-combobox-option 
+          ${optionStyles}`
+        }
+        {...props}
+      >
+        {( {selected }) => (
+         selected ?  <Icon icon="check" /> : ""
+        )}
+        <span>{value}</span>
+      </li>
+
+    </Combobox.Option>
   )
+}
+
+
+ComboBoxOption.propTypes = {
+  value: PropTypes.string,
+  className: PropTypes.string,
+}
+
+ComboBoxOption.defaultProps = {
+  value: "",
+  className: "",
 }
