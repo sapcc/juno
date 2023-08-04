d55eadb63da0310a08a0d83ab2a7b836adbc1da2
D038721 <esther.schmitz@sap.com>
2023-08-04 10:44:08 +0200

    [greenhouse] show info banner for demo system

--- apps/greenhouse/src/components/layout/ShellLayout.js
+++ apps/greenhouse/src/components/layout/ShellLayout.js
@@ -1,5 +1,6 @@
 import React from "react"
 import PluginNav from "../nav/PluginNav"
+import { useDemoMode } from "../../hooks/useStore"
 
 const shellStyles = `
   grid
@@ -14,10 +15,15 @@ const mainStyles = `
 `
 
 const ShellLayout = ({ children }) => {
+  const demoMode = useDemoMode()
+
   return (
     <div className={`greenhouse-shell ${shellStyles}`}>
       <PluginNav />
-      <div className={`greenhouse-main ${mainStyles}`}>{children}</div>
+      <div>
+        {demoMode && <div className="bg-theme-accent/30 py-2 px-4">Welcome to the Greenhouse demo system! We're glad you're here! Just a quick heads up: you won't find any live data here. Enjoy exploring!</div>}
+        <div className={`greenhouse-main ${mainStyles}`}>{children}</div>
+      </div>
     </div>
   )
 }
