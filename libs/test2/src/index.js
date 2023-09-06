78d35a8dba3d7f5e37d7f8c2d418ff73f2f771df
d064310 <andreas.pfau@sap.com>
2023-09-06 13:49:52 +0200

    add juno cli

---
+++ libs/test2/src/index.js
@@ -0,0 +1,50 @@
+/**
+ * test2
+ *
+ * [Brief description of your library's purpose]
+ *
+ * @version 1.0.0
+ * @license Apache-2.0
+ */
+
+/**
+ * [Library description]
+ *
+ * @class
+ */
+class test2 {
+  /**
+   * Create an instance of test2.
+   *
+   * @constructor
+   * @param {Object} options - Library configuration options.
+   */
+  constructor(options = {}) {
+    // Initialize your library here
+  }
+
+  /**
+   * [Method description]
+   *
+   * @param {string} param1 - [Description of param1]
+   * @param {number} param2 - [Description of param2]
+   * @returns {string} - [Description of the return value]
+   */
+  sampleMethod(param1, param2) {
+    // Implement your method logic here
+    return `${param1} ${param2}`
+  }
+
+  /**
+   * [Another method description]
+   *
+   * @param {string} data - [Description of the data parameter]
+   * @returns {Object} - [Description of the return value]
+   */
+  anotherMethod(data) {
+    // Implement another method here
+    return { result: data }
+  }
+}
+
+module.exports = test2
