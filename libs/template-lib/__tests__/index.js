b347b9dc783737ad379f061beb38ec163183f16c
d064310 <andreas.pfau@sap.com>
2023-09-06 13:49:52 +0200

    add juno cli

---
+++ libs/template-lib/__tests__/index.js
@@ -0,0 +1,25 @@
+const Template = require("../src/index")
+
+describe("Template", () => {
+  let library
+
+  beforeEach(() => {
+    // Initialize the library or create a new instance for each test
+    library = new Template()
+  })
+
+  describe("sampleMethod", () => {
+    it("should return a concatenated string", () => {
+      const result = library.sampleMethod("Hello", 42)
+      expect(result).toBe("Hello 42")
+    })
+  })
+
+  describe("anotherMethod", () => {
+    it("should return an object with the provided data", () => {
+      const data = "Test Data"
+      const result = library.anotherMethod(data)
+      expect(result).toEqual({ result: data })
+    })
+  })
+})
