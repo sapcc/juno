e0dd3e39d583666c945ec886cdb3b5cf2119ec9f
Arturo Reuschenbach Puncernau <a.reuschenbach.puncernau@sap.com>
2023-10-06 16:12:57 +0200

    test corrected

--- libs/utils/src/hooks/useEndlessScrollList.test.js
+++ libs/utils/src/hooks/useEndlessScrollList.test.js
@@ -2,7 +2,7 @@ import { renderHook, act, waitFor } from "@testing-library/react"
 import { useEndlessScrollList } from "../index"
 
 describe("useEndlessScrollList", () => {
-  it("return no messages on initialize", () => {
+  it("return no scroll items if items not all provided", () => {
     const { result } = renderHook(() => useEndlessScrollList([]))
     expect(result.current.scrollListItems.length).toBe(0)
   })
