9226044ad6eb694ef54d7a3c01ee5cd0061dbb17
d064310 <andreas.pfau@sap.com>
2023-10-16 15:38:08 +0200

    communicator: fix bug in get function. Wait for promise to be resolved and use the result

--- apps/auth/src/useCommunication.js
+++ apps/auth/src/useCommunication.js
@@ -4,22 +4,29 @@ import { broadcast, watch, onGet } from "communicator"
 const useCommunication = ({ authData, login, logout, debug }) => {
   useEffect(() => {
     // inform that the auth app has been loaded!
-    broadcast("AUTH_APP_LOADED", true)
-    onGet("AUTH_APP_LOADED", () => true)
+    broadcast("AUTH_APP_LOADED", true, { debug, consumerID: "auth" })
+    onGet("AUTH_APP_LOADED", () => true, { debug, consumerID: "auth" })
   }, [])
 
   useEffect(() => {
-    broadcast("AUTH_UPDATE_DATA", authData, { debug })
+    broadcast("AUTH_UPDATE_DATA", authData, { debug, consumerID: "auth" })
     const unwatchGet = onGet("AUTH_GET_DATA", () => authData, {
       debug,
+      consumerID: "auth",
     })
 
     return unwatchGet
   }, [authData])
 
   useEffect(() => {
-    const unwatchLogin = watch("AUTH_LOGIN", login, { debug })
-    const unwatchLogout = watch("AUTH_LOGOUT", logout, { debug })
+    const unwatchLogin = watch("AUTH_LOGIN", login, {
+      debug,
+      consumerID: "auth",
+    })
+    const unwatchLogout = watch("AUTH_LOGOUT", logout, {
+      debug,
+      consumerID: "auth",
+    })
     // unregister on get listener when unmounting
     return () => {
       unwatchLogin()
--- apps/greenhouse/src/hooks/useCommunication.js
+++ apps/greenhouse/src/hooks/useCommunication.js
@@ -36,22 +36,33 @@ const useCommunication = () => {
   useEffect(() => {
     if (!authAppLoaded || authIsProcessing || authError) return
     if (authLastAction?.name === "signOn" && !authLoggedIn) {
-      broadcast("AUTH_LOGIN", "greenhouse", { debug: true })
+      broadcast("AUTH_LOGIN", "greenhouse", {
+        debug: true,
+        consumerID: "greenhouse",
+      })
     } else if (authLastAction?.name === "signOut" && authLoggedIn) {
-      broadcast("AUTH_LOGOUT", "greenhouse", { debug: true })
+      broadcast("AUTH_LOGOUT", "greenhouse", {
+        debug: true,
+        consumerID: "greenhouse",
+      })
     }
   }, [authAppLoaded, authIsProcessing, authError, authLoggedIn, authLastAction])
 
   useEffect(() => {
     if (!authSetData || !authSetAppLoaded) return
-    get("AUTH_APP_LOADED", authSetAppLoaded)
-    const unwatchLoaded = watch("AUTH_APP_LOADED", authSetAppLoaded, {
+    get("AUTH_APP_LOADED", authSetAppLoaded, {
+      consumerID: "greenhouse",
       debug: true,
     })
+    const unwatchLoaded = watch("AUTH_APP_LOADED", authSetAppLoaded, {
+      debug: true,
+      consumerID: "greenhouse",
+    })
 
-    get("AUTH_GET_DATA", setAuthData)
+    get("AUTH_GET_DATA", setAuthData, { consumerID: "greenhouse", debug: true })
     const unwatchUpdate = watch("AUTH_UPDATE_DATA", setAuthData, {
       debug: true,
+      consumerID: "greenhouse",
     })
 
     return () => {
