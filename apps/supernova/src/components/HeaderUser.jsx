8b6e53de328f35be7452eb599b033f98083204a6
D063222 <a.reuschenbach.puncernau@sap.com>
2023-10-18 15:42:30 +0200

    [supernova] switch to avatarUrl

--- apps/supernova/src/components/HeaderUser.jsx
+++ apps/supernova/src/components/HeaderUser.jsx
@@ -1,19 +1,14 @@
-import React, { useMemo } from "react"
+import React from "react"
 import { Stack, Button } from "juno-ui-components"
 import Avatar from "./Avatar"
 
 const HeaderUser = ({ auth, logout }) => {
-  const name = useMemo(() => {
-    return auth?.parsed?.fullName
-  }, [auth])
-
   return (
     <Stack alignment="center" className="ml-auto" distribution="end">
       <div className="mr-4">
         <Avatar
-          userName={name}
-          avatarUrl={auth?.parsed?.avatarUrl}
-          displayName
+          userName={auth?.parsed?.fullName}
+          url={auth?.parsed?.avatarUrl?.small}
         />
       </div>
 
