2301eacd0d1693f4d7ab053f8e35d4a9cc7f4993
D063222 <a.reuschenbach.puncernau@sap.com>
2023-10-18 13:51:10 +0200

    [auth-app] remove custom handling of avatar urls

--- apps/auth/src/App.js
+++ apps/auth/src/App.js
@@ -3,20 +3,6 @@ import useCommunication from "./useCommunication"
 import { oidcSession, mockedSession } from "oauth"
 import { MOCKED_TOKEN } from "./mockedToken"
 
-const enrichAuthData = (data) => {
-  if (!data) return data
-
-  const enrichedAuth = { ...data }
-  const userId = data.auth?.parsed?.loginName
-
-  if (enrichAuthData?.auth?.data?.parsed)
-    enrichedAuth.auth.parsed["avatarUrl"] = {
-      small: `https://avatars.wdf.sap.corp/avatar/${userId}?size=24x24`,
-    }
-
-  return enrichedAuth
-}
-
 const App = (props = {}) => {
   const [authData, setAuthData] = React.useState()
 
@@ -42,8 +28,7 @@ const App = (props = {}) => {
         token,
         initialLogin: props.initialLogin,
         onUpdate: (authData) => {
-          let data = enrichAuthData(authData)
-          setAuthData(data)
+          setAuthData(authData)
         },
       })
     }
@@ -56,8 +41,7 @@ const App = (props = {}) => {
       requestParams: props.requestParams,
       flowType: "code",
       onUpdate: (authData) => {
-        let data = enrichAuthData(authData)
-        setAuthData(data)
+        setAuthData(authData)
       },
     })
   }, [setAuthData])
