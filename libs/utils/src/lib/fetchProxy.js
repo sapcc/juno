1f23e1fde9d4dc19aea0b59222d42351830356b6
Arturo Reuschenbach Puncernau <a.reuschenbach.puncernau@sap.com>
2023-10-20 10:20:50 +0200

    [utils] warn instead of error user when reinitializing the local db

--- libs/utils/src/lib/fetchProxy.js
+++ libs/utils/src/lib/fetchProxy.js
@@ -45,9 +45,9 @@ export const fetchProxyInitDB = (jsonData, options = {}) => {
 
   // check if the localDB is initialized and warns if already initialized
   if (localDB) {
-    // create a new custom error to return
-    throw new Error(
-      `localDB already initialized. Please use fetchProxyInitDB(null) to reset the localDB.`
+    // create a new custom warning to return
+    return console.warn(
+      `fetchProxyInitDB:: localDB already initialized. This typically occurs when the component or hook, responsible for local database initialization, is accidentally re-rendered. If you intend to reset the local database, please ensure to set localDB to null first by invoking fetchProxyInitDB(null) before providing new data."`
     )
   }
 
