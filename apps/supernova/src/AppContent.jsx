e89bdfa9e7111b7f0b66483b0f4aa6946109685f
Arturo Reuschenbach Puncernau <a.reuschenbach.puncernau@sap.com>
2023-11-06 15:01:33 +0100

    [supernova] added warn to inform firefox users to activate allow_client_cert

--- apps/supernova/src/AppContent.jsx
+++ apps/supernova/src/AppContent.jsx
@@ -46,6 +46,30 @@ const AppContent = (props) => {
   useEffect(() => {
     // since the API call is done in a web worker and not logging aware, we need to show the error just in case the user is logged in
     if (!alertsError || !loggedIn) return
+
+    // if user uses firefox warn to activate `allow_client_cert`. Schould be enough to do it when getting the
+    const isFirefox = navigator.userAgent.toLowerCase().includes("firefox")
+    if (isFirefox) {
+      addMessage({
+        variant: "warning",
+        text: (
+          <p>
+            Firefox detected. Please ensure that you have activated{" "}
+            <b>allow_client_cert</b> to enable the retrieval of alerts and
+            silences from the API.
+            <ul>
+              <li>1. Go to about:config (via address bar)</li>
+              <li>
+                2. Change <b>network.cors_preflight.allow_client_cert</b> to{" "}
+                <b>true</b>
+              </li>
+              <li>3. Reload Greenhouse</li>
+            </ul>
+          </p>
+        ),
+      })
+    }
+
     addMessage({
       variant: "error",
       text: parseError(alertsError),
