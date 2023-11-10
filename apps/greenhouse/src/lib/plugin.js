70c512b413dc4fdad4a51fbc8d124027709ade9c
D063222 <a.reuschenbach.puncernau@sap.com>
2023-11-10 16:35:12 +0100

    createPlugin with tests

---
+++ apps/greenhouse/src/lib/plugin.js
@@ -0,0 +1,36 @@
+const pluginConfig = {
+  id: "",
+  name: "",
+  displayName: "",
+  version: "latest",
+  url: null,
+  weight: 0,
+  navigable: true,
+  props: {
+    id: "",
+  },
+}
+
+export const createPluginConfig = (config) => {
+  // check required attrs
+  if (!config?.id || !config?.name) {
+    console.warn(
+      `[greenhouse]::createPluginConfig: id and name are required. Skipping config: ${config}`
+    )
+    return null
+  }
+
+  // clone default pluginConfig
+  const newConfig = { ...pluginConfig }
+  // update just known attrs
+  Object.keys(newConfig).forEach((key) => {
+    // check agains type to update falsy booleans
+    if (typeof config?.[key] !== "undefined") newConfig[key] = config?.[key]
+  })
+  // check displayName
+  if (!newConfig?.displayName) newConfig.displayName = newConfig.name
+  // update id to the props attr
+  newConfig.props = { ...newConfig.props, id: newConfig.id }
+
+  return newConfig
+}
---
+++ apps/greenhouse/src/lib/plugin.test.js
@@ -0,0 +1,83 @@
+import { createPluginConfig } from "./plugin"
+
+describe("Plugin", () => {
+  describe("createPluginConfig", () => {
+    it("requires at least an id and name", () => {
+      const spy = jest.spyOn(console, "warn").mockImplementation(() => {})
+
+      createPluginConfig()
+      createPluginConfig({ id: "test" })
+      createPluginConfig({ name: "test" })
+
+      expect(spy).toHaveBeenCalledTimes(3)
+      expect(spy).toHaveBeenCalledWith(
+        expect.stringContaining(
+          "[greenhouse]::createPluginConfig: id and name are required."
+        )
+      )
+      spy.mockRestore()
+    })
+
+    it("maps name to displayName if missing", () => {
+      expect(createPluginConfig({ id: "id_test", name: "name_test" })).toEqual(
+        expect.objectContaining({ displayName: "name_test" })
+      )
+    })
+
+    it("sets weight to default 0 if missing", () => {
+      expect(createPluginConfig({ id: "id_test", name: "name_test" })).toEqual(
+        expect.objectContaining({ weight: 0 })
+      )
+    })
+
+    it("sets version to latest if missing", () => {
+      expect(createPluginConfig({ id: "id_test", name: "name_test" })).toEqual(
+        expect.objectContaining({ version: "latest" })
+      )
+    })
+
+    it("sets navigable to true if missing", () => {
+      expect(createPluginConfig({ id: "id_test", name: "name_test" })).toEqual(
+        expect.objectContaining({ navigable: true })
+      )
+    })
+
+    it("adds id to the props", () => {
+      expect(createPluginConfig({ id: "id_test", name: "name_test" })).toEqual(
+        expect.objectContaining({
+          props: expect.objectContaining({ id: "id_test" }),
+        })
+      )
+    })
+
+    it("does not save not known keys", () => {
+      expect(
+        createPluginConfig({ id: "id_test", name: "name_test", miau: "bup" })
+      ).not.toEqual(
+        expect.objectContaining({
+          miau: "bup",
+        })
+      )
+    })
+
+    it("creates a plugin", () => {
+      const config = {
+        id: "id_test",
+        name: "name_test",
+        displayName: "displayName_Test",
+        version: "1.2.3",
+        url: "http://miau.bup",
+        weight: 9,
+        navigable: false,
+        props: {
+          test1: "test1",
+          test2: "test2",
+        },
+      }
+      expect(createPluginConfig(config)).toEqual({
+        ...config,
+        props: { ...config.props, id: config.id },
+      })
+    })
+  })
+})
