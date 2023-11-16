e9aead095ab03ec9a554e3fd3196c7744248c546
TilmanHaupt <tilman.haupt@sap.com>
2023-11-16 13:29:12 +0100

    [Storybook] Added allowlist for generatingTailwindThemeClassesJson

--- libs/juno-ui-components/src/docs/ColorPalette/TailwindThemeTextColors.js
+++ libs/juno-ui-components/src/docs/ColorPalette/TailwindThemeTextColors.js
@@ -4,9 +4,5 @@ module.exports={
   "default": "jn-text-theme-default",
   "light": "jn-text-theme-light",
   "disabled": "jn-text-theme-disabled",
-  "link": "jn-text-theme-link",
-  "on-danger": "jn-text-theme-on-danger",
-  "on-default": "jn-text-theme-on-default",
-  "textinput": "jn-text-theme-textinput",
-  "checkbox-checked": "jn-text-theme-checkbox-checked"
+  "link": "jn-text-theme-link"
 }
--- libs/juno-ui-components/src/docs/ColorPalette/generateTailwindThemeClassesJson.js
+++ libs/juno-ui-components/src/docs/ColorPalette/generateTailwindThemeClassesJson.js
@@ -22,33 +22,66 @@ const generateTailwindThemeClassesJson = () => {
 }
 
 const getColors = () => {
+  const allowList = [
+    "accent",
+    "danger",
+    "error",
+    "info",
+    "success",
+    "warning",
+    "focus",
+    "background-lvl-0",
+    "background-lvl-1",
+    "background-lvl-2",
+    "background-lvl-3",
+    "background-lvl-4",
+    "background-lvl-5",
+  ]
   const theme = tailwindConfig.theme.colors.theme
   const colors = {}
   Object.keys(theme).forEach((colorName) => {
-    colors[colorName] = `jn-bg-theme-${colorName}`
+    if (allowList.includes(colorName)) {
+      colors[colorName] = `jn-bg-theme-${colorName}`
+    }
   })
 
   return colors
 }
 
 const getTextColors = () => {
+  const allowList = ["highest", "high", "default", "light", "disabled", "link"]
   const theme = tailwindConfig.theme.extend.textColor.theme
   const colors = {}
   Object.keys(theme).forEach((colorName) => {
-    colors[colorName] = `jn-text-theme-${colorName}`
+    if (allowList.includes(colorName)) {
+      colors[colorName] = `jn-text-theme-${colorName}`
+    }
   })
 
   return colors
 }
 
 const getJunoColors = () => {
-  const rejectList = ["theme", "current", "transparent"]
+  const allowList = [
+    "juno-grey-blue",
+    "juno-blue",
+    "juno-turquoise",
+    "juno-grey-light",
+    "sap-grey",
+    "sap-blue",
+    "sap-gold",
+    "sap-purple",
+    "sap-green",
+    "sap-red",
+    "white",
+    "black",
+  ]
   const colorList = tailwindConfig.theme.colors
 
   let colors = {}
 
   Object.keys(colorList).forEach((colorName) => {
-    if (!rejectList.includes(colorName)) {
+    if (allowList.includes(colorName)) {
       colors[colorName] = `jn-bg-${colorName}`
 
       const colorfam = Object.keys(colorList[colorName])
