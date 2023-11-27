62a525ac6e75cf7eae49725943c3676e9d003290
TilmanHaupt <tilman.haupt@sap.com>
2023-11-27 20:47:09 +0100

    [storybook] added the component JunoComponentParent to allow styled and themed JunoComponents

---
+++ libs/juno-ui-components/src/docs/JunoComponentParent/JunoComponentParent.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react"
+
+import { StyleProvider } from "../../components/StyleProvider/StyleProvider.component"
+import { getCurrentThemeMode } from "../../../.storybook/juno-addon/themes"
+import { addons } from "@storybook/preview-api"
+
+// allows to implement themed components in the storybook
+export function JunoComponentParent(props) {
+  // returns nothing if there is no child
+  if (!props.children) {
+    return
+  }
+
+  const [parentTheme, setParentTheme] = useState(
+    "theme-" + getCurrentThemeMode()
+  )
+
+  useEffect(() => {
+    const updateThemeClass = (mode) => {
+      setParentTheme("theme-" + mode)
+    }
+    const channel = addons.getChannel()
+    if (channel) {
+      channel.on("JUNO_THEME_CHANGE", updateThemeClass)
+    }
+    return () => {
+      if (channel) channel.off("JUNO_THEME_CHANGE", updateThemeClass)
+    }
+  }, [])
+
+  return (
+    <div>
+      <StyleProvider
+        key="decorator"
+        stylesWrapper="shadowRoot"
+        theme={parentTheme}
+      >
+        {props.children}
+      </StyleProvider>
+    </div>
+  )
+}
