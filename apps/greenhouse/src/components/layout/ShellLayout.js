99ddf68f59df9c6b4501728596f7f306a921e8bf
Arturo Reuschenbach <a.reuschenbach.puncernau@sap.com>
2024-01-16 11:21:13 +0100

    [greenhouse] added notifications area with messages provider. Added messages Provider per Plugin (#503)
    * [greenhouse] added notifications area with messages provider. Added messages Provider per Plugin
    
    * upgrade package since 0.1.10 released ist

--- apps/greenhouse/src/components/layout/ShellLayout.js
+++ apps/greenhouse/src/components/layout/ShellLayout.js
@@ -1,6 +1,6 @@
 import React from "react"
 import PluginNav from "../nav/PluginNav"
-import { useDemoMode } from "../../components/StoreProvider"
+import NotificationsContainer from "../NotificationsContainer"
 
 const shellStyles = `
   grid
@@ -16,19 +16,11 @@ const mainStyles = `
 `
 
 const ShellLayout = ({ children }) => {
-  const demoMode = useDemoMode()
-
   return (
     <div className={`greenhouse-shell ${shellStyles}`}>
       <PluginNav />
       <div>
-        {demoMode && (
-          <div className="bg-theme-accent/30 py-2 px-4">
-            Welcome to the Greenhouse demo system! We're glad you're here! Just
-            a quick heads up: you won't find any live data here. Enjoy
-            exploring!
-          </div>
-        )}
+        <NotificationsContainer />
         <div className={`greenhouse-main ${mainStyles}`}>{children}</div>
       </div>
     </div>
