5d6a5f370ec3efdc4e08b2e49b4605c59ae543e1
TilmanHaupt <tilman.haupt@sap.com>
2024-01-22 17:00:07 +0100

    [greenhose-mgmt] implement Review

--- apps/greenhouse-management/src/components/Auth.jsx
+++ apps/greenhouse-management/src/components/Auth.jsx
@@ -1,46 +1,17 @@
-import React, { useEffect, useState } from "react"
+import React from "react"
 import { Spinner, Stack } from "juno-ui-components"
-import { useAuthAppLoaded, useIsLoggedIn } from "./StoreProvider"
+import { useIsLoggedIn } from "./StoreProvider"
 import { Transition } from "@tailwindui/react"
 
 // Adds a loading screen while during auth
 // Shows children when auth is complete
 
 const Auth = ({ children }) => {
-  const authAppLoaded = useAuthAppLoaded()
   const authLoggedIn = useIsLoggedIn()
-
-  const [loading, setLoading] = useState(!authAppLoaded)
-
-  // timeout for waiting for auth
-  useEffect(() => {
-    setLoading(!authAppLoaded)
-    if (authAppLoaded) return
-    // set timeout for waiting for auth app
-    let loadingTimer
-    if (!authAppLoaded) {
-      loadingTimer = setTimeout(() => {
-        if (!authAppLoaded) setLoading(false)
-      }, 30000) // 30 seconds
-    }
-
-    return () => loadingTimer && clearTimeout(loadingTimer)
-  }, [authAppLoaded, setLoading])
 
   return (
     <>
-      <Transition
-        show={!!authLoggedIn}
-        enter="transition-opacity duration-1000"
-        enterFrom="opacity-0"
-        enterTo="opacity-100"
-        leave="transition-opacity duration-0"
-        leaveFrom="opacity-100"
-        leaveTo="opacity-0"
-      >
-        {children}
-      </Transition>
-
+      {!!authLoggedIn && children}
       {!authLoggedIn && (
         <Stack
           alignment="center"
@@ -50,7 +21,7 @@ const Auth = ({ children }) => {
         >
           <Spinner className="mx-6 mb-3" variant="primary" size="1.5rem" />
 
-          {loading ? "Loading..." : "Signing on..."}
+          <p>Loading...</p>
         </Stack>
       )}
     </>
