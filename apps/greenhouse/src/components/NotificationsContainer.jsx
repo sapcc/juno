fe849c35c580b024357df18f6256fff53fdd459e
Arturo Reuschenbach Puncernau <a.reuschenbach.puncernau@sap.com>
2024-01-22 14:31:00 +0100

    [greenhouse] revert to the old style when displaying the demo info

--- apps/greenhouse/src/components/NotificationsContainer.jsx
+++ apps/greenhouse/src/components/NotificationsContainer.jsx
@@ -1,9 +1,22 @@
 import React from "react"
 import { Messages } from "messages-provider"
+import { useDemoMode } from "./StoreProvider"
 
 const NotificationsContainer = () => {
-  // do not use a container here to align the messages to the ones comming from each plugin
-  return <Messages className="mx-4" />
+  const demoMode = useDemoMode()
+
+  return (
+    <>
+      {demoMode && (
+        <div className="bg-theme-accent/30 py-2 px-4">
+          Welcome to the Greenhouse demo system! We're glad you're here! Just a
+          quick heads up: you won't find any live data here. Enjoy exploring!
+        </div>
+      )}
+      {/* do not use a container here to align the messages to the ones coming from each plugin */}
+      <Messages className="mx-4 mt-4" />
+    </>
+  )
 }
 
 export default NotificationsContainer
