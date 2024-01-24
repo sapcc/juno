3df8c003cebca77e0a685b8b46d36ea4f61da9de
d064310 <andreas.pfau@sap.com>
2024-01-24 15:44:08 +0100

    ci: delete obsolete assets versions

--- ci/scripts/delete_obsolete_assets.js
+++ ci/scripts/delete_obsolete_assets.js
@@ -1,13 +1,27 @@
 const fs = require("fs")
 const path = require("path")
+const semverSatisfies = require("semver/functions/satisfies")
+const semverCoerce = require("semver/functions/coerce")
 
+function colorize(color, output) {
+  return ["\x1b[", color, "m", output, "\x1b[0m"].join("")
+}
+const green = (text) => colorize(32, text)
+const yellow = (text) => colorize(33, text)
+const red = (text) => colorize(31, text)
+const blue = (text) => colorize(34, text)
+const cyan = (text) => colorize(36, text)
+
+// read the command line arguments
 const args = process.argv.slice(2)
 
+// default options
 const options = {
   manifest: null,
   src: "./",
 }
 
+// parse the command line arguments
 args.forEach((arg) => {
   let [key, value] = arg.split("=")
   // convert dash-case to camelCase
@@ -15,43 +29,56 @@ args.forEach((arg) => {
   options[key] = value
 })
 
-console.log("options:", options)
-console.log("delete obsolete assets in " + options.src)
+// log given options
+console.log(cyan("INFO:"), "delete obsolete assets in " + options.src)
+console.log(cyan("OPTIONS:"), options)
+// exit if the manifest is not provided
 if (!options.manifest) {
-  console.error("Please provide a source URL")
+  console.error(
+    "Please provide a source of the json file (manifest) containing the list of obsolete assets (e.g. --manifest=obsolete_assets.json."
+  )
   process.exit(1)
 }
 
-async function deleteAssets(src, assetNames) {
+async function deleteAssets(src, obsoleteAssets) {
   fs.readdirSync(src).forEach(async (file) => {
-    // console.log("::::", src, file)
-    const newPath = path.join(src, file)
+    // build absolute path of the file
+    const filePath = path.join(src, file)
+    // return if the path is a file
+    if (fs.lstatSync(filePath).isFile()) return
 
-    const allVersionsPattern = file.replace(/@.*$/, "") + "@*"
-    // console.log("===", allVersionsPattern)
-    if (assetNames.includes(file) || assetNames.includes(allVersionsPattern)) {
-      console.log("delete", file)
-      await fs.rmSync(newPath, { recursive: true })
-    } else if (fs.statSync(newPath).isDirectory()) {
-      deleteAssets(newPath, assetNames)
+    const match = file.match(/^(.*)@(.*)$/)
+    if (match) {
+      const name = match[1]
+      const version = match[2]
+      const obsoleteVersions = obsoleteAssets[name]
+
+      if (obsoleteVersions) {
+        process.stdout.write(
+          cyan("FOUND:") +
+            " " +
+            blue(file) +
+            ", " +
+            yellow(`obsolete versions: ${obsoleteVersions} `)
+        )
+
+        if (
+          obsoleteVersions === "*" ||
+          semverSatisfies(semverCoerce(version), obsoleteVersions)
+        ) {
+          process.stdout.write(red("delete"))
+          fs.rmSync(filePath, { recursive: true })
+        } else {
+          process.stdout.write(green("keep"))
+        }
+        process.stdout.write("\n")
+      }
+    } else {
+      deleteAssets(filePath, obsoleteAssets)
     }
   })
 }
-
-async function main() {
-  const obsoleteAssets = JSON.parse(
-    await fs.readFileSync(options.manifest, "utf-8")
-  )
-
-  const assetNames = []
-  for (let name in obsoleteAssets) {
-    obsoleteAssets[name].forEach((version) => {
-      assetNames.push(`${name}@${version}`)
-    })
-  }
-
-  console.log("Assets to be deleted:", assetNames)
-  await deleteAssets(options.src, assetNames)
-}
-
-main()
+const obsoleteAssetsString = fs.readFileSync(options.manifest, "utf-8")
+console.log(cyan("OBSOLETE ASSETS:"), yellow(obsoleteAssetsString))
+const obsoleteAssets = JSON.parse(obsoleteAssetsString)
+deleteAssets(options.src, obsoleteAssets)
