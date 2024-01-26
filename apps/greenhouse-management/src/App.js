d00e82462c8c3c1f5d79ff5345f373afaf72e139
TilmanHaupt <tilman.haupt@sap.com>
2024-01-26 14:28:07 +0100

    [greenhouse-mgmt] Added Errors and Loading-info

--- apps/greenhouse-management/src/App.js
+++ apps/greenhouse-management/src/App.js
@@ -41,7 +41,7 @@ const App = (props = {}) => {
 
       <AppBody data-testid="greenhouse-management">
         <MessagesProvider>
-          <Messages />
+          <Messages className="mb-4" />
           <Auth>
             <OrgInfo />
             <MainContainer>
