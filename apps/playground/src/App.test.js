488f29115b2cd973728296c1ebe0376f741724e4
Arturo Reuschenbach Puncernau <a.reuschenbach.puncernau@sap.com>
2024-02-27 15:49:50 +0100

    [playground] fix tests

--- apps/playground/src/App.test.js
+++ apps/playground/src/App.test.js
@@ -3,6 +3,17 @@ import { render, act, waitFor } from "@testing-library/react"
 // support shadow dom queries
 // https://reactjsexample.com/an-extension-of-dom-testing-library-to-provide-hooks-into-the-shadow-dom/
 import { screen } from "shadow-dom-testing-library"
+
+jest.mock("@uiw/react-textarea-code-editor", () => {
+  return {
+    __esModule: true,
+    A: true,
+    default: () => {
+      return <div></div>
+    },
+  }
+})
+
 import App from "./App"
 
 test("renders app", async () => {
