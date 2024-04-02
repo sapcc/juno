abdf40b9d2c376c90a84f95679e08114da7cdb1d
Franz Heidl <franz@franzheidl.de>
2024-04-02 17:28:05 +0200

    [ui] TextareaRow, TextInputRow: stories: remove obsolere re-declaration of default props in stories

--- libs/juno-ui-components/src/components/TextareaRow/TextareaRow.stories.js
+++ libs/juno-ui-components/src/components/TextareaRow/TextareaRow.stories.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import { TextareaRow } from './index.js';
+import React from "react"
+import { TextareaRow } from "./index.js"
 
 export default {
-  title: 'Deprecated/TextareaRow',
+  title: "Deprecated/TextareaRow",
   component: TextareaRow,
   parameters: {
     docs: {
       description: {
         component:
-          'DEPRECATED: A textarea row containing a textarea, associated label, optional helptext, and structural markup. This component is DEPRECATED, use Textarea instead.',
+          "DEPRECATED: A textarea row containing a textarea, associated label, optional helptext, and structural markup. This component is DEPRECATED, use Textarea instead.",
       },
     },
   },
@@ -26,200 +26,94 @@ export default {
       control: false,
     },
   },
-};
+}
 
 export const Default = {
   args: {
-    value: '',
-    name: '',
-    id: '',
-    placeholder: '',
-    required: false,
-    invalid: false,
-    errortext: '',
-    valid: false,
-    successtext: '',
-    helptext: '',
-    className: '',
-    disabled: false,
-    onChange: undefined,
-    label: 'Textarea Row',
+    label: "Default Textarea Row",
   },
-};
+}
 
 export const Disabled = {
   args: {
-    value: '',
-    name: '',
-    id: '',
-    placeholder: '',
-    required: false,
-    invalid: false,
-    errortext: '',
-    valid: false,
-    successtext: '',
-    helptext: '',
-    className: '',
-    onChange: undefined,
-    label: 'Disabled Textarea Row',
+    label: "Disabled Textarea Row",
     disabled: true,
   },
-};
+}
 
 export const WithHelpText = {
   args: {
-    value: '',
-    id: '',
-    placeholder: '',
-    required: false,
-    invalid: false,
-    errortext: '',
-    valid: false,
-    successtext: '',
-    className: '',
-    disabled: false,
-    onChange: undefined,
-    name: 'my-input',
-    label: 'Textarea Row with Helptext',
-    helptext: 'Oh so helpful helptext',
+    name: "my-input",
+    label: "Textarea Row with Helptext",
+    helptext: "Oh so helpful helptext",
   },
-};
+}
 
 export const WithHelpTextWithLink = {
   args: {
-    value: '',
-    id: '',
-    placeholder: '',
-    required: false,
-    invalid: false,
-    errortext: '',
-    valid: false,
-    successtext: '',
-    className: '',
-    disabled: false,
-    onChange: undefined,
-    name: 'my-input',
-    label: 'Textarea Row with Helptext',
+    name: "my-input",
+    label: "Textarea Row with Helptext",
     helptext: (
       <>
         Helptext with a <a href="#">Link</a>
       </>
     ),
   },
-};
+}
 
 export const Required = {
   args: {
-    value: '',
-    name: '',
-    id: '',
-    placeholder: '',
-    invalid: false,
-    errortext: '',
-    valid: false,
-    successtext: '',
-    helptext: '',
-    className: '',
-    disabled: false,
-    onChange: undefined,
-    label: 'Required Textarea',
+    label: "Required Textarea",
     required: true,
   },
-};
+}
 
 export const Invalid = {
   args: {
-    value: '',
-    name: '',
-    id: '',
-    placeholder: '',
-    required: false,
-    errortext: '',
-    valid: false,
-    successtext: '',
-    helptext: '',
-    className: '',
-    disabled: false,
-    onChange: undefined,
-    label: 'Invalid TextareaRow',
+    label: "Invalid TextareaRow",
     invalid: true,
   },
-};
+}
 
 export const WithErrorText = {
   args: {
-    value: '',
-    name: '',
-    id: '',
-    placeholder: '',
-    required: false,
-    invalid: false,
-    valid: false,
-    successtext: '',
-    className: '',
-    disabled: false,
-    onChange: undefined,
-    label: 'Textarea Row with Error Text',
-    helptext: 'Oh so helpful helptext',
+    label: "Textarea Row with Error Text",
+    helptext: "Oh so helpful helptext",
     errortext:
-      'When passed an errortext prop, the TextareaRow will be set to invalid automatically.',
+      "When passed an errortext prop, the TextareaRow will be set to invalid automatically.",
   },
 
   parameters: {
     docs: {
       description: {
         story:
-          'Passing an `errortext` prop to the TextareaRow component will automatically invalidate it, so there is no need to explicitly set `invalid` as well.',
+          "Passing an `errortext` prop to the TextareaRow component will automatically invalidate it, so there is no need to explicitly set `invalid` as well.",
       },
     },
   },
-};
+}
 
 export const Valid = {
   args: {
-    value: '',
-    name: '',
-    id: '',
-    placeholder: '',
-    required: false,
-    invalid: false,
-    errortext: '',
-    successtext: '',
-    helptext: '',
-    className: '',
-    disabled: false,
-    onChange: undefined,
-    label: 'Valid TextareaRow',
+    label: "Valid TextareaRow",
     valid: true,
   },
-};
+}
 
 export const WithSuccessText = {
   args: {
-    value: '',
-    name: '',
-    id: '',
-    placeholder: '',
-    required: false,
-    invalid: false,
-    errortext: '',
-    valid: false,
-    helptext: '',
-    className: '',
-    disabled: false,
-    onChange: undefined,
-    label: 'Textarea Row with Success Text',
-    helptext: 'Oh so helpful helptext',
+    label: "Textarea Row with Success Text",
+    helptext: "Oh so helpful helptext",
     successtext:
-      'When passed a successtext prop, the TextareaRow will be set to valid automatically.',
+      "When passed a successtext prop, the TextareaRow will be set to valid automatically.",
   },
 
   parameters: {
     docs: {
       description: {
         story:
-          'Passing a `successtext` prop to the TextareaRow component will automatically set it to valid, so there is no need to explicitly set `valid` as well.',
+          "Passing a `successtext` prop to the TextareaRow component will automatically set it to valid, so there is no need to explicitly set `valid` as well.",
       },
     },
   },
-};
+}
