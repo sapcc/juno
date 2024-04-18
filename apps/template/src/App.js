a6395071e5a610842ecd3d788f5aaa57f8d85d5a
TilmanHaupt <tilman.haupt@sap.com>
2024-04-18 14:09:50 +0200

    [template] clean up images

--- apps/template/src/App.js
+++ apps/template/src/App.js
@@ -6,18 +6,14 @@
 import React from "react"
 
 import { AppShell, AppShellProvider } from "juno-ui-components"
-import StoreProvider, { useGlobalsActions } from "./components/StoreProvider"
+import StoreProvider from "./components/StoreProvider"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import AppContent from "./AppContent"
 import styles from "./styles.scss"
 
-/* IMPORTANT: Replace this with your app's name */
-const URL_STATE_KEY = "template"
 /* --------------------------- */
 
 const App = (props = {}) => {
-  const { setUrlStateKey } = useGlobalsActions()
-
   // Create query client which it can be used from overall in the app
   // set default endpoint to fetch data
   const queryClient = new QueryClient({
@@ -29,13 +25,6 @@ const App = (props = {}) => {
       },
     },
   })
-
-  // on app initial load save Endpoint and URL_STATE_KEY so it can be
-  // used from overall in the application
-  React.useEffect(() => {
-    // set to empty string to fetch local test data in dev mode
-    setUrlStateKey(URL_STATE_KEY)
-  }, [])
 
   return (
     <QueryClientProvider client={queryClient}>
