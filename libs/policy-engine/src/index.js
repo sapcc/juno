3ebb213a0bd02cc2882185b40d724d941333ef55
Arturo Reuschenbach <a.reuschenbach.puncernau@sap.com>
2024-05-24 11:27:44 +0200

    [migration] remove migrated libs (#608)

--- libs/policy-engine/src/index.js
+++
@@ -1,9 +0,0 @@
-/*
- * SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Juno contributors
- * SPDX-License-Identifier: Apache-2.0
- */
-
-import PolicyEngine from "./engine"
-
-const init = (config) => new PolicyEngine(config)
-export { init as default, PolicyEngine }
