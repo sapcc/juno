d3ff16bdd262ef74e5e6648606e6feb035e1e0c2
Arturo Reuschenbach Puncernau <reuschenbach@gmail.com>
2024-06-03 15:12:38 +0200

    Revert "Revert "[migration] remove migrated libs (#608)""
    This reverts commit 99b1006ff0f8e6f6935f1089e5639e6e7f911b08.

--- libs/juno-ui-components/src/components/FilterPill/index.js
+++
@@ -1,6 +0,0 @@
-/*
- * SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Juno contributors
- * SPDX-License-Identifier: Apache-2.0
- */
-
-export { FilterPill } from "./FilterPill.component"
