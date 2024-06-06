59e9d04e64ce0dcc07e5cdbbb0b7275e4343315a
d064310 <andreas.pfau@sap.com>
2024-06-06 13:41:49 +0200

    dashboard: add build to git

---
+++ apps/dashboard/build/index.js
@@ -0,0 +1,1 @@
+import{createRoot as n}from"react-dom/client";import m from"react";var o=(t,r={})=>{import("./App-EQUWQ3XA.js").then(e=>{o.root=n(t),o.root.render(m.createElement(e.default,r?.props))})},c=()=>o.root&&o.root.unmount();export{o as mount,c as unmount};
