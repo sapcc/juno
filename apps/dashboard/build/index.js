b30c28c2102538fabbc179d3bca6640aeb060c46
Arturo Reuschenbach Puncernau <reuschenbach@gmail.com>
2024-06-11 10:34:19 +0200

    fix(dashboard) added gitignore to avoid to commit node_modules and build folder

--- apps/dashboard/build/index.js
+++
@@ -1,6 +0,0 @@
-/*
- * SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Juno contributors
- * SPDX-License-Identifier: Apache-2.0
- */
-
-import{createRoot as n}from"react-dom/client";import m from"react";var o=(t,r={})=>{import("./App-EQUWQ3XA.js").then(e=>{o.root=n(t),o.root.render(m.createElement(e.default,r?.props))})},c=()=>o.root&&o.root.unmount();export{o as mount,c as unmount};
