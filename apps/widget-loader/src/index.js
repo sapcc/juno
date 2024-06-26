ac7fbc2dce4a773ace0ad9d8086f79e160c3a48f
d064310 <andreas.pfau@sap.com>
2024-06-26 15:36:08 +0200

    fix(widget-loader): use importmapAppNAme instead of name

--- apps/widget-loader/src/index.js
+++ apps/widget-loader/src/index.js
@@ -147,7 +147,7 @@ const ESM_SHIM_URL = "/externals/es-module-shims@1.8.3/dist/es-module-shims.js"
   const importmapAppName = name?.startsWith("@") ? name : `@juno/${name}`
   // get the app URL
   // from given url or from importmap based on name and version
-  let appURL = url || importmap.imports[`${name}@${version}`]
+  let appURL = url || importmap.imports[`${importmapAppName}@${version}`]
 
   // get app props from the data-props-* arguments
   let appProps = { currentHost: new URL(appURL).href }
